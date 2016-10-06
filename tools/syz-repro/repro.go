2a2fd0ddce258f1d943ad8d3e3bec79423a0f434
Dmitry Vyukov <dvyukov@google.com>
2016-10-06 16:15:10 +0200

    vm/gce: add support GCE VMs

--- tools/syz-repro/repro.go
+++ tools/syz-repro/repro.go
@@ -22,6 +22,7 @@ import (
 	"github.com/google/syzkaller/report"
 	"github.com/google/syzkaller/vm"
 	_ "github.com/google/syzkaller/vm/adb"
+	_ "github.com/google/syzkaller/vm/gce"
 	_ "github.com/google/syzkaller/vm/kvm"
 	_ "github.com/google/syzkaller/vm/qemu"
 )
