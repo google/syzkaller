d105629ea2f7a4b351392fdb21d828311f8f410e
Dmitry Vyukov <dvyukov@google.com>
2016-12-06 20:06:45 +0100

    vm/qemu: use panic=86400 instead of panic=-1
    panic=-1 causes instant reboot, which is bad for crash logs.
    panic=86400 makes it hang. Use panic=86400 as create-gce-image.sh does.

--- vm/qemu/qemu.go
+++ vm/qemu/qemu.go
@@ -189,7 +189,7 @@ func (inst *instance) Boot() error {
 		)
 	}
 	if inst.cfg.Kernel != "" {
-		cmdline := "console=ttyS0 vsyscall=native rodata=n oops=panic panic_on_warn=1 panic=-1" +
+		cmdline := "console=ttyS0 vsyscall=native rodata=n oops=panic panic_on_warn=1 panic=86400" +
 			" ftrace_dump_on_oops=orig_cpu earlyprintk=serial slub_debug=UZ net.ifnames=0 biosdevname=0 "
 		if inst.cfg.Image == "9p" {
 			cmdline += "root=/dev/root rootfstype=9p rootflags=trans=virtio,version=9p2000.L,cache=loose "
