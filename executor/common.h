5d23ba9171e7afb86a7ac3a5eb89ece1c097d057
Dmitry Vyukov <dvyukov@google.com>
2016-12-16 15:36:29 +0100

    executor: don't fail on ENOMEM

--- executor/common.h
+++ executor/common.h
@@ -80,7 +80,9 @@ __attribute__((noreturn)) void fail(const char* msg, ...)
 	vfprintf(stderr, msg, args);
 	va_end(args);
 	fprintf(stderr, " (errno %d)\n", e);
-	doexit(kFailStatus);
+	// ENOMEM is frequent cause of failures in fuzzing context,
+	// so handle it here as non-fatal error.
+	doexit(e == ENOMEM ? kRetryStatus : kFailStatus);
 }
 
 #if defined(SYZ_EXECUTOR)
