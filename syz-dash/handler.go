c6619d5b5857806b1917ac673b89750d2e013b94
Dmitry Vyukov <dvyukov@google.com>
2017-06-21 14:36:28 +0200

    syz-dash: fix build

--- syz-dash/handler.go
+++ syz-dash/handler.go
@@ -6,6 +6,7 @@
 package dash
 
 import (
+	"bufio"
 	"bytes"
 	"fmt"
 	"html/template"
@@ -16,7 +17,6 @@ import (
 	"strings"
 	"time"
 
-	"github.com/google/syzkaller/pkg/kernel"
 	"appengine"
 	ds "appengine/datastore"
 	"appengine/user"
@@ -413,7 +413,7 @@ func handleBug(c appengine.Context, w http.ResponseWriter, r *http.Request) erro
 		}
 		dropCached(c)
 	case "Add patch":
-		title, diff, err := kernel.ParsePatch(r.FormValue("patch"))
+		title, diff, err := ParsePatch(r.FormValue("patch"))
 		if err != nil {
 			return fmt.Errorf("failed to parse patch: %v", err)
 		}
@@ -826,3 +826,65 @@ func formatTime(t time.Time) string {
 }
 
 var templates = template.Must(template.New("").Funcs(tmplFuncs).ParseGlob("*.html"))
+
+// kernel package depends on fileutil which depends on syscall,
+// syscall is not supported in standard appengine environment.
+// Copy ParsePatch function here for now.
+func ParsePatch(text string) (title string, diff string, err error) {
+	s := bufio.NewScanner(strings.NewReader(text))
+	parsingDiff := false
+	diffStarted := false
+	lastLine := ""
+	for s.Scan() {
+		ln := s.Text()
+		if strings.HasPrefix(ln, "--- a/") || strings.HasPrefix(ln, "--- /dev/null") {
+			parsingDiff = true
+			if title == "" {
+				title = lastLine
+			}
+		}
+		if parsingDiff {
+			if ln == "--" || ln == "-- " {
+				break
+			}
+			diff += ln + "\n"
+			continue
+		}
+		if strings.HasPrefix(ln, "diff --git") {
+			diffStarted = true
+			continue
+		}
+		if strings.HasPrefix(ln, "Subject: ") {
+			title = ln[len("Subject: "):]
+			continue
+		}
+		if ln == "" || title != "" || diffStarted {
+			continue
+		}
+		lastLine = ln
+		if strings.HasPrefix(ln, "    ") {
+			title = ln[4:]
+		}
+	}
+	if err = s.Err(); err != nil {
+		return
+	}
+	if strings.Contains(strings.ToLower(title), "[patch") {
+		pos := strings.IndexByte(title, ']')
+		if pos == -1 {
+			err = fmt.Errorf("title contains '[patch' but not ']'")
+			return
+		}
+		title = title[pos+1:]
+	}
+	title = strings.TrimSpace(title)
+	if title == "" {
+		err = fmt.Errorf("failed to extract title")
+		return
+	}
+	if diff == "" {
+		err = fmt.Errorf("failed to extract diff")
+		return
+	}
+	return
+}
