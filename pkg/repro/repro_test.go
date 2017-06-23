3b8cfbd805538752fb6732472b397105b756c706
Andrey Konovalov <andreyknvl@google.com>
2017-06-23 15:54:57 +0200

    repro: add bisection test

---
+++ pkg/repro/repro_test.go
@@ -0,0 +1,63 @@
+// Copyright 2017 syzkaller project authors. All rights reserved.
+// Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
+
+package repro
+
+import (
+	"math/rand"
+	"testing"
+	"time"
+
+	"github.com/google/syzkaller/prog"
+)
+
+func initTest(t *testing.T) (*rand.Rand, int) {
+	iters := 1000
+	if testing.Short() {
+		iters = 100
+	}
+	seed := int64(time.Now().UnixNano())
+	rs := rand.NewSource(seed)
+	t.Logf("seed=%v", seed)
+	return rand.New(rs), iters
+}
+
+func TestBisect(t *testing.T) {
+	rd, iters := initTest(t)
+	for n := 0; n < iters; n++ {
+		var progs []*prog.LogEntry
+		numTotal := rd.Intn(300)
+		numGuilty := 0
+		for i := 0; i < numTotal; i++ {
+			var prog prog.LogEntry
+			if rd.Intn(30) == 0 {
+				prog.Proc = 42
+				numGuilty += 1
+			}
+			progs = append(progs, &prog)
+		}
+		if numGuilty == 0 {
+			var prog prog.LogEntry
+			prog.Proc = 42
+			progs = append(progs, &prog)
+			numGuilty += 1
+		}
+		progs, _ = bisectProgs(progs, "test", func(p []*prog.LogEntry) (bool, error) {
+			guilty := 0
+			for _, prog := range p {
+				if prog.Proc == 42 {
+					guilty += 1
+				}
+			}
+			return guilty == numGuilty, nil
+		})
+		if len(progs) != numGuilty {
+			t.Fatalf("bisect test failed: wrong number of guilty progs: got: %v, want: %v", len(progs), numGuilty)
+		}
+		for _, prog := range progs {
+			if prog.Proc != 42 {
+				t.Fatalf("bisect test failed: wrong program is guilty: progs: %v", progs)
+			}
+		}
+	}
+}
