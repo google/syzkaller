2e2cca17c463238d418141cab72b73660d8b13a8
Dmitry Vyukov <dvyukov@google.com>
2017-06-26 13:43:20 +0200

    pkg/osutil: port to darwin

--- pkg/osutil/osutil.go
+++ pkg/osutil/osutil.go
@@ -52,9 +52,7 @@ func LongPipe() (io.ReadCloser, io.WriteCloser, error) {
 	if err != nil {
 		return nil, nil, fmt.Errorf("failed to create pipe: %v", err)
 	}
-	for sz := 128 << 10; sz <= 2<<20; sz *= 2 {
-		syscall.Syscall(syscall.SYS_FCNTL, w.Fd(), syscall.F_SETPIPE_SZ, uintptr(sz))
-	}
+	prolongPipe(r, w)
 	return r, w, err
 }
 
