34492f29c97688b15f1664408600b93d0ef759d0
Dmitry Vyukov <dvyukov@google.com>
2017-07-03 14:43:37 +0200

    pkg/osutil: don't depend on syscall in appengine build
    Dashboard app now depends on osutil through config package.
    Reshuffle functions so that the package does not depend
    on syscall in appengine build.

--- pkg/osutil/fileutil.go
+++ pkg/osutil/fileutil.go
@@ -8,9 +8,6 @@ import (
 	"io"
 	"io/ioutil"
 	"os"
-	"path/filepath"
-	"strconv"
-	"syscall"
 )
 
 // CopyFile atomically copies oldFile to newFile preserving permissions and modification time.
@@ -56,52 +53,4 @@ func WriteTempFile(data []byte) (string, error) {
 	}
 	f.Close()
 	return f.Name(), nil
-}
-
-// ProcessTempDir creates a new temp dir in where and returns its path and an unique index.
-// It also cleans up old, unused temp dirs after dead processes.
-func ProcessTempDir(where string) (string, error) {
-	lk := filepath.Join(where, "instance-lock")
-	lkf, err := syscall.Open(lk, syscall.O_RDWR|syscall.O_CREAT, DefaultFilePerm)
-	if err != nil {
-		return "", err
-	}
-	defer syscall.Close(lkf)
-	if err := syscall.Flock(lkf, syscall.LOCK_EX); err != nil {
-		return "", err
-	}
-	defer syscall.Flock(lkf, syscall.LOCK_UN)
-
-	for i := 0; i < 1e3; i++ {
-		path := filepath.Join(where, fmt.Sprintf("instance-%v", i))
-		pidfile := filepath.Join(path, ".pid")
-		err := os.Mkdir(path, DefaultDirPerm)
-		if os.IsExist(err) {
-			// Try to clean up.
-			data, err := ioutil.ReadFile(pidfile)
-			if err == nil && len(data) > 0 {
-				pid, err := strconv.Atoi(string(data))
-				if err == nil && pid > 1 {
-					if err := syscall.Kill(pid, 0); err == syscall.ESRCH {
-						if os.Remove(pidfile) == nil {
-							if os.RemoveAll(path) == nil {
-								i--
-								continue
-							}
-						}
-					}
-				}
-			}
-			// If err != nil, assume that the pid file is not created yet.
-			continue
-		}
-		if err != nil {
-			return "", err
-		}
-		if err := WriteFile(pidfile, []byte(strconv.Itoa(syscall.Getpid()))); err != nil {
-			return "", err
-		}
-		return path, nil
-	}
-	return "", fmt.Errorf("too many live instances")
 }
---
+++ pkg/osutil/osutil_unix.go
@@ -0,0 +1,92 @@
+// Copyright 2017 syzkaller project authors. All rights reserved.
+// Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
+
+// +build linux,!appengine darwin,!appengine
+
+package osutil
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"strconv"
+	"syscall"
+)
+
+// ProcessTempDir creates a new temp dir in where and returns its path and an unique index.
+// It also cleans up old, unused temp dirs after dead processes.
+func ProcessTempDir(where string) (string, error) {
+	lk := filepath.Join(where, "instance-lock")
+	lkf, err := syscall.Open(lk, syscall.O_RDWR|syscall.O_CREAT, DefaultFilePerm)
+	if err != nil {
+		return "", err
+	}
+	defer syscall.Close(lkf)
+	if err := syscall.Flock(lkf, syscall.LOCK_EX); err != nil {
+		return "", err
+	}
+	defer syscall.Flock(lkf, syscall.LOCK_UN)
+
+	for i := 0; i < 1e3; i++ {
+		path := filepath.Join(where, fmt.Sprintf("instance-%v", i))
+		pidfile := filepath.Join(path, ".pid")
+		err := os.Mkdir(path, DefaultDirPerm)
+		if os.IsExist(err) {
+			// Try to clean up.
+			data, err := ioutil.ReadFile(pidfile)
+			if err == nil && len(data) > 0 {
+				pid, err := strconv.Atoi(string(data))
+				if err == nil && pid > 1 {
+					if err := syscall.Kill(pid, 0); err == syscall.ESRCH {
+						if os.Remove(pidfile) == nil {
+							if os.RemoveAll(path) == nil {
+								i--
+								continue
+							}
+						}
+					}
+				}
+			}
+			// If err != nil, assume that the pid file is not created yet.
+			continue
+		}
+		if err != nil {
+			return "", err
+		}
+		if err := WriteFile(pidfile, []byte(strconv.Itoa(syscall.Getpid()))); err != nil {
+			return "", err
+		}
+		return path, nil
+	}
+	return "", fmt.Errorf("too many live instances")
+}
+
+// HandleInterrupts closes shutdown chan on first SIGINT
+// (expecting that the program will gracefully shutdown and exit)
+// and terminates the process on third SIGINT.
+func HandleInterrupts(shutdown chan struct{}) {
+	go func() {
+		c := make(chan os.Signal, 3)
+		signal.Notify(c, syscall.SIGINT)
+		<-c
+		close(shutdown)
+		fmt.Fprint(os.Stderr, "SIGINT: shutting down...\n")
+		<-c
+		fmt.Fprint(os.Stderr, "SIGINT: shutting down harder...\n")
+		<-c
+		fmt.Fprint(os.Stderr, "SIGINT: terminating\n")
+		os.Exit(int(syscall.SIGINT))
+	}()
+}
+
+func LongPipe() (io.ReadCloser, io.WriteCloser, error) {
+	r, w, err := os.Pipe()
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to create pipe: %v", err)
+	}
+	prolongPipe(r, w)
+	return r, w, err
+}
