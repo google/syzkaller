f93be58429d77a7928d96205e7ee34e4bbb7cc9c
Dmitry Vyukov <dvyukov@google.com>
2017-08-16 13:42:04 +0200

    sys: add some known xattr names
    Move xattr syscalls to own file and better describe xattr names.
    They usually have prefix+name structure and there are some
    known values.

--- sys/extract.sh
+++ sys/extract.sh
@@ -28,7 +28,8 @@ UPSTREAM_FILES="sys/bpf.txt sys/dri.txt sys/fuse.txt sys/input.txt sys/ipc.txt
 		sys/socket_inet_sctp.txt sys/socket_inet_dccp.txt
 		sys/socket_kcm.txt sys/socket_key.txt sys/socket_netlink.txt
 		sys/socket_netrom.txt sys/socket_nfc.txt sys/socket_unix.txt
-		sys/socket_ipx.txt sys/socket_ax25.txt sys/socket_llc.txt"
+		sys/socket_ipx.txt sys/socket_ax25.txt sys/socket_llc.txt
+		sys/xattr.txt"
 
 ANDROID_FILES="sys/tlk_device.txt sys/ion.txt"
 
