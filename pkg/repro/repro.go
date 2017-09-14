93ffdc74726154bd0e6c8937525d620e8720abf2
Dmitry Vyukov <dvyukov@google.com>
2017-09-14 09:49:26 +0200

    syz-fuzzer, syz-execprog: add -arch flag
    arch flag specifies target arch, which can be different from GOARCH.
    For example, 386 executor with amd64 fuzzer.

--- pkg/repro/repro.go
+++ pkg/repro/repro.go
@@ -537,8 +537,10 @@ func (ctx *context) testProgs(entries []*prog.LogEntry, duration time.Duration, 
 		}
 		program += "]"
 	}
-	command := fmt.Sprintf("%v -executor %v -cover=0 -procs=%v -repeat=%v -sandbox %v -threaded=%v -collide=%v %v",
-		inst.execprogBin, inst.executorBin, opts.Procs, repeat, opts.Sandbox, opts.Threaded, opts.Collide, vmProgFile)
+	command := fmt.Sprintf("%v -executor %v -arch=%v -cover=0 -procs=%v -repeat=%v"+
+		" -sandbox %v -threaded=%v -collide=%v %v",
+		inst.execprogBin, inst.executorBin, ctx.cfg.TargetArch, opts.Procs, repeat,
+		opts.Sandbox, opts.Threaded, opts.Collide, vmProgFile)
 	ctx.reproLog(2, "testing program (duration=%v, %+v): %s", duration, opts, program)
 	return ctx.testImpl(inst.Instance, command, duration)
 }
