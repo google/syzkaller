02a7a5401946658aa06fd618cf36e6848b0078fd
Dmitry Vyukov <dvyukov@google.com>
2017-10-02 16:00:18 +0200

    sys/syz-extract: factor out compilation function
    Each arch duplicates significant portion of logic
    to compile the extract source file.
    Factor this logic into a separate function and reuse
    it across all OSes.

--- sys/syz-extract/linux.go
+++ sys/syz-extract/linux.go
@@ -5,16 +5,11 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"os"
-	"os/exec"
-	"regexp"
 	"strings"
 	"time"
 
 	"github.com/google/syzkaller/pkg/compiler"
 	"github.com/google/syzkaller/pkg/osutil"
-	"github.com/google/syzkaller/sys/targets"
 )
 
 type linux struct{}
@@ -73,133 +68,40 @@ func (*linux) prepareArch(arch *Arch) error {
 }
 
 func (*linux) processFile(arch *Arch, info *compiler.ConstInfo) (map[string]uint64, map[string]bool, error) {
-	vals := info.Consts
-	includes := append(info.Includes, "asm/unistd.h")
-	bin, out, err := linuxCompile(arch.target, arch.sourceDir, arch.buildDir, nil,
-		includes, info.Incdirs, nil, nil)
-	if err != nil {
-		return nil, nil, fmt.Errorf("failed to run gcc: %v\n%v", err, string(out))
-	}
-	os.Remove(bin)
-
-	valMap := make(map[string]bool)
-	for _, val := range vals {
-		valMap[val] = true
-	}
-
-	undeclared := make(map[string]bool)
-	bin, out, err = linuxCompile(arch.target, arch.sourceDir, arch.buildDir, vals,
-		includes, info.Incdirs, info.Defines, undeclared)
-	if err != nil {
-		for _, errMsg := range []string{
-			"error: ‘([a-zA-Z0-9_]+)’ undeclared",
-			"note: in expansion of macro ‘([a-zA-Z0-9_]+)’",
-		} {
-			re := regexp.MustCompile(errMsg)
-			matches := re.FindAllSubmatch(out, -1)
-			for _, match := range matches {
-				val := string(match[1])
-				if !undeclared[val] && valMap[val] {
-					undeclared[val] = true
-				}
-			}
-		}
-		bin, out, err = linuxCompile(arch.target, arch.sourceDir, arch.buildDir, vals,
-			includes, info.Incdirs, info.Defines, undeclared)
-		if err != nil {
-			return nil, nil, fmt.Errorf("failed to run gcc: %v\n%v", err, string(out))
-		}
-	}
-	defer os.Remove(bin)
-
-	res, err := runBinaryAndParse(bin, vals, undeclared)
-	if err != nil {
-		return nil, nil, err
-	}
-	return res, undeclared, nil
-}
-
-func linuxCompile(target *targets.Target, kernelDir, buildDir string, vals, includes, incdirs []string, defines map[string]string, undeclared map[string]bool) (bin string, out []byte, err error) {
-	includeText := ""
-	for _, inc := range includes {
-		includeText += fmt.Sprintf("#include <%v>\n", inc)
-	}
-	definesText := ""
-	for k, v := range defines {
-		definesText += fmt.Sprintf("#ifndef %v\n#define %v %v\n#endif\n", k, k, v)
-	}
-	valsText := ""
-	for _, v := range vals {
-		if undeclared[v] {
-			continue
-		}
-		if valsText != "" {
-			valsText += ","
-		}
-		valsText += v
-	}
-	src := strings.Replace(linuxSrc, "[[INCLUDES]]", includeText, 1)
-	src = strings.Replace(src, "[[DEFAULTS]]", definesText, 1)
-	src = strings.Replace(src, "[[VALS]]", valsText, 1)
-	binFile, err := ioutil.TempFile("", "")
-	if err != nil {
-		return "", nil, fmt.Errorf("failed to create temp file: %v", err)
-	}
-	binFile.Close()
-
-	arch := target.KernelHeaderArch
-	args := []string{"-x", "c", "-", "-o", binFile.Name(), "-fmessage-length=0"}
-	args = append(args, target.CFlags...)
-	args = append(args, []string{
+	headerArch := arch.target.KernelHeaderArch
+	sourceDir := arch.sourceDir
+	buildDir := arch.buildDir
+	args := []string{
 		// This would be useful to ensure that we don't include any host headers,
 		// but kernel includes at least <stdarg.h>
 		// "-nostdinc",
-		"-w",
+		"-w", "-fmessage-length=0",
 		"-O3", // required to get expected values for some __builtin_constant_p
 		"-I.",
 		"-D__KERNEL__",
 		"-DKBUILD_MODNAME=\"-\"",
-		"-I" + kernelDir + "/arch/" + arch + "/include",
-		"-I" + buildDir + "/arch/" + arch + "/include/generated/uapi",
-		"-I" + buildDir + "/arch/" + arch + "/include/generated",
+		"-I" + sourceDir + "/arch/" + headerArch + "/include",
+		"-I" + buildDir + "/arch/" + headerArch + "/include/generated/uapi",
+		"-I" + buildDir + "/arch/" + headerArch + "/include/generated",
 		"-I" + buildDir + "/include",
-		"-I" + kernelDir + "/include",
-		"-I" + kernelDir + "/arch/" + arch + "/include/uapi",
-		"-I" + buildDir + "/arch/" + arch + "/include/generated/uapi",
-		"-I" + kernelDir + "/include/uapi",
+		"-I" + sourceDir + "/include",
+		"-I" + sourceDir + "/arch/" + headerArch + "/include/uapi",
+		"-I" + buildDir + "/arch/" + headerArch + "/include/generated/uapi",
+		"-I" + sourceDir + "/include/uapi",
 		"-I" + buildDir + "/include/generated/uapi",
-		"-I" + kernelDir,
-		"-include", kernelDir + "/include/linux/kconfig.h",
-	}...)
-	for _, incdir := range incdirs {
-		args = append(args, "-I"+kernelDir+"/"+incdir)
+		"-I" + sourceDir,
+		"-include", sourceDir + "/include/linux/kconfig.h",
 	}
-	cmd := exec.Command("gcc", args...)
-	cmd.Stdin = strings.NewReader(src)
-	out, err = cmd.CombinedOutput()
-	if err != nil {
-		os.Remove(binFile.Name())
-		return "", out, err
+	args = append(args, arch.target.CFlags...)
+	for _, incdir := range info.Incdirs {
+		args = append(args, "-I"+sourceDir+"/"+incdir)
 	}
-	return binFile.Name(), nil, nil
-}
-
-var linuxSrc = `
-[[INCLUDES]]
-[[DEFAULTS]]
-int printf(const char *format, ...);
+	const addSource = `
+#include <asm/unistd.h>
 unsigned long phys_base;
 #ifndef __phys_addr
 unsigned long __phys_addr(unsigned long addr) { return 0; }
 #endif
-int main() {
-	int i;
-	unsigned long long vals[] = {[[VALS]]};
-	for (i = 0; i < sizeof(vals)/sizeof(vals[0]); i++) {
-		if (i != 0)
-			printf(" ");
-		printf("%llu", vals[i]);
-	}
-	return 0;
-}
 `
+	return extract(info, "gcc", args, addSource)
+}
