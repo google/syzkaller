817b6773969a061755952e325967ce5125d5c605
Dmitry Vyukov <dvyukov@google.com>
2017-10-10 15:46:59 +0200

    syz-manager/mgrconfig: add gce examples

--- syz-manager/mgrconfig/mgrconfig_test.go
+++ syz-manager/mgrconfig/mgrconfig_test.go
@@ -4,25 +4,37 @@
 package mgrconfig
 
 import (
+	"path/filepath"
 	"testing"
 
 	"github.com/google/syzkaller/pkg/config"
+	"github.com/google/syzkaller/vm/gce"
 	"github.com/google/syzkaller/vm/qemu"
 )
 
 func TestCanned(t *testing.T) {
-	cfg := new(Config)
-	if err := config.LoadFile("testdata/qemu.cfg", cfg); err != nil {
-		t.Fatal(err)
+	files, err := filepath.Glob(filepath.Join("testdata", "*.cfg"))
+	if err != nil || len(files) == 0 {
+		t.Fatalf("failed to read input files: %v", err)
 	}
-	var vmCfg interface{}
-	switch cfg.Type {
-	case "qemu":
-		vmCfg = new(qemu.Config)
-	default:
-		t.Fatalf("unknown VM type: %v", cfg.Type)
-	}
-	if err := config.LoadData(cfg.VM, vmCfg); err != nil {
-		t.Fatalf("failed to load %v config: %v", cfg.Type, err)
+	for _, file := range files {
+		t.Run(file, func(t *testing.T) {
+			cfg := new(Config)
+			if err := config.LoadFile(file, cfg); err != nil {
+				t.Fatal(err)
+			}
+			var vmCfg interface{}
+			switch cfg.Type {
+			case "qemu":
+				vmCfg = new(qemu.Config)
+			case "gce":
+				vmCfg = new(gce.Config)
+			default:
+				t.Fatalf("unknown VM type: %v", cfg.Type)
+			}
+			if err := config.LoadData(cfg.VM, vmCfg); err != nil {
+				t.Fatalf("failed to load %v config: %v", cfg.Type, err)
+			}
+		})
 	}
 }
