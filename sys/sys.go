3f955eba7f6eee3d98f78d3398863f3d922a0b35
Utkarsh Anand <uanand009@gmail.com>
2017-10-25 12:57:47 +0530

    Lots of changes to sys/netbsd (#397)
    * Lots of changes to sys/netbsd:
    - Removed a few syscalls that did not have proper constants defined.
    - Autogenerated *.const files.
    - Removed a few types like uid and gid, that were not available.
    - Ran make generate
    
    * Few changes for NetBSD support:
    - Added sys/netbsd/init.go
    - Added netbsd to sys/sys.go
    
    * Fix order in sys/sys.go
    
    * Update documentation for NetBSD

--- sys/sys.go
+++ sys/sys.go
@@ -8,6 +8,7 @@ import (
 	_ "github.com/google/syzkaller/sys/freebsd"
 	_ "github.com/google/syzkaller/sys/fuchsia"
 	_ "github.com/google/syzkaller/sys/linux"
+	_ "github.com/google/syzkaller/sys/netbsd"
 	_ "github.com/google/syzkaller/sys/windows"
 )
 
