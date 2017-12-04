7a5e495bdf4d204bdd696f1e51422661e58ace9c
Dmitry Vyukov <dvyukov@google.com>
2017-12-04 11:04:01 +0100

    pkg/report: fix out-of-bounds access
    Update #457

--- pkg/report/linux.go
+++ pkg/report/linux.go
@@ -401,7 +401,7 @@ func (ctx *linux) isCorrupted(title string, report []byte, format oopsFormat) bo
 		frames = frames[1:]
 		corrupted := true
 		// Check that at least one of the next 10 lines contains a frame.
-		for i := 0; i < 10; i++ {
+		for i := 0; i < 10 && i < len(frames); i++ {
 			if bytes.Contains(frames[i], []byte("(stack is not available)")) || linuxSymbolizeRe.Match(frames[i]) {
 				corrupted = false
 				break
