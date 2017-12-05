b5e7b1e4f994f753aa898082238b9edd910fab8a
Dmitry Vyukov <dvyukov@google.com>
2017-12-05 13:55:23 +0100

    vm/gce: always ignore serial relay errors
    GCE serial reply seems to be buggy, we see lots of "serialport: VM disconnected"
    and "packet_write_wait: Connection to 1.2.3.4 port 9600: Broken pipe"
    errors, which do not have any explanation.
    Ignore all serial relay errors.

--- vm/vmimpl/merger_test.go
+++ vm/vmimpl/merger_test.go
@@ -5,6 +5,7 @@ package vmimpl
 
 import (
 	"bytes"
+	"io"
 	"testing"
 	"time"
 
@@ -61,8 +62,10 @@ func TestMerger(t *testing.T) {
 		t.Fatalf("bad line: '%s', want '%s'", got, want)
 	}
 
-	if err := <-merger.Err; err == nil || err.Error() != "failed to read from pipe1: EOF" {
-		t.Fatalf("merger did not produce io.EOF: %v", err)
+	if err := <-merger.Err; err == nil {
+		t.Fatalf("merger did not produce an error on pipe close")
+	} else if merr := err.(MergerError); merr.Name != "pipe1" || merr.R != rp1 || merr.Err != io.EOF {
+		t.Fatalf("merger produced wrong error: %v", err)
 	}
 
 	wp2.Close()
