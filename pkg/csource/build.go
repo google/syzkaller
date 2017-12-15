dafdaaf578ff3cff62a3d64c58b7a41c3bcf9887
Dmitry Vyukov <dvyukov@google.com>
2017-12-15 11:25:19 +0100

    pkg/csource: refactor
    csource.go is too large and messy.
    Move Build/Format into buid.go.
    Move generation of common header into common.go.
    Split generation of common header into smaller managable functions.

---
+++ pkg/csource/build.go
@@ -0,0 +1,86 @@
+// Copyright 2017 syzkaller project authors. All rights reserved.
+// Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
+
+package csource
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+
+	"github.com/google/syzkaller/pkg/osutil"
+	"github.com/google/syzkaller/prog"
+	"github.com/google/syzkaller/sys/targets"
+)
+
+// Build builds a C/C++ program from source src and returns name of the resulting binary.
+// lang can be "c" or "c++".
+func Build(target *prog.Target, lang, src string) (string, error) {
+	bin, err := ioutil.TempFile("", "syzkaller")
+	if err != nil {
+		return "", fmt.Errorf("failed to create temp file: %v", err)
+	}
+	bin.Close()
+	sysTarget := targets.List[target.OS][target.Arch]
+	compiler := sysTarget.CCompilerPrefix + "gcc"
+	if _, err := exec.LookPath(compiler); err != nil {
+		return "", NoCompilerErr
+	}
+	flags := []string{
+		"-x", lang, "-Wall", "-Werror", "-O1", "-g", "-o", bin.Name(),
+		src, "-pthread",
+	}
+	flags = append(flags, sysTarget.CrossCFlags...)
+	if sysTarget.PtrSize == 4 {
+		// We do generate uint64's for syscall arguments that overflow longs on 32-bit archs.
+		flags = append(flags, "-Wno-overflow")
+	}
+	out, err := osutil.Command(compiler, append(flags, "-static")...).CombinedOutput()
+	if err != nil {
+		// Some distributions don't have static libraries.
+		out, err = osutil.Command(compiler, flags...).CombinedOutput()
+	}
+	if err != nil {
+		os.Remove(bin.Name())
+		data, _ := ioutil.ReadFile(src)
+		return "", fmt.Errorf("failed to build program:\n%s\n%s\ncompiler invocation: %v %v\n",
+			data, out, compiler, flags)
+	}
+	return bin.Name(), nil
+}
+
+var NoCompilerErr = errors.New("no target compiler")
+
+// Format reformats C source using clang-format.
+func Format(src []byte) ([]byte, error) {
+	stdout, stderr := new(bytes.Buffer), new(bytes.Buffer)
+	cmd := osutil.Command("clang-format", "-assume-filename=/src.c", "-style", style)
+	cmd.Stdin = bytes.NewReader(src)
+	cmd.Stdout = stdout
+	cmd.Stderr = stderr
+	if err := cmd.Run(); err != nil {
+		return src, fmt.Errorf("failed to format source: %v\n%v", err, stderr.String())
+	}
+	return stdout.Bytes(), nil
+}
+
+// Something acceptable for kernel developers and email-friendly.
+var style = `{
+BasedOnStyle: LLVM,
+IndentWidth: 2,
+UseTab: Never,
+BreakBeforeBraces: Linux,
+IndentCaseLabels: false,
+DerivePointerAlignment: false,
+PointerAlignment: Left,
+AlignTrailingComments: true,
+AllowShortBlocksOnASingleLine: false,
+AllowShortCaseLabelsOnASingleLine: false,
+AllowShortFunctionsOnASingleLine: false,
+AllowShortIfStatementsOnASingleLine: false,
+AllowShortLoopsOnASingleLine: false,
+ColumnLimit: 72,
+}`
---
+++ pkg/csource/common.go
@@ -0,0 +1,123 @@
+// Copyright 2017 syzkaller project authors. All rights reserved.
+// Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
+
+package csource
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+
+	"github.com/google/syzkaller/pkg/osutil"
+	"github.com/google/syzkaller/prog"
+	"github.com/google/syzkaller/sys/targets"
+)
+
+func createCommonHeader(p *prog.Prog, opts Options) ([]byte, error) {
+	commonHeader, err := getCommonHeader(p.Target.OS)
+	if err != nil {
+		return nil, err
+	}
+	defines, err := defineList(p, opts)
+	if err != nil {
+		return nil, err
+	}
+
+	cmd := osutil.Command("cpp", "-nostdinc", "-undef", "-fdirectives-only", "-dDI", "-E", "-P", "-")
+	for _, def := range defines {
+		cmd.Args = append(cmd.Args, "-D"+def)
+	}
+	cmd.Stdin = strings.NewReader(commonHeader)
+	stderr := new(bytes.Buffer)
+	stdout := new(bytes.Buffer)
+	cmd.Stderr = stderr
+	cmd.Stdout = stdout
+	if err := cmd.Run(); len(stdout.Bytes()) == 0 {
+		return nil, fmt.Errorf("cpp failed: %v\n%v\n%v\n", err, stdout.String(), stderr.String())
+	}
+
+	src, err := removeSystemDefines(stdout.Bytes(), defines)
+	if err != nil {
+		return nil, err
+	}
+	return src, nil
+}
+
+func defineList(p *prog.Prog, opts Options) ([]string, error) {
+	var defines []string
+	if prog.RequiresBitmasks(p) {
+		defines = append(defines, "SYZ_USE_BITMASKS")
+	}
+	if prog.RequiresChecksums(p) {
+		defines = append(defines, "SYZ_USE_CHECKSUMS")
+	}
+	switch opts.Sandbox {
+	case "":
+		// No sandbox, do nothing.
+	case "none":
+		defines = append(defines, "SYZ_SANDBOX_NONE")
+	case "setuid":
+		defines = append(defines, "SYZ_SANDBOX_SETUID")
+	case "namespace":
+		defines = append(defines, "SYZ_SANDBOX_NAMESPACE")
+	default:
+		return nil, fmt.Errorf("unknown sandbox mode: %v", opts.Sandbox)
+	}
+	if opts.Threaded {
+		defines = append(defines, "SYZ_THREADED")
+	}
+	if opts.Collide {
+		defines = append(defines, "SYZ_COLLIDE")
+	}
+	if opts.Repeat {
+		defines = append(defines, "SYZ_REPEAT")
+	}
+	if opts.Fault {
+		defines = append(defines, "SYZ_FAULT_INJECTION")
+	}
+	if opts.EnableTun {
+		defines = append(defines, "SYZ_TUN_ENABLE")
+	}
+	if opts.UseTmpDir {
+		defines = append(defines, "SYZ_USE_TMP_DIR")
+	}
+	if opts.HandleSegv {
+		defines = append(defines, "SYZ_HANDLE_SEGV")
+	}
+	if opts.WaitRepeat {
+		defines = append(defines, "SYZ_WAIT_REPEAT")
+	}
+	if opts.Debug {
+		defines = append(defines, "SYZ_DEBUG")
+	}
+	for _, c := range p.Calls {
+		defines = append(defines, "__NR_"+c.Meta.CallName)
+	}
+	defines = append(defines, targets.List[p.Target.OS][p.Target.Arch].CArch...)
+	return defines, nil
+}
+
+func removeSystemDefines(src []byte, defines []string) ([]byte, error) {
+	remove := append(defines, []string{
+		"__STDC__",
+		"__STDC_HOSTED__",
+		"__STDC_UTF_16__",
+		"__STDC_UTF_32__",
+	}...)
+	for _, def := range remove {
+		src = bytes.Replace(src, []byte("#define "+def+" 1\n"), nil, -1)
+	}
+	// strip: #define __STDC_VERSION__ 201112L
+	for _, def := range []string{"__STDC_VERSION__"} {
+		pos := bytes.Index(src, []byte("#define "+def))
+		if pos == -1 {
+			continue
+		}
+		end := bytes.IndexByte(src[pos:], '\n')
+		if end == -1 {
+			continue
+		}
+		src = bytes.Replace(src, src[pos:end+1], nil, -1)
+	}
+	return src, nil
+}
