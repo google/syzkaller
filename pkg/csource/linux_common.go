ad02789395f58bac29bbfa4a1d279aa5d0a02442
Dmitry Vyukov <dvyukov@google.com>
2018-04-20 19:06:39 +0200

    executor: don't fail when ipv6 is not enabled
    Check in checkpoint_iptables is not enough as
    reset_iptables will fail even if no tables enabled.
    Add similar check to reset_iptables.

--- pkg/csource/linux_common.go
+++ pkg/csource/linux_common.go
@@ -2574,8 +2574,14 @@ static void reset_iptables(struct ipt_table_desc* tables, int num_tables, int fa
 	int fd, i;
 
 	fd = socket(family, SOCK_STREAM, IPPROTO_TCP);
-	if (fd == -1)
+	if (fd == -1) {
+		switch (errno) {
+		case EAFNOSUPPORT:
+		case ENOPROTOOPT:
+			return;
+		}
 		fail("socket(%d, SOCK_STREAM, IPPROTO_TCP)", family);
+	}
 	for (i = 0; i < num_tables; i++) {
 		struct ipt_table_desc* table = &tables[i];
 		if (table->info.valid_hooks == 0)
