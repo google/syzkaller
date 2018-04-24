02bbd80e7c322d5a5641d3c45fc5803f18cdf5e6
Dmitry Vyukov <dvyukov@google.com>
2018-04-24 13:22:19 +0200

    prog: fix gometalinter warnings

--- prog/parse_test.go
+++ prog/parse_test.go
@@ -47,7 +47,7 @@ func TestParseMulti(t *testing.T) {
 	entries := target.ParseLog([]byte(execLog))
 	if len(entries) != 5 {
 		for i, ent := range entries {
-			t.Logf("program #%v: %s\n", i, ent.P)
+			t.Logf("program #%v: %v\n", i, ent.P)
 		}
 		t.Fatalf("got %v programs, want 5", len(entries))
 	}
