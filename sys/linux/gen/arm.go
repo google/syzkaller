9148078645e12dfcd0bebdca47f74163c589f613
Denis Efremov <efremov@linux.com>
2018-05-29 15:55:15 +0300

    sys/linux: video4linux v4l2_jpegcompression APP_len fix
    Signed-off-by: Denis Efremov <efremov@linux.com>

--- sys/linux/gen/arm.go
+++ sys/linux/gen/arm.go
@@ -14419,18 +14419,18 @@ var structDescs_arm = []*KeyedStruct{
 	{Key: StructKey{Name: "v4l2_jpegcompression"}, Desc: &StructDesc{TypeCommon: TypeCommon{TypeName: "v4l2_jpegcompression", TypeSize: 140}, Fields: []Type{
 		&IntType{IntTypeCommon: IntTypeCommon{TypeCommon: TypeCommon{TypeName: "int32", FldName: "quality", TypeSize: 4}}},
 		&IntType{IntTypeCommon: IntTypeCommon{TypeCommon: TypeCommon{TypeName: "int32", FldName: "APPn", TypeSize: 4}}, Kind: 2, RangeEnd: 15},
-		&LenType{IntTypeCommon: IntTypeCommon{TypeCommon: TypeCommon{TypeName: "len", FldName: "APP_len", TypeSize: 4}}, Buf: "APPn"},
+		&IntType{IntTypeCommon: IntTypeCommon{TypeCommon: TypeCommon{TypeName: "int32", FldName: "APP_len", TypeSize: 4}}, Kind: 2, RangeEnd: 60},
 		&BufferType{TypeCommon: TypeCommon{TypeName: "array", FldName: "APP_data", TypeSize: 60}, Kind: 1, RangeBegin: 60, RangeEnd: 60},
-		&LenType{IntTypeCommon: IntTypeCommon{TypeCommon: TypeCommon{TypeName: "len", FldName: "COM_len", TypeSize: 4}}, Buf: "COM_data"},
+		&IntType{IntTypeCommon: IntTypeCommon{TypeCommon: TypeCommon{TypeName: "int32", FldName: "COM_len", TypeSize: 4}}, Kind: 2, RangeEnd: 60},
 		&BufferType{TypeCommon: TypeCommon{TypeName: "array", FldName: "COM_data", TypeSize: 60}, Kind: 1, RangeBegin: 60, RangeEnd: 60},
 		&FlagsType{IntTypeCommon: IntTypeCommon{TypeCommon: TypeCommon{TypeName: "v4l2_jpeg_markers", FldName: "jpeg_markers", TypeSize: 4}}, Vals: []uint64{8, 16, 32, 64, 128}},
 	}}},
 	{Key: StructKey{Name: "v4l2_jpegcompression", Dir: 1}, Desc: &StructDesc{TypeCommon: TypeCommon{TypeName: "v4l2_jpegcompression", TypeSize: 140, ArgDir: 1}, Fields: []Type{
 		&IntType{IntTypeCommon: IntTypeCommon{TypeCommon: TypeCommon{TypeName: "int32", FldName: "quality", TypeSize: 4, ArgDir: 1}}},
 		&IntType{IntTypeCommon: IntTypeCommon{TypeCommon: TypeCommon{TypeName: "int32", FldName: "APPn", TypeSize: 4, ArgDir: 1}}, Kind: 2, RangeEnd: 15},
-		&LenType{IntTypeCommon: IntTypeCommon{TypeCommon: TypeCommon{TypeName: "len", FldName: "APP_len", TypeSize: 4, ArgDir: 1}}, Buf: "APPn"},
+		&IntType{IntTypeCommon: IntTypeCommon{TypeCommon: TypeCommon{TypeName: "int32", FldName: "APP_len", TypeSize: 4, ArgDir: 1}}, Kind: 2, RangeEnd: 60},
 		&BufferType{TypeCommon: TypeCommon{TypeName: "array", FldName: "APP_data", TypeSize: 60, ArgDir: 1}, Kind: 1, RangeBegin: 60, RangeEnd: 60},
-		&LenType{IntTypeCommon: IntTypeCommon{TypeCommon: TypeCommon{TypeName: "len", FldName: "COM_len", TypeSize: 4, ArgDir: 1}}, Buf: "COM_data"},
+		&IntType{IntTypeCommon: IntTypeCommon{TypeCommon: TypeCommon{TypeName: "int32", FldName: "COM_len", TypeSize: 4, ArgDir: 1}}, Kind: 2, RangeEnd: 60},
 		&BufferType{TypeCommon: TypeCommon{TypeName: "array", FldName: "COM_data", TypeSize: 60, ArgDir: 1}, Kind: 1, RangeBegin: 60, RangeEnd: 60},
 		&FlagsType{IntTypeCommon: IntTypeCommon{TypeCommon: TypeCommon{TypeName: "v4l2_jpeg_markers", FldName: "jpeg_markers", TypeSize: 4, ArgDir: 1}}, Vals: []uint64{8, 16, 32, 64, 128}},
 	}}},
@@ -33505,4 +33505,4 @@ var consts_arm = []ConstValue{
 	{Name: "bpf_insn_load_imm_dw", Value: 24},
 }
 
-const revision_arm = "abd87ec72ff685b28bc4cbb853278942ce7f1866"
+const revision_arm = "b19718035c8f145e2ecd9c2d12fafc6554ed7a45"
