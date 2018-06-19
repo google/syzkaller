b26353c3d5797059d2e618f203022bea4e7490e6
Dmitry Vyukov <dvyukov@google.com>
2018-06-19 13:06:02 +0200

    dashboard/app: fix table sorting on firefox
    Under Firefox 60 browser, sort operation does not work and
    
      ReferenceError: event is not defined
    
    error message is printed in the Web Console window.
    Let's explicitly pass an object reference to the sortTable function.
    
    Credit goes to Tetsuo Handa.

--- dashboard/app/static/common.js
+++ dashboard/app/static/common.js
@@ -1,8 +1,8 @@
 // Copyright 2018 syzkaller project authors. All rights reserved.
 // Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
 
-function sortTable(colName, conv) {
-	table = event.srcElement.parentNode.parentNode.parentNode;
+function sortTable(item, colName, conv) {
+	table = item.parentNode.parentNode.parentNode;
 	rows = table.getElementsByTagName("tr");
 	col = findColumnByName(rows[0].getElementsByTagName("th"), colName);
 	values = new Array;
