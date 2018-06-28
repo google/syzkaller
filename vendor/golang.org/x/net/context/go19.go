7b4e273e539d93682dc87e9a7c7d1f79a4cd1b77
Dmitry Vyukov <dvyukov@google.com>
2018-06-28 09:59:11 +0200

    vendor: add github.com/ianlancetaylor/demangle
    Add the new package required by pkg/report/fuchsia.go.
    Also update everything else because there does not seem
    to be a way to add a package without a wholesale update.

--- vendor/golang.org/x/net/context/go19.go
+++ vendor/golang.org/x/net/context/go19.go
@@ -2,12 +2,10 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// This is broken, see https://github.com/golang/go/issues/21337
-// +build
+// +build go1.9
 
 package context
 
-/*
 import "context" // standard library's context, as of Go 1.7
 
 // A Context carries a deadline, a cancelation signal, and other values across
@@ -20,4 +18,3 @@ type Context = context.Context
 // A CancelFunc does not wait for the work to stop.
 // After the first call, subsequent calls to a CancelFunc do nothing.
 type CancelFunc = context.CancelFunc
-*/
