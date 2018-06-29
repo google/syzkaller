0a971ab1d087800b55cd2bf7daed44ab323ff900
Dmitry Vyukov <dvyukov@google.com>
2018-06-29 18:15:41 +0200

    syz-manager: implement fallback coverage report

--- pkg/signal/signal.go
+++ pkg/signal/signal.go
@@ -191,3 +191,11 @@ func Minimize(corpus []Context) []interface{} {
 	}
 	return result
 }
+
+func EncodeFallback(id, errno int) uint32 {
+	return uint32(id)<<16 | uint32(errno)&0x3ff
+}
+
+func DecodeFallback(s uint32) (int, int) {
+	return int(s >> 16), int(s & 0x3ff)
+}
