2f6b7afe65f4839e900b3663ba942a07f9d8ef7c
Dmitry Vyukov <dvyukov@google.com>
2018-06-30 17:28:48 +0200

    executor: include more headers on fuchsia
    Since we are taking address of functions in syscall table,
    we need all headers even if we don't use them directly.

--- executor/common_fuchsia.h
+++ executor/common_fuchsia.h
@@ -11,6 +11,8 @@
 #include <poll.h>
 #include <signal.h>
 #include <sys/file.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
 #include <sys/stat.h>
 #include <sys/time.h>
 #include <sys/types.h>
