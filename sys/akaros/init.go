df5ae5f537c164da6feb31c645aa607d0c017ba5
Dmitry Vyukov <dvyukov@google.com>
2018-07-16 17:01:29 +0200

    sys/akaros: don't call provision(-1)
    It causes kernel debug splat,
    which has high chances of corrupting kernel crashes.

--- sys/akaros/init.go
+++ sys/akaros/init.go
@@ -13,6 +13,25 @@ func init() {
 	prog.RegisterTarget(gen.Target_amd64, initTarget)
 }
 
+type arch struct {
+	unix *targets.UnixSanitizer
+}
+
 func initTarget(target *prog.Target) {
+	arch := &arch{
+		unix: targets.MakeUnixSanitizer(target),
+	}
 	target.MakeMmap = targets.MakePosixMmap(target)
+	target.SanitizeCall = arch.sanitizeCall
+}
+
+func (arch *arch) sanitizeCall(c *prog.Call) {
+	arch.unix.SanitizeCall(c)
+	switch c.Meta.CallName {
+	case "provision":
+		if pid, ok := c.Args[0].(*prog.ConstArg); ok && uint32(pid.Val) == ^uint32(0) {
+			// pid -1 causes some debugging splat on console.
+			pid.Val = 0
+		}
+	}
 }
