e7689d4ef45ffe0eb357346e0e66b729050467e6
Anton Lindqvist <anton.lindqvist@gmail.com>
2018-07-24 20:38:48 +0200

    pkg/csource: remove /bin/bash assumption
    OpenBSD and probably other BSDs does not ship with /bin/bash. This particular
    script runs fine with regular /bin/sh.

--- pkg/csource/gen.sh
+++ pkg/csource/gen.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 # Copyright 2018 syzkaller project authors. All rights reserved.
 # Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
 
