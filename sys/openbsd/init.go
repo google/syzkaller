fb8086083a81ac57bceac3566f990079bcfa1614
Anton Lindqvist <anton.lindqvist@gmail.com>
2018-08-28 09:47:17 +0200

    fixup! openbsd: add mandatory bits

--- sys/openbsd/init.go
+++ sys/openbsd/init.go
@@ -8,73 +8,42 @@ import (
 	"github.com/google/syzkaller/sys/targets"
 )
 
-type arch struct {
-	MAP_ANONYMOUS uint64
-	MAP_PRIVATE   uint64
-	MAP_FIXED     uint64
-	PROT_READ     uint64
-	PROT_WRITE    uint64
-	S_IFMT        uint64
-	S_IFCHR       uint64
-	S_IFBLK       uint64
-	S_IFIFO       uint64
-}
-
 func InitTarget(target *prog.Target) {
 	arch := &arch{
-		MAP_ANONYMOUS: target.ConstMap["MAP_ANON"],
-		MAP_FIXED:     target.ConstMap["MAP_FIXED"],
-		MAP_PRIVATE:   target.ConstMap["MAP_PRIVATE"],
-		PROT_READ:     target.ConstMap["PROT_READ"],
-		PROT_WRITE:    target.ConstMap["PROT_WRITE"],
-		S_IFBLK:       target.ConstMap["S_IFBLK"],
-		S_IFCHR:       target.ConstMap["S_IFCHR"],
-		S_IFIFO:       target.ConstMap["S_IFIFO"],
-		S_IFMT:        target.ConstMap["S_IFMT"],
+		unix: targets.MakeUnixSanitizer(target),
 	}
 
 	target.MakeMmap = targets.MakePosixMmap(target)
-	target.SanitizeCall = arch.sanitizeCall
+	target.SanitizeCall = arch.SanitizeCall
 }
 
-func (arch *arch) sanitizeCall(c *prog.Call) {
-	mknodModeIdx := -1
+type arch struct {
+	unix *targets.UnixSanitizer
+}
+
+func (arch *arch) SanitizeCall(c *prog.Call) {
+	arch.unix.SanitizeCall(c)
+
+	// Prevent vnodes of type VBAD from being created. Such vnodes will
+	// likely trigger assertion errors by the kernel.
+	pos := 1
 	switch c.Meta.CallName {
-	case "mmap":
-		// Add MAP_FIXED flag, otherwise it produces non-deterministic results.
-		c.Args[3].(*prog.ConstArg).Val |= arch.MAP_FIXED
-	case "mknod", "mknod$loop":
-		mknodModeIdx = 1
 	case "mknodat":
-		mknodModeIdx = 2
-	case "exit":
-		code := c.Args[0].(*prog.ConstArg)
-		// These codes are reserved by executor.
-		if code.Val%128 == 67 || code.Val%128 == 68 {
-			code.Val = 1
+		pos = 2
+		fallthrough
+	case "mknod":
+		mode := c.Args[pos].(*prog.ConstArg)
+		if (mode.Val & arch.unix.S_IFMT) != arch.unix.S_IFMT {
+			return
 		}
-	}
-	if mknodModeIdx != -1 {
-		val := c.Args[mknodModeIdx].(*prog.ConstArg).Val
-		c.Args[mknodModeIdx].(*prog.ConstArg).Val = arch.sanitizeMknodMode(val)
-	}
-}
-
-// Sanitize the mode argument passed to mknod in order to prevent vnodes of type
-// VBAD from being created. Such vnodes will likely trigger assertion errors by
-// the kernel.
-func (arch *arch) sanitizeMknodMode(mode uint64) uint64 {
-	saneMode := mode & ^arch.S_IFMT
-	switch {
-	case (mode & arch.S_IFMT) != arch.S_IFMT:
-		return mode
-	case (mode & arch.S_IFCHR) == arch.S_IFCHR:
-		return saneMode | arch.S_IFCHR
-	case (mode & arch.S_IFBLK) == arch.S_IFBLK:
-		return saneMode | arch.S_IFBLK
-	case (mode & arch.S_IFIFO) == arch.S_IFIFO:
-		return saneMode | arch.S_IFIFO
-	default:
-		return saneMode
+		saneMode := mode.Val & ^arch.unix.S_IFMT
+		switch {
+		case (mode.Val & arch.unix.S_IFCHR) == arch.unix.S_IFCHR:
+			mode.Val = saneMode | arch.unix.S_IFCHR
+		case (mode.Val & arch.unix.S_IFBLK) == arch.unix.S_IFBLK:
+			mode.Val = saneMode | arch.unix.S_IFBLK
+		case (mode.Val & arch.unix.S_IFIFO) == arch.unix.S_IFIFO:
+			mode.Val = saneMode | arch.unix.S_IFIFO
+		}
 	}
 }
