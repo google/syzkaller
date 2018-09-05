bfffcc791c10a25910e2607bcbe0eef9adb7265b
Dokyung Song <dokyungs@google.com>
2018-09-05 12:45:17 -0700

    sys/fuchsia: add Go script that generates fidl descriptions

--- sys/fuchsia/init.go
+++ sys/fuchsia/init.go
@@ -1,6 +1,8 @@
 // Copyright 2017 syzkaller project authors. All rights reserved.
 // Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
 
+//go:generate go run fidlgen/main.go
+
 package fuchsia
 
 import (
