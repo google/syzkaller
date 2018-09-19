932ecbc2f7a560d01bb196293a3310264e31fce9
Greg Steuck <gnezdo@google.com>
2018-09-19 16:28:02 -0700

    tools/*openbsd*: smaller VMM images and tweaks.

--- tools/create-openbsd-vmm-worker.sh
+++ tools/create-openbsd-vmm-worker.sh
@@ -4,7 +4,7 @@
 # Use of this source code is governed by Apache 2 LICENSE that can be found in
 # the LICENSE file.
 
-# Produces GCE image of syz-ci running on OpenBSD.
+# Produces a very minimal image for running syzkaller fuzzers running on OpenBSD.
 
 # Mostly derived from Go buildlet generator with blessing from bradfitz@.
 
@@ -27,20 +27,18 @@ fi
 
 # Create custom siteXX.tgz set.
 mkdir -p etc
-cat >install.site <<EOF
+cat >install.site <<'EOF'
 #!/bin/sh
-syspatch
-pkg_add -I bash git gmake go llvm nano wget && echo pkg_add OK
-
 echo 'set tty com0' > boot.conf
 echo 'PasswordAuthentication no' >> /etc/ssh/sshd_config
 
-mkdir /syzkaller
-echo '/dev/sd1a /syzkaller ffs rw,noauto 1 0' >> /etc/fstab
+rm /usr/libexec/reorder_kernel
+ln -s /usr/bin/true /usr/libexec/reorder_kernel
+rm -fr /usr/share/relink
 
-rm -rf /usr/{src,ports}
-ln -s /syzkaller/src /usr/src
-ln -s /syzkaller/ports /usr/ports
+perl -i.bak -pne 's/^(ttyC.*)vt220.*/$1unknown off/' /etc/ttys
+
+touch root/.hushlogin home/syzkaller/.hushlogin
 EOF
 
 cat >etc/installurl <<EOF
@@ -51,62 +49,58 @@ cat >etc/rc.local <<EOF
 (
   set -x
 
-  echo "starting syz-ci"
-  fsck -y /dev/sd1a
-  mount /syzkaller
+  echo "starting syz-manager"
 )
 EOF
 chmod +x install.site
 
 cat >etc/rc.conf.local <<EOF
+cron_flags=NO
+pflogd_flags=NO
+library_aslr=NO
 slaacd_flags=NO
 smtpd_flags=NO
 sndiod_flags=NO
-vmd_flags=
+pf=NO
+ntpd_flags=NO
 EOF
 
-cat >etc/vm.conf <<EOF
-vm "syzkaller" {
-  disable
-  disk "/syzkaller/syzkaller.img"
-  local interface
-  owner syzkaller
-  allow instance { boot, disk, memory }
-}
-EOF
+# Generate the worker keys.
+rm -f worker_key*
+ssh-keygen -t ed25519 -N '' -f worker_key -C worker_key
 
 tar --owner=root --group=root -zcvf site${RELNO}.tgz install.site etc/*
 
 # Autoinstall script.
 cat >auto_install.conf <<EOF
-System hostname = ci-openbsd
+System hostname = worker
 DNS domain name = syzkaller
 Which network interface = vio0
 IPv4 address for vio0 = dhcp
 IPv6 address for vio0 = none
 Password for root account = root
-Public ssh key for root account = ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBJeLpmSq+Dgrk01Ht+QtY0GDsY2gcwhT12SqizmacAF67TCa0n8OcpjVOpiiurb01Aa5lcl2WbakUFYmsp1U1l8=
+Public ssh key for root account = $(cat worker_key.pub)
 Do you expect to run the X Window System = no
 Change the default console to com0 = yes
 Which speed should com0 use = 115200
 Setup a user = syzkaller
 Full name for user syzkaller = Syz Kaller
 Password for user syzkaller = syzkaller
-Public ssh key for user syzkaller = ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBJeLpmSq+Dgrk01Ht+QtY0GDsY2gcwhT12SqizmacAF67TCa0n8OcpjVOpiiurb01Aa5lcl2WbakUFYmsp1U1l8=
+Public ssh key for user syzkaller = $(cat worker_key.pub)
 Allow root ssh login = prohibit-password
 What timezone = US/Pacific
 Which disk = sd0
 Use (W)hole disk or (E)dit the MBR = whole
 Use (A)uto layout, (E)dit auto layout, or create (C)ustom layout = auto
 URL to autopartitioning template for disklabel = file://disklabel.template
-Set name(s) = +* -x* -game* done
+Set name(s) = -* +bsd +bsd.mp +base* +site* done
 Directory does not contain SHA256.sig. Continue without verification = yes
 EOF
 
 # Disklabel template.
 cat >disklabel.template <<EOF
-/	5G-*	95%
-swap	1G
+/	700M-*	100%
+swap	300M
 EOF
 
 # Hack install CD a bit.
@@ -121,15 +115,15 @@ growisofs -M "${ISO_PATCHED}" -l -R -graft-points \
   /etc/random.seed=random.seed
 
 # Initialize disk image.
-rm -f disk.raw
-qemu-img create -f raw disk.raw 10G
+rm -f worker_disk.raw
+qemu-img create -f raw worker_disk.raw 1G
 
 # Run the installer to create the disk image.
 expect 2>&1 <<EOF | tee install_log
 set timeout 1800
 
 spawn qemu-system-x86_64 -nographic -smp 2 \
-  -drive if=virtio,file=disk.raw,format=raw -cdrom "${ISO_PATCHED}" \
+  -drive if=virtio,file=worker_disk.raw,format=raw -cdrom "${ISO_PATCHED}" \
   -net nic,model=virtio -net user -boot once=d
 
 expect timeout { exit 1 } "boot>"
@@ -177,21 +171,11 @@ expect {
 }
 EOF
 
-grep 'pkg_add OK' install_log > /dev/null \
-    || { echo Package installation failed. Inspect install_log. 2>&1 ; exit 1; }
-
 # Create Compute Engine disk image.
-echo "Archiving disk.raw... (this may take a while)"
-i="openbsd-${ARCH}-${RELNO}-gce.tar.gz"
-tar -Szcf "$i" disk.raw
+echo "Archiving worker_disk.raw... (this may take a while)"
+i="openbsd-${ARCH}-${RELNO}-vmm.tar.gz"
+tar -zcf "$i" worker_disk.raw
 
 cat <<EOF
-Done.
-
-To create GCE image run the following commands:
-
-gsutil cp -a public-read "$i" gs://syzkaller/
-gcloud compute images create ci-openbsd-root --source-uri gs://syzkaller/"$i" \\
-    --licenses "https://www.googleapis.com/compute/v1/projects/vm-options/global/licenses/enable-vmx"
-
+Done: $i
 EOF
