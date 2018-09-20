370797126e9ba28a49317bf099076a5ca06e4501
Greg Steuck <blackgnezdo@gmail.com>
2018-09-20 11:46:46 -0700

    tools/create-openbsd-gce-ci.sh mount 10G ramdisk for worker images

--- tools/create-openbsd-gce-ci.sh
+++ tools/create-openbsd-gce-ci.sh
@@ -54,6 +54,9 @@ cat >etc/rc.local <<EOF
   echo "starting syz-ci"
   fsck -y /dev/sd1a
   mount /syzkaller
+  mkdir -p /syzkaller/ramdisk
+  mount -t mfs -o-s=10G /dev/sd0b /syzkaller/ramdisk
+  chown syzkaller:syzkaller /syzkaller/ramdisk
 )
 EOF
 chmod +x install.site
