725244f95beb8e3530d8424674ab6799f9728b0f
Greg Steuck <syzkaller@nest.cx>
2018-11-25 12:49:06 -0800

    prog/analysis: prevent sandbox escaping files from entering s.files
    It causes panic later in (*randGen).filename.

---
+++ prog/rand_test.go
@@ -0,0 +1,27 @@
+// Copyright 2018 syzkaller project authors. All rights reserved.
+// Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
+
+package prog
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+)
+
+func TestNotEscaping(t *testing.T) {
+	r := newRand(nil, rand.NewSource(0))
+	s := &state{
+		files: map[string]bool{"./file0": true},
+	}
+	bound := 1000000
+	if testing.Short() {
+		bound = 1000
+	}
+	for i := 0; i < bound; i++ {
+		fn := r.filenameImpl(s)
+		if escapingFilename(fn) {
+			t.Errorf(fmt.Sprintf("sandbox escaping file name %q", fn))
+		}
+	}
+}
