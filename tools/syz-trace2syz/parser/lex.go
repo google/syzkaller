854b348c172bbf99f741a4f0896cf07cbd2772fb
Shankara Pailoor <shankarapailoor@gmail.com>
2018-11-27 11:58:40 -0800

    presubmit

--- tools/syz-trace2syz/parser/lex.go
+++ tools/syz-trace2syz/parser/lex.go
@@ -24,7 +24,6 @@ const strace_en_comment int = 211
 const strace_en_main int = 157
 
 //line straceLex.rl:22
-
 type Stracelexer struct {
 	result      *Syscall
 	data        []byte
@@ -5913,7 +5912,6 @@ func (lex *Stracelexer) Lex(out *StraceSymType) int {
 	}
 
 //line straceLex.rl:111
-
 	return tok
 }
 
