4f39cef6c2104a54ad45ac29ade31d38905452e0
Dmitry Vyukov <dvyukov@google.com>
2018-12-07 14:00:33 +0100

    tools/syz-trace2syz/proggen: convert tests to table format
    This has number of advantages:
    1. Tests are readable and writable.
       The current checks [1] are neither.
    2. Tests are much more compact.
    3. Tests verify all aspects rather than just
       1 aspect of the resulting program.
    4. Tests are much less fragile.
    5. Any diffs in the results will be more clearly visible.
    
    [1]
    switch a := p.Calls[1].Args[0].(type) {
    case *prog.ResultArg:
    	if a.Res != p.Calls[0].Ret {
    
    switch a := p.Calls[1].Args[0].(type) {
    case *prog.ResultArg:
    	pipeSecondFd := p.Calls[0].Args[0].(*prog.PointerArg).Res.(*prog.GroupArg).Inner[1]
    	if a.Res != pipeSecondFd {
    
    write := p.Calls[len(p.Calls)-2]
    inotifyRmWatch := p.Calls[len(p.Calls)-1]
    switch a := write.Args[0].Type().(type) {
    case *prog.ResourceType:
    	if a.TypeName != "fd" {
    		t.Fatalf("expected first argument of write to have type fd, got: %s", a.TypeName)
    	}
    default:
    	t.Fatalf("first argument of write is not resource type: %s", a.Name())
    }
    switch a := inotifyRmWatch.Args[1].(type) {
    case *prog.ResultArg:
    	b := a.Type().(*prog.ResourceType)
    	if b.TypeName != "inotifydesc" {
    		t.Fatalf("expected second argument of inotify_rm_watch to have type inoitfydesc, got: %s", b.TypeName)
    	}
    	if a.Res != p.Calls[2].Ret {
    		t.Fatalf("inotify_rm_watch's second argument should match the result of inotify_add_watch.")
    	}
    }
    
    sockaddr, ok := a.(*prog.PointerArg).Res.(*prog.GroupArg)
    if !ok {
    	t.Fatalf("%s", a.Type().Name())
    }
    ipv4Addr, ok := sockaddr.Inner[2].(*prog.UnionArg)
    if !ok {
    	t.Fatalf("expected 3rd argument to be unionArg, got %s", sockaddr.Inner[2].Type().Name())
    }
    optName := ipv4Addr.Option.Type().FieldName()
    if !strings.Contains(optName, "rand") {
    	t.Fatalf("expected ip option to be random opt, got: %s", optName)
    }
    ip, ok := ipv4Addr.Option.(*prog.ConstArg)
    if !ok {
    	t.Fatalf("ipv4Addr option is not IntType")
    }
    if ip.Val != expectedIp {
    	t.Fatalf("parsed != expected, %d != %d", ip.Val, expectedIp)
    }

--- tools/syz-trace2syz/proggen/proggen_test.go
+++ tools/syz-trace2syz/proggen/proggen_test.go
@@ -4,337 +4,189 @@
 package proggen
 
 import (
-	"fmt"
+	"bytes"
 	"strings"
 	"testing"
 
-	"github.com/google/syzkaller/pkg/log"
 	"github.com/google/syzkaller/prog"
 	_ "github.com/google/syzkaller/sys"
 	"github.com/google/syzkaller/tools/syz-trace2syz/parser"
 )
 
-const (
-	OS   = "linux"
-	Arch = "amd64"
-)
-
-func initializeTarget(os, arch string) *prog.Target {
-	target, err := prog.GetTarget(os, arch)
+func TestParse(t *testing.T) {
+	type Test struct {
+		input  string
+		output string
+	}
+	tests := []Test{
+		{`
+open("file", 66) = 3
+write(3, "somedata", 8) = 8
+`, `
+r0 = open(&(0x7f0000000000)='file\x00', 0x42, 0x0)
+write(r0, &(0x7f0000000040)='somedata\x00', 0x9)
+`,
+		}, {`
+pipe([5,6]) = 0
+write(6, "\xff\xff\xfe\xff", 4) = 4
+`, `
+pipe(&(0x7f0000000000)={0xffffffffffffffff, <r0=>0xffffffffffffffff})
+write(r0, &(0x7f0000000040)="fffffeff00", 0x5)
+`,
+		}, {`
+pipe({0x0, 0x1}) = 0
+shmget(0x0, 0x1, 0x2, 0x3) = 0
+`, `
+pipe(&(0x7f0000000000))
+shmget(0x0, 0x1, 0x2, &(0x7f0000001000/0x1)=nil)
+`,
+		}, {`
+socket(29, 3, 1) = 3
+getsockopt(-1, 132, 119, 0x200005c0, [14]) = -1 EBADF (Bad file descriptor)
+`, `
+socket$can_raw(0x1d, 0x3, 0x1)
+getsockopt$inet_sctp6_SCTP_RESET_STREAMS(0xffffffffffffffff, 0x84, 0x77, &(0x7f0000000000), &(0x7f0000000040)=0x8)
+`,
+		}, {`
+inotify_init() = 2
+open("tmp", 66) = 3
+inotify_add_watch(3, "\x2e", 0xfff) = 3
+write(3, "temp", 5) = 5
+inotify_rm_watch(2, 3) = 0
+`, `
+r0 = inotify_init()
+r1 = open(&(0x7f0000000000)='tmp\x00', 0x42, 0x0)
+r2 = inotify_add_watch(r1, &(0x7f0000000040)='.\x00', 0xfff)
+write(r1, &(0x7f0000000080)='temp\x00', 0x5)
+inotify_rm_watch(r0, r2)
+`,
+		}, {`
+socket(1, 1, 0) = 3
+socket(1, 1 | 2048, 0) = 3
+socket(1, 1 | 524288, 0) = 3
+socket(1, 1 | 524288, 0) = 3
+`, `
+socket$unix(0x1, 0x1, 0x0)
+socket$unix(0x1, 0x801, 0x0)
+socket$unix(0x1, 0x80001, 0x0)
+socket$unix(0x1, 0x80001, 0x0)
+`,
+		}, {`
+open("temp", 1) = 3
+connect(3, {sa_family=2, sin_port=37957, sin_addr=0x0}, 16) = -1
+`, `
+r0 = open(&(0x7f0000000000)='temp\x00', 0x1, 0x0)
+connect(r0, &(0x7f0000000040)=@in={0x2, 0x9445}, 0x80)
+`,
+		}, {`
+open("temp", 1) = 3
+connect(3, {sa_family=1, sun_path="temp"}, 110) = -1
+`, `
+r0 = open(&(0x7f0000000000)='temp\x00', 0x1, 0x0)
+connect(r0, &(0x7f0000000040)=@un=@file={0x1, 'temp\x00'}, 0x80)
+`,
+		}, {`
+open("temp", 1) = 3
+bind(5, {sa_family=16, nl_pid=0x2, nl_groups=00000003}, 12)  = -1
+`, `
+open(&(0x7f0000000000)='temp\x00', 0x1, 0x0)
+bind(0x5, &(0x7f0000000040)=@nl=@proc={0x10, 0x2, 0x3}, 0x80)
+`,
+		}, {`
+socket(17, 3, 768)  = 3
+ioctl(3, 35111, {ifr_name="\x6c\x6f", ifr_hwaddr=00:00:00:00:00:00}) = 0
+`, `
+r0 = socket$packet(0x11, 0x3, 0x300)
+ioctl$sock_ifreq(r0, 0x8927, &(0x7f0000000000)={'lo\x00'})
+`,
+		}, {`
+socket(1, 1, 0) = 3
+connect(3, {sa_family=1, sun_path="temp"}, 110) = -1 ENOENT (Bad file descriptor)
+`, `
+r0 = socket$unix(0x1, 0x1, 0x0)
+connect$unix(r0, &(0x7f0000000000)=@file={0x1, 'temp\x00'}, 0x6e)
+`,
+		}, {`
+socket(1, 1, 0) = 3
+`, `
+socket$unix(0x1, 0x1, 0x0)
+`,
+		}, {`
+socket(2, 1, 0) = 5
+ioctl(5, 21537, [1]) = 0
+`, `
+r0 = socket$inet_tcp(0x2, 0x1, 0x0)
+ioctl$int_in(r0, 0x5421, &(0x7f0000000000)=0x1)
+`,
+		}, {`
+socket(2, 1, 0) = 3
+setsockopt(3, 1, 2, [1], 4) = 0
+`, `
+r0 = socket$inet_tcp(0x2, 0x1, 0x0)
+setsockopt$sock_int(r0, 0x1, 0x2, &(0x7f0000000000)=0x1, 0x4)
+`,
+		}, {`
+9795  socket(17, 3, 768)  = 3
+9795  ioctl(3, 35123, {ifr_name="\x6c\x6f", }) = 0
+`, `
+r0 = socket$packet(0x11, 0x3, 0x300)
+ioctl$ifreq_SIOCGIFINDEX_team(r0, 0x8933, &(0x7f0000000000)={'lo\x00'})
+`,
+		}, {`
+open("temp", 1) = 3
+connect(3, {sa_family=2, sin_port=17812, sin_addr=0x0}, 16) = -1
+`, `
+r0 = open(&(0x7f0000000000)='temp\x00', 0x1, 0x0)
+connect(r0, &(0x7f0000000040)=@in={0x2, 0x4594}, 0x80)
+`,
+		}, {`
+ioprio_get(1, 0) = 4
+`, `
+ioprio_get$pid(0x1, 0x0)
+`,
+		}, {`
+socket(17, 2, 768) = 3
+`, `
+socket$packet(0x11, 0x2, 0x300)
+`,
+		}, {`
+socket(2, 1, 0) = 3
+connect(3, {sa_family=2, sin_port=17812, sin_addr=0x0}, 16) = 0
+`, `
+r0 = socket$inet_tcp(0x2, 0x1, 0x0)
+connect$inet(r0, &(0x7f0000000000)={0x2, 0x4594}, 0x10)
+`,
+		}, {`
+socket(2, 1, 0) = 3
+connect(3, {sa_family=2, sin_port=17812, sin_addr=0x7f000001}, 16) = 0
+`, `
+r0 = socket$inet_tcp(0x2, 0x1, 0x0)
+connect$inet(r0, &(0x7f0000000000)={0x2, 0x4594, @rand_addr=0x7f000001}, 0x10)
+`,
+		},
+	}
+	target, err := prog.GetTarget("linux", "amd64")
 	if err != nil {
-		log.Fatalf("%s", err)
+		t.Fatal(err)
 	}
 	target.ConstMap = make(map[string]uint64)
 	for _, c := range target.Consts {
 		target.ConstMap[c.Name] = c.Value
 	}
-	return target
-}
-
-func parseSingleTrace(t *testing.T, data string) *prog.Prog {
-	target := initializeTarget(OS, Arch)
-	traceTree, err := parser.ParseData([]byte(data))
-	if err != nil {
-		t.Fatal(err)
-	}
-	p := genProg(traceTree.TraceMap[traceTree.RootPid], target)
-	if p == nil {
-		t.Fatalf("failed to parse trace")
-	}
-	return p
-}
-
-func TestParseTraceBasic(t *testing.T) {
-	test := `open("file", 66) = 3
-			 write(3, "somedata", 8) = 8`
-	p := parseSingleTrace(t, test)
-	expectedSeq := "open-write"
-	if p.String() != expectedSeq {
-		t.Fatalf("expected: %s != %s", expectedSeq, p.String())
-	}
-	switch a := p.Calls[1].Args[0].(type) {
-	case *prog.ResultArg:
-		if a.Res != p.Calls[0].Ret {
-			t.Fatalf("first argument of write should equal result of open.")
-		}
-	default:
-		t.Fatalf("expected result arg, got: %s\n", a.Type().Name())
-	}
-}
-
-func TestParseVMA(t *testing.T) {
-	test := `pipe({0x0, 0x1}) = 0
-		 shmget(0x0, 0x1, 0x2, 0x3) = 0`
-	p := parseSingleTrace(t, test)
-	expectedSeq := "pipe-shmget"
-	if p.String() != expectedSeq {
-		t.Fatalf("expected: %s != %s", expectedSeq, p.String())
-	}
-}
-
-func TestParseTraceInnerResource(t *testing.T) {
-	test := `pipe([5,6]) = 0
-			 write(6, "\xff\xff\xfe\xff", 4) = 4`
-	p := parseSingleTrace(t, test)
-	expectedSeq := "pipe-write"
-	if p.String() != expectedSeq {
-		t.Fatalf("Expected: %s != %s", expectedSeq, p.String())
-	}
-	switch a := p.Calls[1].Args[0].(type) {
-	case *prog.ResultArg:
-		pipeSecondFd := p.Calls[0].Args[0].(*prog.PointerArg).Res.(*prog.GroupArg).Inner[1]
-		if a.Res != pipeSecondFd {
-			t.Fatalf("first argument of write must match second fd from pipe")
-		}
-	default:
-		t.Fatalf("expected result arg, got: %s\n", a.Type().Name())
-	}
-}
-
-func TestNegativeResource(t *testing.T) {
-	test := `socket(29, 3, 1) = 3
- 			  getsockopt(-1, 132, 119, 0x200005c0, [14]) = -1 EBADF (Bad file descriptor)`
-
-	p := parseSingleTrace(t, test)
-	expectedSeq := "socket$can_raw-getsockopt$inet_sctp6_SCTP_RESET_STREAMS"
-	if p.String() != expectedSeq {
-		t.Fatalf("expected: %s != %s", expectedSeq, p.String())
-	}
-	switch a := p.Calls[1].Args[0].(type) {
-	case *prog.ResultArg:
-		if a.Val != ^uint64(0) {
-			t.Fatalf("expected resource type to be negative, got: %d", a.Val)
-		}
-	default:
-		t.Fatalf("expected result arg, got: %s\n", a.Type().Name())
-	}
-}
-
-func TestDistinguishResourceTypes(t *testing.T) {
-	test := `inotify_init() = 2
-			 open("tmp", 66) = 3
-			 inotify_add_watch(3, "\x2e", 0xfff) = 3
-	 		 write(3, "temp", 5) = 5
-			 inotify_rm_watch(2, 3) = 0`
-	expectedSeq := "inotify_init-open-inotify_add_watch-write-inotify_rm_watch"
-	p := parseSingleTrace(t, test)
-	if p.String() != expectedSeq {
-		t.Fatalf("Expected: %s != %s", expectedSeq, p.String())
-	}
-	write := p.Calls[len(p.Calls)-2]
-	inotifyRmWatch := p.Calls[len(p.Calls)-1]
-	switch a := write.Args[0].Type().(type) {
-	case *prog.ResourceType:
-		if a.TypeName != "fd" {
-			t.Fatalf("expected first argument of write to have type fd, got: %s", a.TypeName)
-		}
-	default:
-		t.Fatalf("first argument of write is not resource type: %s", a.Name())
-	}
-	switch a := inotifyRmWatch.Args[1].(type) {
-	case *prog.ResultArg:
-		b := a.Type().(*prog.ResourceType)
-		if b.TypeName != "inotifydesc" {
-			t.Fatalf("expected second argument of inotify_rm_watch to have type inoitfydesc, got: %s", b.TypeName)
-		}
-		if a.Res != p.Calls[2].Ret {
-			t.Fatalf("inotify_rm_watch's second argument should match the result of inotify_add_watch.")
-		}
-	}
-}
-
-func TestSocketLevel(t *testing.T) {
-	test := `socket(1, 1, 0) = 3
-			 socket(1, 1 | 2048, 0) = 3
-			 socket(1, 1 | 524288, 0) = 3
-			 socket(1, 1 | 524288, 0) = 3`
-	expectedSeq := "socket$unix-socket$unix-socket$unix-socket$unix"
-	p := parseSingleTrace(t, test)
-	if p.String() != expectedSeq {
-		t.Fatalf("Expected: %s != %s", expectedSeq, p.String())
-	}
-}
-
-func TestIdentifySockaddrStorage(t *testing.T) {
-	type identifyStorageTest struct {
-		test        string
-		expectedSeq string
-		callIdx     int
-		argIdx      int
-		fieldName   string
-	}
-	tests := []identifyStorageTest{
-		{
-			`open("temp", 1) = 3
-			  connect(3, {sa_family=2, sin_port=37957, sin_addr=0x0}, 16) = -1`,
-			"open-connect",
-			1,
-			1,
-			"sockaddr_in",
-		},
-		{
-			`open("temp", 1) = 3
-			  connect(3, {sa_family=1, sun_path="temp"}, 110) = -1`,
-			"open-connect",
-			1,
-			1,
-			"sockaddr_un",
-		},
-		{
-			`open("temp", 1) = 3
-			  bind(5, {sa_family=16, nl_pid=0, nl_groups=00000000}, 12)  = -1`,
-			"open-bind",
-			1,
-			1,
-			"sockaddr_nl",
-		},
-	}
-
-	validator := func(arg prog.Arg, field string) error {
-		storagePtr := arg.(*prog.PointerArg)
-		storageArg, ok := storagePtr.Res.(*prog.UnionArg)
-		if !ok {
-			t.Fatalf("second argument not union: %s", storagePtr.Res.Type().Name())
-		}
-		fieldName := storageArg.Option.Type().Name()
-		if fieldName != field {
-			return fmt.Errorf("incorrect storage type, expected %s != %s", field, fieldName)
-		}
-		return nil
-	}
-
-	for i, test := range tests {
-		p := parseSingleTrace(t, test.test)
-		if p.String() != test.expectedSeq {
-			t.Fatalf("failed btest: %d, expected: %s != %s", i, test.expectedSeq, p.String())
-		}
-		err := validator(p.Calls[test.callIdx].Args[test.argIdx], test.fieldName)
+	for _, test := range tests {
+		input := strings.TrimSpace(test.input)
+		tree, err := parser.ParseData([]byte(input))
 		if err != nil {
-			t.Fatalf("failed subtest: %d with err: %s", i, err)
+			t.Fatal(err)
 		}
-	}
-}
-
-func TestIdentifyIfru(t *testing.T) {
-	type testIfru struct {
-		test        string
-		expectedSeq string
-	}
-	tests := []testIfru{
-		{
-			`socket(17, 3, 768)  = 3
-			 ioctl(3, 35111, {ifr_name="\x6c\x6f", ifr_hwaddr=00:00:00:00:00:00}) = 0`,
-			"socket$packet-ioctl$sock_ifreq",
-		},
-	}
-
-	for i, test := range tests {
-		p := parseSingleTrace(t, test.test)
-		if p.String() != test.expectedSeq {
-			t.Fatalf("failed subtest: %d, expected %s != %s", i, test.expectedSeq, p.String())
+		p := genProg(tree.TraceMap[tree.RootPid], target)
+		if p == nil {
+			t.Fatalf("failed to parse trace")
 		}
-	}
-}
-
-func TestParseVariants(t *testing.T) {
-	type variantTest struct {
-		test        string
-		expectedSeq string
-	}
-	tests := []variantTest{
-		{
-			`socket(1, 1, 0) = 3
-			  connect(3, {sa_family=1, sun_path="temp"}, 110) = -1 ENOENT (Bad file descriptor)`,
-			"socket$unix-connect$unix",
-		},
-		{
-			`socket(1, 1, 0) = 3`,
-			"socket$unix",
-		},
-		{
-			`socket(2, 1, 0) = 5
-			  ioctl(5, 21537, [1]) = 0`,
-			"socket$inet_tcp-ioctl$int_in",
-		},
-		{
-			`socket(2, 1, 0) = 3
-			  setsockopt(3, 1, 2, [1], 4) = 0`,
-			"socket$inet_tcp-setsockopt$sock_int",
-		},
-		{
-			`9795  socket(17, 3, 768)  = 3
-			  9795  ioctl(3, 35123, {ifr_name="\x6c\x6f", }) = 0`,
-			"socket$packet-ioctl$ifreq_SIOCGIFINDEX_team",
-		},
-		{
-			`open("temp", 1) = 3
-			  connect(3, {sa_family=2, sin_port=17812, sin_addr=0x0}, 16) = -1`,
-			"open-connect",
-		},
-		{
-			`ioprio_get(1, 0) = 4`,
-			"ioprio_get$pid",
-		},
-		{
-			`socket(17, 2, 768) = 3`,
-			"socket$packet",
-		},
-	}
-
-	for i, test := range tests {
-		p := parseSingleTrace(t, test.test)
-		if p.String() != test.expectedSeq {
-			t.Fatalf("failed subtest: %d, expected %s != %s", i, test.expectedSeq, p.String())
+		got := string(bytes.TrimSpace(p.Serialize()))
+		want := strings.TrimSpace(test.output)
+		if want != got {
+			t.Errorf("input:\n%v\n\nwant:\n%v\n\ngot:\n%v", input, want, got)
 		}
-	}
-}
-
-func TestParseIPv4(t *testing.T) {
-	type ip4test struct {
-		test        string
-		expectedSeq string
-		ip4         uint64
-	}
-	tests := []ip4test{
-		{
-			`socket(2, 1, 0) = 3
-			  connect(3, {sa_family=2, sin_port=17812, sin_addr=0x0}, 16) = 0`,
-			"socket$inet_tcp-connect$inet",
-			0,
-		},
-		{
-			`socket(2, 1, 0) = 3
-			  connect(3, {sa_family=2, sin_port=17812, sin_addr=0x7f000001}, 16) = 0`,
-			"socket$inet_tcp-connect$inet",
-			0x7f000001,
-		},
-	}
-	testIpv4 := func(expectedIp uint64, a prog.Arg, t *testing.T) {
-		sockaddr, ok := a.(*prog.PointerArg).Res.(*prog.GroupArg)
-		if !ok {
-			t.Fatalf("%s", a.Type().Name())
-		}
-		ipv4Addr, ok := sockaddr.Inner[2].(*prog.UnionArg)
-		if !ok {
-			t.Fatalf("expected 3rd argument to be unionArg, got %s", sockaddr.Inner[2].Type().Name())
-		}
-		optName := ipv4Addr.Option.Type().FieldName()
-		if !strings.Contains(optName, "rand") {
-			t.Fatalf("expected ip option to be random opt, got: %s", optName)
-		}
-		ip, ok := ipv4Addr.Option.(*prog.ConstArg)
-		if !ok {
-			t.Fatalf("ipv4Addr option is not IntType")
-		}
-		if ip.Val != expectedIp {
-			t.Fatalf("parsed != expected, %d != %d", ip.Val, expectedIp)
-		}
-	}
-	for i, test := range tests {
-		p := parseSingleTrace(t, test.test)
-		if p.String() != test.expectedSeq {
-			t.Fatalf("failed subtest: %d, expected %s != %s", i, test.expectedSeq, p.String())
-		}
-		testIpv4(test.ip4, p.Calls[1].Args[1], t)
 	}
 }
