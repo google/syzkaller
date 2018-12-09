bfda3b94e1cc1962b910503045bc62c395291d68
Dmitry Vyukov <dvyukov@google.com>
2018-12-09 15:37:35 +0100

    tools/syz-cover: add utility for generation of coverage reports
    syz-cover generates coverage HTML report from raw coverage files.
    Raw coverage files are text files with one PC in hex form per line, e.g.:
    
    	0xffffffff8398658d
    	0xffffffff839862fc
    	0xffffffff8398633f
    
    Raw coverage files can be obtained either from /rawcover manager HTTP handler,
    or from syz-execprog with -coverfile flag.
    
    Usage:
    	syz-cover [-os=OS -arch=ARCH -kernel_src=. -kernel_obj=.] rawcover.file*

---
+++ tools/syz-cover/syz-cover.go
@@ -0,0 +1,106 @@
+// Copyright 2018 syzkaller project authors. All rights reserved.
+// Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
+
+// syz-cover generates coverage HTML report from raw coverage files.
+// Raw coverage files are text files with one PC in hex form per line, e.g.:
+//
+//	0xffffffff8398658d
+//	0xffffffff839862fc
+//	0xffffffff8398633f
+//
+// Raw coverage files can be obtained either from /rawcover manager HTTP handler,
+// or from syz-execprog with -coverfile flag.
+//
+// Usage:
+//	syz-cover [-os=OS -arch=ARCH -kernel_src=. -kernel_obj=.] rawcover.file*
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"strconv"
+
+	"github.com/google/syzkaller/pkg/cover"
+	"github.com/google/syzkaller/pkg/osutil"
+	"github.com/google/syzkaller/sys/targets"
+)
+
+func main() {
+	var (
+		flagOS        = flag.String("os", runtime.GOOS, "target os")
+		flagArch      = flag.String("arch", runtime.GOARCH, "target arch")
+		flagKernelSrc = flag.String("kernel_src", "", "path to kernel sources")
+		flagKernelObj = flag.String("kernel_obj", "", "path to kernel build/obj dir")
+	)
+	flag.Parse()
+
+	if len(flag.Args()) == 0 {
+		fmt.Fprintf(os.Stderr, "usage: syz-cover [flags] rawcover.file\n")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+	if *flagKernelSrc == "" {
+		*flagKernelSrc = "."
+	}
+	if *flagKernelObj == "" {
+		*flagKernelObj = *flagKernelSrc
+	}
+	target := targets.Get(*flagOS, *flagArch)
+	if target == nil {
+		failf("unknown target %v/%v", *flagOS, *flagArch)
+	}
+	pcs, err := readPCs(flag.Args())
+	if err != nil {
+		failf("%v", err)
+	}
+	kernelObj := filepath.Join(*flagKernelObj, target.KernelObject)
+	rg, err := cover.MakeReportGenerator(kernelObj, *flagKernelSrc, *flagArch)
+	if err != nil {
+		failf("%v", err)
+	}
+	buf := new(bytes.Buffer)
+	if err := rg.Do(buf, pcs); err != nil {
+		failf("%v", err)
+	}
+	fn, err := osutil.TempFile("syz-cover")
+	if err != nil {
+		failf("%v", err)
+	}
+	fn += ".html"
+	if err := osutil.WriteFile(fn, buf.Bytes()); err != nil {
+		failf("%v", err)
+	}
+	if err := exec.Command("xdg-open", fn).Start(); err != nil {
+		failf("failed to start browser: %v", err)
+	}
+}
+
+func readPCs(files []string) ([]uint64, error) {
+	var pcs []uint64
+	for _, file := range files {
+		data, err := ioutil.ReadFile(file)
+		if err != nil {
+			return nil, err
+		}
+		for s := bufio.NewScanner(bytes.NewReader(data)); s.Scan(); {
+			pc, err := strconv.ParseUint(s.Text(), 0, 64)
+			if err != nil {
+				return nil, err
+			}
+			pcs = append(pcs, pc)
+		}
+	}
+	return pcs, nil
+}
+
+func failf(msg string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, msg+"\n", args...)
+	os.Exit(1)
+}
