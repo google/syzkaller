cf8d7387e9fb137df5dc77161b37e87d4e8fb433
Siddharth M <siddharth.muralee@gmail.com>
2019-01-24 13:08:56 +0530

    Support cross compilation on linux

--- sys/targets/targets.go
+++ sys/targets/targets.go
@@ -184,7 +184,10 @@ var List = map[string]map[string]*Target{
 			PtrSize:     8,
 			PageSize:    4 << 10,
 			CFlags:      []string{"-m64"},
-			CrossCFlags: []string{"-m64", "-static"},
+			CrossCFlags: []string{"-m64", "-static"
+				"--sysroot", os.ExpandEnv("${SOURCEDIR}../dest/"),
+			},
+			CCompiler:   os.ExpandEnv("${SOURCEDIR}../tools/bin/x86_64--netbsd-g++"),
 		},
 	},
 	"openbsd": {
@@ -303,6 +306,7 @@ var oses = map[string]osCommon{
 		CPP:                    "g++",
 	},
 	"netbsd": {
+		BuildOS:                "linux",
 		SyscallNumbers:         true,
 		SyscallPrefix:          "SYS_",
 		ExecutorUsesShmem:      true,
