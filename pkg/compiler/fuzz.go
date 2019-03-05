2581bfb1244ca7b651c100c657b884ac338e0161
Dmitry Vyukov <dvyukov@google.com>
2019-03-05 14:32:33 +0100

    fuzz.yaml: add pkg/compiler and trace2syz fuzzers

--- pkg/compiler/fuzz.go
+++ pkg/compiler/fuzz.go
@@ -1,12 +1,11 @@
 // Copyright 2017 syzkaller project authors. All rights reserved.
 // Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
 
-// +build gofuzz
-
 package compiler
 
 import (
 	"github.com/google/syzkaller/pkg/ast"
+	"github.com/google/syzkaller/sys/targets"
 )
 
 func Fuzz(data []byte) int {
@@ -15,11 +14,14 @@ func Fuzz(data []byte) int {
 	if desc == nil {
 		return 0
 	}
-	prog := Compile(desc, fuzzConsts, eh)
+	prog := Compile(desc, fuzzConsts, fuzzTarget, eh)
 	if prog == nil {
 		return 0
 	}
 	return 1
 }
 
-var fuzzConsts = map[string]uint64{"A": 1, "B": 2, "C": 3, "SYS_C": 4}
+var (
+	fuzzTarget = targets.Get("test", "64")
+	fuzzConsts = map[string]uint64{"A": 1, "B": 2, "C": 3, "SYS_A": 4, "SYS_B": 5, "SYS_C": 6}
+)
