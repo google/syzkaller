2806a9214b417b0a050b64f7032b51204f6a48a1
Mark Johnston <markjdb@gmail.com>
2019-03-08 11:41:48 -0500

    pkg/vcs: add freebsd
    This is currently identical to support for OpenBSD and NetBSD.  A
    FreeBSD git repo is available at github.com/freebsd/freebsd.

--- pkg/vcs/vcs.go
+++ pkg/vcs/vcs.go
@@ -90,6 +90,8 @@ func NewRepo(os, vm, dir string) (Repo, error) {
 		return newOpenBSD(vm, dir), nil
 	case "netbsd":
 		return newNetBSD(vm, dir), nil
+	case "freebsd":
+		return newFreeBSD(vm, dir), nil
 	}
 	return nil, fmt.Errorf("vcs is unsupported for %v", os)
 }
