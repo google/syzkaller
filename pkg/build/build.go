4f9d2f70eca7f3bad640974ea88fcb438a7b23d5
Andrew Donnellan <andrew.donnellan@au1.ibm.com>
2019-03-26 14:36:39 +1100

    pkg/build: Add ppc64le Linux as a supported configuration
    Enable the Linux builder for ppc64le.
    
    For ppc64le, we use zImage rather than bzImage as our kernel target. Pass
    through the target architecture to buildKernel() so we can pick the right
    target based on arch.
    
    Closes: #1084 ("pkg/build: Support building Linux on ppc64le")
    Signed-off-by: Andrew Donnellan <andrew.donnellan@au1.ibm.com>

--- pkg/build/build.go
+++ pkg/build/build.go
@@ -72,6 +72,7 @@ func getBuilder(targetOS, targetArch, vmType string) (builder, error) {
 	}{
 		{"linux", "amd64", []string{"gvisor"}, gvisor{}},
 		{"linux", "amd64", []string{"gce", "qemu"}, linux{}},
+		{"linux", "ppc64le", []string{"qemu"}, linux{}},
 		{"fuchsia", "amd64", []string{"qemu"}, fuchsia{}},
 		{"fuchsia", "arm64", []string{"qemu"}, fuchsia{}},
 		{"akaros", "amd64", []string{"qemu"}, akaros{}},
