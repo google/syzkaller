ccf4ede4ddf5612d4cd25d6aa186fd86226aa73a
Anton Lindqvist <anton@basename.se>
2019-05-02 04:30:36 +0200

    sys/openbsd: add vmm descriptions (#1152)
    Most probably limited to input validation for now. In the future, it
    could be extended to provide a bootable kernel during vm create (/bsd)
    and turn vmid into a proper resource.
    
    The OpenBSD VMs on GCE does support vmm(4).

--- pkg/host/host_openbsd.go
+++ pkg/host/host_openbsd.go
@@ -4,10 +4,27 @@
 package host
 
 import (
+	"fmt"
+	"strings"
+	"syscall"
+
 	"github.com/google/syzkaller/prog"
 )
 
 func isSupported(c *prog.Syscall, target *prog.Target, sandbox string) (bool, string) {
+	if strings.HasPrefix(c.CallName, "ioctl$VMM_") {
+		return isSupportedVMM()
+	}
+	return true, ""
+}
+
+func isSupportedVMM() (bool, string) {
+	device := "/dev/vmm"
+	fd, err := syscall.Open(device, syscall.O_RDONLY, 0)
+	if fd == -1 {
+		return false, fmt.Sprintf("open(%v) failed: %v", device, err)
+	}
+	syscall.Close(fd)
 	return true, ""
 }
 
