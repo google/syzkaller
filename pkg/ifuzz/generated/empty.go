0b853dc7891accae333c744514f16a46d779fe3d
Dmitry Vyukov <dvyukov@google.com>
2019-05-15 10:04:22 +0200

    .golangci.yml: add codeanalysis build tag
    Using a build tag to exclude files for golangci-lint
    reduces memory consumption (it does not parse them).
    The naive attempt with skip-dirs did not work.
    So add codeanalysis build tag and use it in auto-generated files.
    
    Update #977

---
+++ pkg/ifuzz/generated/empty.go
@@ -0,0 +1,6 @@
+// Copyright 2019 syzkaller project authors. All rights reserved.
+// Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
+
+// To unbreak build with insns.go is excluded by build tags.
+
+package generated
