33d2506b79b567169526d4ed7060c6d87df9e8b3
R3x <siddharth.muralee@gmail.com>
2019-05-19 03:25:10 +0000

    make changes to prevent failing build

--- pkg/host/host_netbsd.go
+++ pkg/host/host_netbsd.go
@@ -10,3 +10,7 @@ import (
 func isSupported(c *prog.Syscall, target *prog.Target, sandbox string) (bool, string) {
 	return true, ""
 }
+
+func init() {
+	checkFeature[FeatureCoverage] = unconditionallyEnabled
+}
