d25bb7ad41f9d44dc58107b2d5e264cae1169611
R3x <siddharth.muralee@gmail.com>
2019-06-12 04:27:58 +0000

    pkg/report: use cover protect on NetBSD also

--- executor/executor_bsd.h
+++ executor/executor_bsd.h
@@ -86,7 +86,7 @@ static void cover_open(cover_t* cov, bool extra)
 
 static void cover_protect(cover_t* cov)
 {
-#if GOOS_freebsd
+#if GOOS_freebsd || GOOS_netbsd
 	size_t mmap_alloc_size = kCoverSize * KCOV_ENTRY_SIZE;
 	long page_size = sysconf(_SC_PAGESIZE);
 	if (page_size > 0)
@@ -107,7 +107,7 @@ static void cover_protect(cover_t* cov)
 
 static void cover_unprotect(cover_t* cov)
 {
-#if GOOS_freebsd
+#if GOOS_freebsd || GOOS_netbsd
 	size_t mmap_alloc_size = kCoverSize * KCOV_ENTRY_SIZE;
 	mprotect(cov->data, mmap_alloc_size, PROT_READ | PROT_WRITE);
 #elif GOOS_openbsd
