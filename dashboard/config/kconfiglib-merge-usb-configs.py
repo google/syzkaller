1b329f765e5d51c33e8a0e7fab9490916d9f4be7
Andrey Konovalov <andreyknvl@google.com>
2019-07-16 14:31:59 +0200

    dashboard: fix usb config extraction script
    Fix extracting more configs that are actually enabled.
    Regenerate upsteam-usb.config and sys/linux/init_vusb_ids.go.

--- dashboard/config/kconfiglib-merge-usb-configs.py
+++ dashboard/config/kconfiglib-merge-usb-configs.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+from __future__ import print_function
+
 import sys
 
 import kconfiglib
@@ -23,7 +25,7 @@ def unpack_dep_expr(expr):
 	r.update(unpack_dep_expr(expr[2]))
 	return r
 
-# Extract dependencies recuresively.
+# Extract item dependencies recursively.
 def extract_deps(item):
 	deps = set()
 	handled = set()
@@ -38,6 +40,7 @@ def extract_deps(item):
 	extract_deps_impl(item)
 	return deps
 
+# Extract all dependencies for a list of nodes.
 def extract_nodes_deps(nodes):
 	deps = set()
 	for node in nodes:
@@ -47,49 +50,61 @@ def extract_nodes_deps(nodes):
 if len(sys.argv) < 3:
 	sys.exit('Usage: {} usb.config'.format(sys.argv[0]))
 
+# Load config given in SCRIPT_ARG.
 kconf = kconfiglib.Kconfig(warn=False)
 kconf.load_config(sys.argv[2])
 
+# Make a list of some core USB symbols.
 core_usb_syms_names = ['USB_SUPPORT', 'USB', 'USB_ARCH_HAS_HCD']
 core_usb_syms = set()
 for name in core_usb_syms_names:
 	core_usb_syms.add(kconf.syms[name])
 
+# Extract all enabled (as =y or =m) USB nodes. A USB node is detected as a
+# node, which depends on least one USB core symbol.
 usb_nodes = set()
 for node in kconf.node_iter():
 	if node.item.__class__ not in [kconfiglib.Symbol, kconfiglib.Choice]:
 		continue
+	if node.item.tri_value == 0:
+		continue
 	if len(core_usb_syms.intersection(extract_deps(node.item))) > 0:
 		usb_nodes.add(node)
-print('usb_nodes', len(usb_nodes))
+print('USB nodes:', len(usb_nodes))
 
+# Extract USB nodes dependencies.
 deps = extract_nodes_deps(usb_nodes)
-print('deps', len(deps))
+print('USB nodes dependencies:', len(deps))
 
-# Only leave the last choice.
+# Extract choice options to be disabled to only leave the last option enabled
+# for each choice.
 exclude = set()
 for dep in deps:
 	if dep.__class__ is not kconfiglib.Choice:
 		continue
 	for sym in dep.syms[:-1]:
 		exclude.add(sym)
-print('exclude', len(exclude))
+print('Excluded choice options:', len(exclude))
 
+# Load current .config.
 kconf = kconfiglib.Kconfig(warn=False)
 kconf.load_config()
 
+# First, enable all dependencies.
 for dep in deps:
 	if dep.__class__ is kconfiglib.Symbol:
 		if dep in exclude:
 			continue
 		kconf.syms[dep.name].set_value(2)
 
+# Then, enable extract USB nodes as =y.
 for node in list(usb_nodes):
 	if node.item.__class__ is kconfiglib.Symbol:
 		if node.item in exclude:
 			continue
 		kconf.syms[node.item.name].set_value(2)
 
+# Remove testing related symbols.
 blacklist = ['COMPILE_TEST']
 for sym in blacklist:
 	kconf.syms[sym].set_value(0)
