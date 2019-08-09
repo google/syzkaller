e5701ed16c822ef5a4abc4224b44c36b3489aca3
Yevgeny Pats <yp@fuzzit.dev>
2019-08-09 10:46:28 +0300

    fuzzit.sh: migrate syzkaller to new org and introduce regression
    The fuzzers now run on every new code that is pushed to master
    
    Regression using the generated corpus from the above step is
    run locally in the travis. This should fail the travis if new
    or old bugs are introduce and thus help catch bugs earlier.
    
    To reproduce locally you can run
    
    ./fuzzit create job --local syzkaller/target-name $PATH_TO_FUZZER

--- fuzzit.sh
+++ fuzzit.sh
@@ -2,6 +2,7 @@
 # Copyright 2019 syzkaller project authors. All rights reserved.
 # Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
 
+
 # Helper script for working with fuzzit.dev
 # https://github.com/fuzzitdev/example-go
 
@@ -10,16 +11,21 @@ set -eux
 function target {
 	go-fuzz-build -libfuzzer -func $3 -o fuzzer.a $2
 	clang -fsanitize=fuzzer fuzzer.a -o fuzzer
-	./fuzzit create job --type fuzzing --branch $TRAVIS_BRANCH --revision $TRAVIS_COMMIT $1 ./fuzzer
+	./fuzzit create job $LOCAL --type fuzzing --branch $TRAVIS_BRANCH --revision $TRAVIS_COMMIT syzkaller/$1 ./fuzzer
 }
 
 go get -u github.com/dvyukov/go-fuzz/go-fuzz-build
-wget -q -O fuzzit https://github.com/fuzzitdev/fuzzit/releases/download/v1.2.7/fuzzit_Linux_x86_64
+wget -q -O fuzzit https://github.com/fuzzitdev/fuzzit/releases/download/v2.4.12/fuzzit_Linux_x86_64
 chmod a+x fuzzit
-./fuzzit auth ${FUZZIT_API_KEY}
+if [ "$1" = "fuzzing" ]; then
+    ./fuzzit auth ${FUZZIT_API_KEY}
+    export LOCAL=""
+else
+    export LOCAL="--local"
+fi
 
-target QOmcj5QL4FVtUWV2UmhG ./prog/test FuzzDeserialize
-target ddurE2yrDlqpklLYgNc6 ./prog/test FuzzParseLog
-target 4A7DVc22Gni7tUtZBc19 ./pkg/compiler Fuzz
-target YMCIxz61XkKWaB4jmiS5 ./pkg/report Fuzz
-target 1d75bUDf9zNQz1HgHyM0 ./tools/syz-trace2syz/proggen Fuzz
+target syzkaller-prog-deserialize ./prog/test FuzzDeserialize
+target syzkaller-prog-parselog ./prog/test FuzzParseLog
+target syzkaller-compiler ./pkg/compiler Fuzz
+target syzkaller-report ./pkg/report Fuzz
+target syzkaller-trace2syz ./tools/syz-trace2syz/proggen Fuzz
