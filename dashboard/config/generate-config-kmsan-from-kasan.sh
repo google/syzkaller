6c19c36919c3040459c321551e9d8993b3cd5182
Andrey Konovalov <andreyknvl@google.com>
2019-08-30 18:19:06 +0200

    dashboard/config: improve config generation scripts
    1. Use MAKE_ARGS var to pass arguments to make.
    2. Pass -m to merge_config.sh to avoid calling make without CC.
    3. Make util_add_syzbot_extra_bits() operate on .config.

--- dashboard/config/generate-config-kmsan-from-kasan.sh
+++ dashboard/config/generate-config-kmsan-from-kasan.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 # Generate upstream-kmsan.config from upstream-kasan.config.
-# Make sure $CC points to the Clang binary and $SOURCEDIR - to the kernel tree.
-
-THISDIR=`cd $(dirname $0); pwd`
-KASAN_CONFIG=${THISDIR}/upstream-kasan.config
-KMSAN_CONFIG=${THISDIR}/upstream-kmsan.config
-KMSAN_ADD=${THISDIR}/bits-kmsan.config
-. ${THISDIR}/util.sh
+# Make sure $CC points to the Clang binary and $KERNEL_SOURCE - to the kernel tree.
 
 [ -z "${CC}" ] && echo 'Please set $CC to point to the compiler!' && exit
-[ -z "${SOURCEDIR}" ] && echo 'Please set $SOURCEDIR to point to the kernel tree!' && exit
+[ -z "${KERNEL_SOURCE}" ] && echo 'Please set $KERNEL_SOURCE to point to the kernel tree!' && exit
 
-(
-cd $SOURCEDIR
+THIS_DIR=`cd $(dirname $0); pwd`
+. ${THIS_DIR}/util.sh
+
+KASAN_CONFIG=${THIS_DIR}/upstream-kasan.config
+KMSAN_CONFIG=${THIS_DIR}/upstream-kmsan.config
+KMSAN_ADD=${THIS_DIR}/bits-kmsan.config
+
+cd ${KERNEL_SOURCE}
+
 cp ${KASAN_CONFIG} .config
-scripts/kconfig/merge_config.sh .config ${KMSAN_ADD}
-make CC="${CC}" oldconfig < /dev/null
+scripts/kconfig/merge_config.sh -m .config ${KMSAN_ADD}
+make ${MAKE_VARS} olddefconfig
 
 util_add_usb_bits
-util_add_syzbot_extra_bits "${KMSAN_CONFIG}"
+util_add_syzbot_extra_bits
 
-cat .config >> ${KMSAN_CONFIG}
-cp ${KMSAN_CONFIG} .config
-)
+cp .config ${KMSAN_CONFIG}
