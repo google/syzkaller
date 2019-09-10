8491e03fb279f9bf4f3f8678318b4e602355fa84
Veronica Radu <veronicaradu@google.com>
2019-09-10 10:06:53 +0200

    prog: add better call-to-call priority calculation Update #1380

--- prog/prio_test.go
+++ prog/prio_test.go
@@ -59,3 +59,39 @@ func TestPrioChoice(t *testing.T) {
 		t.Fatal(diff)
 	}
 }
+
+// Test static priorities assigned based on argument direction.
+func TestStaticPriorities(t *testing.T) {
+	target, rs, iters := initTest(t)
+	// The first call is the one that creates a resource and the rest are calls that can use that resource.
+	tests := [][]string{
+		{"open", "read", "write", "mmap"},
+		{"socket", "listen", "setsockopt"},
+	}
+	ct := target.BuildChoiceTable(target.CalculatePriorities(nil), nil)
+	r := rand.New(rs)
+	for _, syscalls := range tests {
+		// Counts the number of times a call is chosen after a call that creates a resource (referenceCall).
+		counter := make(map[string]int)
+		referenceCall := syscalls[0]
+		for _, call := range syscalls {
+			count := 0
+			for it := 0; it < iters*10000; it++ {
+				chosenCall := target.Syscalls[ct.Choose(r, target.SyscallMap[call].ID)].Name
+				if call == referenceCall {
+					counter[chosenCall]++
+				} else if chosenCall == referenceCall {
+					count++
+				}
+			}
+			if call == referenceCall {
+				continue
+			}
+			// Checks that prio[callCreatesRes][callUsesRes] > prio[callUsesRes][callCreatesRes]
+			if count >= counter[call] {
+				t.Fatalf("Too high priority for %s -> %s: %d vs %s -> %s: %d", call, referenceCall,
+					count, referenceCall, call, counter[call])
+			}
+		}
+	}
+}
