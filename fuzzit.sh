32d593576a7ee67f588218d3a44a0b69fe31b0a0
Yevgeny Pats <yp@fuzzit.dev>
2019-09-13 16:13:36 +0300

    fix fuzzit script to work with latest CLI syntax
    Signed-off-by: Yevgeny Pats <yp@fuzzit.dev>

--- fuzzit.sh
+++ fuzzit.sh
@@ -7,22 +7,17 @@
 # https://github.com/fuzzitdev/example-go
 
 set -eux
+export TYPE="${1}"
 
 function target {
 	go-fuzz-build -libfuzzer -func $3 -o fuzzer.a $2
 	clang -fsanitize=fuzzer fuzzer.a -o fuzzer
-	./fuzzit create job $LOCAL --type fuzzing --branch $TRAVIS_BRANCH --revision $TRAVIS_COMMIT syzkaller/$1 ./fuzzer
+	./fuzzit create job --type "${TYPE}" --branch $TRAVIS_BRANCH --revision $TRAVIS_COMMIT syzkaller/$1 ./fuzzer
 }
 
 go get -u github.com/dvyukov/go-fuzz/go-fuzz-build
 wget -q -O fuzzit https://github.com/fuzzitdev/fuzzit/releases/download/v2.4.46/fuzzit_Linux_x86_64
 chmod a+x fuzzit
-if [ "$1" = "fuzzing" ]; then
-    ./fuzzit auth ${FUZZIT_API_KEY}
-    export LOCAL=""
-else
-    export LOCAL="--local"
-fi
 
 target syzkaller-prog-deserialize ./prog/test FuzzDeserialize
 target syzkaller-prog-parselog ./prog/test FuzzParseLog
