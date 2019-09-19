e9f995b8c32c39f6140b75dbdd81856ce180cbe8
Zubin Mithra <zsm@chromium.org>
2019-09-19 09:56:57 -0700

    dashboard/app: no reporting fix bisection results for already fixed bugs
    * Modify pollCompletedJobs(); for bugs that are already marked as fixed,
    invalid or duplicate do not report bisection results.
    * Add TestNotReportingAlreadyFixed() to test that reporting does not
    occur for already fixed bugs.

--- dashboard/app/jobs.go
+++ dashboard/app/jobs.go
@@ -583,6 +583,17 @@ func pollCompletedJobs(c context.Context, typ string) ([]*dashapi.BugReport, err
 		if job.Type == JobBisectFix && len(job.Commits) != 1 {
 			continue
 		}
+		// If the bug is already known to be fixed, invalid or duplicate, do not report the
+		// bisection results.
+		bug := new(Bug)
+		bugKey := keys[i].Parent()
+		if err := db.Get(c, bugKey, bug); err != nil {
+			return nil, fmt.Errorf("job %v: failed to get bug: %v", extJobID(keys[i]), err)
+		}
+		if len(bug.Commits) != 0 || bug.Status != BugStatusOpen {
+			jobReported(c, extJobID(keys[i]))
+			continue
+		}
 		rep, err := createBugReportForJob(c, job, keys[i], reporting.Config)
 		if err != nil {
 			log.Errorf(c, "failed to create report for job: %v", err)
