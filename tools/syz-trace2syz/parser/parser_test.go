0f33548a39d8debf54b8552a250a275176595cda
Dmitry Vyukov <dvyukov@google.com>
2019-10-08 12:26:27 +0200

    tools/syz-trace2syz: disable golangci-lint
    A new version of golangci-lint started failing trying to build syz-trace2syz
    (because we already ignore some files).
    Ignore the rest of files to fix the failure.

--- tools/syz-trace2syz/parser/parser_test.go
+++ tools/syz-trace2syz/parser/parser_test.go
@@ -1,6 +1,8 @@
 // Copyright 2018 syzkaller project authors. All rights reserved.
 // Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
 
+// +build !codeanalysis
+
 package parser
 
 import (
