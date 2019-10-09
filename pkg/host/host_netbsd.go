c4b9981b5f5b70dc03eb3f76c618398510101a1d
Kamil Rytarowski <n54@gmx.com>
2019-10-09 16:52:02 +0200

    Disable checkFeature[FeatureNetworkInjection] for NetBSD
    There is a problem with the image. Until the problem will be resolved,
    disable the feature as it causes premature death of the syzbot setup.

--- pkg/host/host_netbsd.go
+++ pkg/host/host_netbsd.go
@@ -14,5 +14,4 @@ func isSupported(c *prog.Syscall, target *prog.Target, sandbox string) (bool, st
 func init() {
 	checkFeature[FeatureCoverage] = unconditionallyEnabled
 	checkFeature[FeatureComparisons] = unconditionallyEnabled
-	checkFeature[FeatureNetworkInjection] = unconditionallyEnabled
 }
