a1009fa9519933718054c9dd25717c5e5d3d19c2
Paul Chaignon <paul.chaignon@orange.com>
2019-10-23 09:27:03 +0200

    pkg/compiler: fix root node not visited in typedef checks
    Without this fix, the compiler throws an error 'template argument BASE is
    not used' for the following typedef.
    
      type templ1[BASE] BASE
      foo(a ptr[in, templ1[int64]])
    
    Signed-off-by: Paul Chaignon <paul.chaignon@orange.com>

--- pkg/compiler/check.go
+++ pkg/compiler/check.go
@@ -942,7 +942,7 @@ func (comp *compiler) instantiate(templ ast.Node, params []*ast.Ident, args []*a
 	}
 	argUsed := make(map[string]bool)
 	err0 := comp.errors
-	templ.Walk(ast.PostRecursive(func(n ast.Node) {
+	ast.PostRecursive(func(n ast.Node) {
 		templArg, ok := n.(*ast.Type)
 		if !ok {
 			return
@@ -972,7 +972,7 @@ func (comp *compiler) instantiate(templ ast.Node, params []*ast.Ident, args []*a
 				col.Pos = concreteArg.Pos
 			}
 		}
-	}))
+	})(templ)
 	for _, param := range params {
 		if !argUsed[param.Name] {
 			comp.error(argMap[param.Name].Pos,
