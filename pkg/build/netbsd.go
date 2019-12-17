8ca23f0f72b9a5cca1b52aae6e02c6c5226279bf
Siddharth Muralee <siddharth.muralee@gmail.com>
2019-12-17 13:20:50 +0000

    pkg/build: Fix always true case

--- pkg/build/netbsd.go
+++ pkg/build/netbsd.go
@@ -28,19 +28,9 @@ func (ctx netbsd) build(params *Params) error {
 	if err := osutil.WriteFile(filepath.Join(confDir, kernelName), params.Config); err != nil {
 		return err
 	}
-	// Build tools before building kernel
-	if strings.HasSuffix(params.Compiler, "g++") {
-		if _, err := osutil.RunCmd(10*time.Minute, params.KernelDir, "./build.sh", "-m", params.TargetArch,
-			"-U", "-u", "-j"+strconv.Itoa(runtime.NumCPU()), "-V", "MKCTF=no", "tools"); err != nil {
-			return err
-		}
 
-		// Build kernel
-		if _, err := osutil.RunCmd(10*time.Minute, params.KernelDir, "./build.sh", "-m", params.TargetArch,
-			"-U", "-u", "-j"+strconv.Itoa(runtime.NumCPU()), "-V", "MKCTF=no", "kernel="+kernelName); err != nil {
-			return err
-		}
-	} else if strings.HasSuffix(params.Compiler, "clang++") {
+	if strings.HasSuffix(params.Compiler, "clang++") {
+		// Build tools before building kernel
 		if _, err := osutil.RunCmd(10*time.Minute, params.KernelDir, "./build.sh", "-m", params.TargetArch,
 			"-U", "-u", "-j"+strconv.Itoa(runtime.NumCPU()), "-V", "MKCTF=no",
 			"-V", "MKLLVM=yes", "-V", "MKGCC=no", "-V", "HAVE_LLVM=yes", "tools"); err != nil {
@@ -51,6 +41,16 @@ func (ctx netbsd) build(params *Params) error {
 		if _, err := osutil.RunCmd(10*time.Minute, params.KernelDir, "./build.sh", "-m", params.TargetArch,
 			"-U", "-u", "-j"+strconv.Itoa(runtime.NumCPU()), "-V", "MKCTF=no",
 			"-V", "MKLLVM=yes", "-V", "MKGCC=no", "-V", "HAVE_LLVM=yes", "kernel="+kernelName); err != nil {
+			return err
+		}
+	} else if strings.HasSuffix(params.Compiler, "g++") {
+		if _, err := osutil.RunCmd(10*time.Minute, params.KernelDir, "./build.sh", "-m", params.TargetArch,
+			"-U", "-u", "-j"+strconv.Itoa(runtime.NumCPU()), "-V", "MKCTF=no", "tools"); err != nil {
+			return err
+		}
+
+		if _, err := osutil.RunCmd(10*time.Minute, params.KernelDir, "./build.sh", "-m", params.TargetArch,
+			"-U", "-u", "-j"+strconv.Itoa(runtime.NumCPU()), "-V", "MKCTF=no", "kernel="+kernelName); err != nil {
 			return err
 		}
 	}
