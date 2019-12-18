dae1e0d4f51505d52b12781eaea06855a93908c4
Dmitry Vyukov <dvyukov@google.com>
2019-12-18 16:21:14 +0100

    pkg/ipc: don't use rate limiting for test os
    It's not needed for test os and slows down pkg/runtest tests.

--- pkg/ipc/ipc.go
+++ pkg/ipc/ipc.go
@@ -265,7 +265,7 @@ func (env *Env) Exec(opts *ExecOpts, p *prog.Prog) (output []byte, info *ProgInf
 
 	atomic.AddUint64(&env.StatExecs, 1)
 	if env.cmd == nil {
-		if targets.Get(p.Target.OS, p.Target.Arch).HostFuzzer {
+		if p.Target.OS != "test" && targets.Get(p.Target.OS, p.Target.Arch).HostFuzzer {
 			// The executor is actually ssh,
 			// starting them too frequently leads to timeouts.
 			<-rateLimit.C
