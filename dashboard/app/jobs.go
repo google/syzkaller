google/syzkaller

commit f8368f999a1964df6d39a225cd3f5ab3942dd755

dashboard/app: no reporting fix bisection results for already fixed bugs
* Modify pollCompletedJobs(); for bugs that are already marked as fixed,
invalid or duplicate do not report bisection results.
* Add TestNotReportingAlreadyFixed() to test that reporting does not
occur for already fixed bugs.

dashboard/app/jobs.go
@@ -583,6 +583,17 @@ func pollCompletedJobs(c context.Context, typ string) ([]*dashapi.BugReport, err
 		if job.Type == JobBisectFix && len(job.Commits) != 1 {
 			continue
 		}
+		// If the bug is already known to be fixed, invalid or duplicate, do not report the
+		// bisection results.
+		bug := new(Bug)
+		bugKey := keys[i].Parent()
+		if err := db.Get(c, bugKey, bug); err != nil {
+			return nil, fmt.Errorf("job %v: failed to get bug: %v", extJobID(keys[i]), err)
+		}
+		if len(bug.Commits) != 0 || bug.Status != BugStatusOpen {
+			jobReported(c, extJobID(keys[i]))
+			continue
+		}
 		rep, err := createBugReportForJob(c, job, keys[i], reporting.Config)
 		if err != nil {
 			log.Errorf(c, "failed to create report for job: %v", err)

dashboard/app/jobs_test.go
@@ -469,3 +469,84 @@ func TestBisectFixRetry(t *testing.T) {
 	}
 	c.client2.expectOK(c.client2.JobDone(done))
 }
+
+// Test that bisection results are not reported for bugs that are already marked as fixed.
+func TestNotReportingAlreadyFixed(t *testing.T) {
+	c := NewCtx(t)
+	defer c.Close()
+
+	// Upload a crash report.
+	build := testBuild(1)
+	c.client2.UploadBuild(build)
+	crash := testCrashWithRepro(build, 1)
+	c.client2.ReportCrash(crash)
+	c.client2.pollEmailBug()
+
+	// Receive the JobBisectCause.
+	resp := c.client2.pollJobs(build.Manager)
+	c.client2.expectNE(resp.ID, "")
+	c.client2.expectEQ(resp.Type, dashapi.JobBisectCause)
+	done := &dashapi.JobDoneReq{
+		ID:    resp.ID,
+		Error: []byte("testBisectFixRetry:JobBisectCause"),
+	}
+	c.client2.expectOK(c.client2.JobDone(done))
+
+	sender := ""
+	// Advance time by 30 days and read out any notification emails.
+	{
+		c.advanceTime(30 * 24 * time.Hour)
+		msg := c.client2.pollEmailBug()
+		c.expectEQ(msg.Subject, "title1")
+		c.expectTrue(strings.Contains(msg.Body, "Sending this report upstream."))
+
+		msg = c.client2.pollEmailBug()
+		c.expectEQ(msg.Subject, "title1")
+		c.expectTrue(strings.Contains(msg.Body, "syzbot found the following crash"))
+		sender = msg.Sender
+	}
+
+	// Poll for a BisectFix job.
+	resp = c.client2.pollJobs(build.Manager)
+	c.client2.expectNE(resp.ID, "")
+	c.client2.expectEQ(resp.Type, dashapi.JobBisectFix)
+
+	// Meanwhile, the bug is marked as fixed separately.
+	c.incomingEmail(sender, "#syz fix: kernel: add a fix", EmailOptCC(nil))
+
+	{
+		// Email notification of "Your 'fix:' command is accepted, but please keep
+		// bugs@syzkaller.com mailing list in CC next time."
+		c.client2.pollEmailBug()
+	}
+
+	// At this point, send back the results for the BisectFix job also.
+	done = &dashapi.JobDoneReq{
+		ID:          resp.ID,
+		Build:       *build,
+		Log:         []byte("bisectfix log 4"),
+		CrashTitle:  "bisectfix crash title 4",
+		CrashLog:    []byte("bisectfix crash log 4"),
+		CrashReport: []byte("bisectfix crash report 4"),
+		Commits: []dashapi.Commit{
+			{
+				Hash:       "46e65cb4a0448942ec316b24d60446bbd5cc7827",
+				Title:      "kernel: add a fix",
+				Author:     "author@kernel.org",
+				AuthorName: "Author Kernelov",
+				CC: []string{
+					"reviewer1@kernel.org", "\"Reviewer2\" <reviewer2@kernel.org>",
+					// These must be filtered out:
+					"syzbot@testapp.appspotmail.com",
+					"syzbot+1234@testapp.appspotmail.com",
+					"\"syzbot\" <syzbot+1234@testapp.appspotmail.com>",
+				},
+				Date: time.Date(2000, 2, 9, 4, 5, 6, 7, time.UTC),
+			},
+		},
+	}
+	c.expectOK(c.client2.JobDone(done))
+
+	// No reporting should come in at this point. If there is reporting, c.Close()
+	// will fail.
+}

commit 2b98fdbcbcac6e99d12c88857406ef446bcac872

dashboard/app: fix send of replies for patch testing
We used to send replies even if bug has fixing commits.
Was broken in f8368f999a1964df6d39a225cd3f5ab3942dd755
and we did not have a test. Add a test and restore old
behavior.

dashboard/app/jobs.go
@@ -581,23 +581,24 @@ func pollCompletedJobs(c context.Context, typ string) ([]*dashapi.BugReport, err
 		if reporting.Config.Type() != typ {
 			continue
 		}
-		if !notifyAboutUnsuccessfulBisections && job.Type == JobBisectCause && len(job.Commits) != 1 {
+		if job.Type == JobBisectCause && !notifyAboutUnsuccessfulBisections && len(job.Commits) != 1 {
 			continue
 		}
 		// If BisectFix results in a crash on HEAD, no notification is sent out.
 		if job.Type == JobBisectFix && len(job.Commits) != 1 {
 			continue
 		}
-		// If the bug is already known to be fixed, invalid or duplicate, do not report the
-		// bisection results.
-		bug := new(Bug)
-		bugKey := keys[i].Parent()
-		if err := db.Get(c, bugKey, bug); err != nil {
-			return nil, fmt.Errorf("job %v: failed to get bug: %v", extJobID(keys[i]), err)
-		}
-		if len(bug.Commits) != 0 || bug.Status != BugStatusOpen {
-			jobReported(c, extJobID(keys[i]))
-			continue
+		// If the bug is already known to be fixed, invalid or duplicate, do not report the bisection results.
+		if job.Type == JobBisectCause || job.Type == JobBisectFix {
+			bug := new(Bug)
+			bugKey := keys[i].Parent()
+			if err := db.Get(c, bugKey, bug); err != nil {
+				return nil, fmt.Errorf("job %v: failed to get bug: %v", extJobID(keys[i]), err)
+			}
+			if len(bug.Commits) != 0 || bug.Status != BugStatusOpen {
+				jobReported(c, extJobID(keys[i]))
+				continue
+			}
 		}
 		rep, err := createBugReportForJob(c, job, keys[i], reporting.Config)
 		if err != nil {

dashboard/app/jobs_test.go
@@ -286,7 +286,11 @@ func TestJobWithoutPatch(t *testing.T) {
 	_, extBugID, err := email.RemoveAddrContext(sender)
 	c.expectOK(err)
 
+	// Patch testing should happen for bugs with fix commits too.
+	c.incomingEmail(sender, "#syz fix: some commit title\n")
+
 	c.incomingEmail(sender, "#syz test git://mygit.com/git.git 5e6a2eea\n", EmailOptMessageID(1))
+	c.expectNoEmail()
 	pollResp := c.client2.pollJobs(build.Manager)
 	c.expectEQ(pollResp.Type, dashapi.JobTestPatch)
 	testBuild := testBuild(2)