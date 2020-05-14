13f2da2924273bc0054e68ffd423264fc3ab74c4
Dmitry Vyukov <dvyukov@google.com>
2020-05-14 13:33:22 +0200

    tools/check-copyright.sh: check py and yml files
    Update #1699

--- tools/check-copyright.sh
+++ tools/check-copyright.sh
@@ -5,7 +5,8 @@
 FILES=0
 FAILED=""
 for F in $(find . -name "*.go" -o -name "*.sh" -o -name "*.cc" -o -name "*.h" \
-	-o -name "*.S" -o \( -path "./sys/*/*.txt" \) | egrep -v "/vendor/|/gen/"); do
+	-o -name "*.S" -o -name "*.py" -o -name "*.yml" -o -name "*.yaml" \
+	-o \( -path "./sys/*/*.txt" \) | egrep -v "/vendor/|/gen/"); do
 	((FILES+=1))
 	cat $F | tr '\n' '_' | egrep "(//|#) Copyright 20[0-9]{2}(/20[0-9]{2})? syzkaller project authors\. All rights reserved\._(//|#) Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file\." >/dev/null
 	if [ $? -eq 0 ]; then continue; fi
@@ -15,7 +16,7 @@ for F in $(find . -name "*.go" -o -name "*.sh" -o -name "*.cc" -o -name "*.h" \
 	# Ignore untracked files.
 	git ls-files --error-unmatch $F >/dev/null 2>&1
 	if [ $? -ne 0 ]; then continue; fi
-	echo "$F: does not have standard copyright statement"
+	echo "$F: does not have the standard copyright statement (please add)"
 	FAILED="1"
 done
 if [ "$FAILED" != "" ]; then exit 1; fi
