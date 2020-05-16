672c10ad7c2491ae3ab24736fc153e2ecb4c7044
Dmitry Vyukov <dvyukov@google.com>
2020-05-16 07:05:33 +0200

    all: add missing copyright statements

--- tools/check_links.py
+++ tools/check_links.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+# Copyright 2017 syzkaller project authors. All rights reserved.
+# Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
 
 from __future__ import print_function
 import os
