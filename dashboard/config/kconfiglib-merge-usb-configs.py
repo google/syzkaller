0e1c96bd5525330fc402f8283174db7d725aeb5f
Dmitry Vyukov <dvyukov@google.com>
2020-05-16 07:05:33 +0200

    all: add missing copyright statements

--- dashboard/config/kconfiglib-merge-usb-configs.py
+++ dashboard/config/kconfiglib-merge-usb-configs.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python
+# Copyright 2019 syzkaller project authors. All rights reserved.
+# Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
 
 from __future__ import print_function
 
