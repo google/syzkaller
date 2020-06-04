153758d593f97416e0bf832986524447f05d5b64
Dmitry Vyukov <dvyukov@google.com>
2020-06-04 22:04:02 +0200

    .github/workflows: support errors without :column
    Also transform:
    
    file.go:line: error message
    
    to the github format.

--- .github/workflows/run.sh
+++ .github/workflows/run.sh
@@ -8,4 +8,4 @@ set -o pipefail
 # Run the specified command in syz-env and convert error messages to github format:
 # https://help.github.com/en/actions/reference/workflow-commands-for-github-actions#setting-an-error-message
 gopath/src/github.com/google/syzkaller/tools/$1 "${@:2}" | \
-	sed -E 's#\s*(.+):([0-9]+):([0-9]+): (.+)#\0\n::error file=\1,line=\2,col=\3::\4#'
+	sed -E 's#\s*([a-zA-Z0-9._/-]+):([0-9]+):(([0-9]+):)? (.+)#\0\n::error file=\1,line=\2,col=0\4::\5#'
