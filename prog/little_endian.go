d01fc6f500384cdc38f80cbcca0290eb560c9d11
Tobias Klauser <tklauser@distanz.ch>
2020-06-25 18:03:34 +0200

    prog: add support for riscv64
    This will allow to run `make TARGETVMARCH=riscv64`.
    
    Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

--- prog/little_endian.go
+++ prog/little_endian.go
@@ -1,7 +1,7 @@
 // Copyright 2020 syzkaller project authors. All rights reserved.
 // Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
 
-// +build amd64 386 arm64 arm mips64le ppc64le
+// +build amd64 386 arm64 arm mips64le ppc64le riscv64
 
 package prog
 
