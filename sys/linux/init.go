eccf619f434ff4d8c71e2b10740ba25654a9fca6
Tobias Klauser <tklauser@distanz.ch>
2020-06-25 18:33:18 +0200

    sys/linux: add initial support for riscv64
    Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

--- sys/linux/init.go
+++ sys/linux/init.go
@@ -82,7 +82,7 @@ func InitTarget(target *prog.Target) {
 		target.SpecialPointers = []uint64{
 			0xffffffff81000000, // kernel text
 		}
-	case "386", "arm64", "arm", "ppc64le", "mips64le", "s390x":
+	case "386", "arm64", "arm", "ppc64le", "mips64le", "s390x", "riscv64":
 	default:
 		panic("unknown arch")
 	}
