a57e50f790c5698be3322177270410ac572e111f
Dmitry Vyukov <dvyukov@google.com>
2020-07-02 18:38:27 +0200

    tools/syz-linter: add custom linter
    For now we have 2 simple checks:
    
    1. for multiline comments:
    /* */ -> //
    
    2. for string len comparison with 0:
    len(str) != 0 -> str != ""
    
    Update #1876

---
+++ tools/syz-linter/testdata/src/lintertest/lintertest.go
@@ -0,0 +1,19 @@
+// Copyright 2020 syzkaller project authors. All rights reserved.
+// Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
+
+package lintertest
+
+/* some comment */ // want "Use C-style comments // instead of /* */"
+var comment = 1    /* some comment */ // want "Use C-style comments // instead of /* */"
+
+func stringComparison() {
+	str := ""
+	if len(str) == 0 { // want "Compare string with \"\", don't compare len with 0"
+	}
+	if 0 != len(str) { // want "Compare string with \"\", don't compare len with 0"
+	}
+	if len(returnString()+"foo") > 0 { // want "Compare string with \"\", don't compare len with 0"
+	}
+}
+
+func returnString() string { return "foo" }
