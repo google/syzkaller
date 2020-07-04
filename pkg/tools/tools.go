c7d7f10bdff703e4a3c0414e8a33d4e45c91eb35
Dmitry Vyukov <dvyukov@google.com>
2020-07-04 11:12:55 +0200

    go.mod: vendor golangci-lint

---
+++ pkg/tools/tools.go
@@ -0,0 +1,13 @@
+// Copyright 2020 syzkaller project authors. All rights reserved.
+// Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
+
+// +build tools
+
+// tools is not a normal package, it's only purpose is tools dependency management.
+// It allows us to vendor all used tools, ensure that all contributors have the same versions of tools,
+// and have custom golangci-lint checkers.
+package tools
+
+import (
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+)
