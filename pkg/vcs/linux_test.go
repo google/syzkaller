ef6b94b52219de122b7848a031dbcae34ca80965
Dmitry Vyukov <dvyukov@google.com>
2020-07-07 08:36:58 +0200

    pkg/vcs: fix config minimization test
    The test requires new git binary.
    It's not necessary installed everywhere.
    Skip the test if git binary does not support bisection.
    
    Also reoder functions from more important to less important.
    Mark the test as parallel.
    Move individual tests into subtests.
    Split trace per subtest.

--- pkg/vcs/linux_test.go
+++ pkg/vcs/linux_test.go
@@ -5,6 +5,7 @@ package vcs
 
 import (
 	"bytes"
+	"fmt"
 	"io/ioutil"
 	"os"
 	"strings"
@@ -22,12 +23,71 @@ type MinimizationTest struct {
 	passing bool
 }
 
+func TestConfigMinimizer(t *testing.T) {
+	t.Parallel()
+	tests := []MinimizationTest{
+		{
+			config:         "CONFIG_ORIGINAL=y",
+			baselineConfig: "CONFIG_FAILING=y",
+			expectedConfig: "CONFIG_ORIGINAL=y",
+			passing:        false,
+		},
+		{
+			config:         "CONFIG_ORIGINAL=y",
+			baselineConfig: "CONFIG_REPRODUCES_CRASH=y",
+			expectedConfig: "CONFIG_REPRODUCES_CRASH=y",
+			passing:        true,
+		},
+		{
+			config:         configBisectTag,
+			baselineConfig: "CONFIG_NOT_REPRODUCE_CRASH=y",
+			expectedConfig: configBisectTag,
+			passing:        true,
+		},
+	}
+
+	baseDir := createTestLinuxRepo(t)
+	repo, err := NewRepo("linux", "64", baseDir)
+	if err != nil {
+		t.Fatalf("Unable to create repository")
+	}
+	pred := func(test []byte) (BisectResult, error) {
+		if strings.Contains(string(test), "CONFIG_REPRODUCES_CRASH=y") {
+			return BisectBad, nil
+		}
+		return BisectGood, nil
+	}
+
+	minimizer, ok := repo.(ConfigMinimizer)
+	if !ok {
+		t.Fatalf("Config minimization is not implemented")
+	}
+	for i, test := range tests {
+		t.Run(fmt.Sprint(i), func(t *testing.T) {
+			trace := new(bytes.Buffer)
+			outConfig, err := minimizer.Minimize([]byte(test.config),
+				[]byte(test.baselineConfig), trace, pred)
+			if test.passing && err != nil {
+				t.Fatalf("failed to run Minimize: %v", err)
+			} else if test.passing && !strings.Contains(string(outConfig),
+				test.expectedConfig) {
+				t.Fatalf("output is not expected %v vs. %v", string(outConfig),
+					test.expectedConfig)
+			}
+			t.Log(trace.String())
+		})
+	}
+}
+
 func createTestLinuxRepo(t *testing.T) string {
 	baseDir, err := ioutil.TempDir("", "syz-config-bisect-test")
 	if err != nil {
 		t.Fatal(err)
 	}
 	repo := CreateTestRepo(t, baseDir, "")
+	if !repo.SupportsBisection() {
+		t.Skip("bisection is unsupported by git (probably too old version)")
+	}
 	repo.CommitChange("commit")
 	repo.SetTag("v4.1")
 	err = os.MkdirAll(baseDir+"/tools/testing/ktest", 0755)
@@ -52,57 +112,4 @@ func createTestLinuxRepo(t *testing.T) string {
 	}
 
 	return baseDir
-}
-
-func TestMinimizationResults(t *testing.T) {
-	tests := []MinimizationTest{
-		{
-			config:         "CONFIG_ORIGINAL=y",
-			baselineConfig: "CONFIG_FAILING=y",
-			expectedConfig: "CONFIG_ORIGINAL=y",
-			passing:        false,
-		},
-		{
-			config:         "CONFIG_ORIGINAL=y",
-			baselineConfig: "CONFIG_REPRODUCES_CRASH=y",
-			expectedConfig: "CONFIG_REPRODUCES_CRASH=y",
-			passing:        true,
-		},
-		{
-			config:         configBisectTag,
-			baselineConfig: "CONFIG_NOT_REPRODUCE_CRASH=y",
-			expectedConfig: configBisectTag,
-			passing:        true,
-		},
-	}
-
-	trace := new(bytes.Buffer)
-	baseDir := createTestLinuxRepo(t)
-	repo, err := NewRepo("linux", "64", baseDir)
-	if err != nil {
-		t.Fatalf("Unable to create repository")
-	}
-	pred := func(test []byte) (BisectResult, error) {
-		if strings.Contains(string(test), "CONFIG_REPRODUCES_CRASH=y") {
-			return BisectBad, nil
-		}
-		return BisectGood, nil
-	}
-
-	minimizer, ok := repo.(ConfigMinimizer)
-	if !ok {
-		t.Fatalf("Config minimization is not implemented")
-	}
-	for _, test := range tests {
-		outConfig, err := minimizer.Minimize([]byte(test.config),
-			[]byte(test.baselineConfig), trace, pred)
-		if test.passing && err != nil {
-			t.Fatalf("failed to run Minimize: %v", err)
-		} else if test.passing && !strings.Contains(string(outConfig),
-			test.expectedConfig) {
-			t.Fatalf("output is not expected %v vs. %v", string(outConfig),
-				test.expectedConfig)
-		}
-	}
-	t.Log(trace.String())
 }
