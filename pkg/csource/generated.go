df960cfb7539afa969a592fbc4d049e0c3ba6f46
Dmitry Vyukov <dvyukov@google.com>
2020-10-25 12:12:10 +0100

    pkg/csource: setup sysctl's in C reproducers
    Sysctl's are not captured as part of reproducers.
    This can result in failure to reproduce a bug on developer machine.
    Include sysctl setup as part of C reproducers.

--- pkg/csource/generated.go
+++ pkg/csource/generated.go
@@ -9227,6 +9227,31 @@ static void setup_usb()
 }
 #endif
 
+#if SYZ_EXECUTOR || SYZ_SYSCTL
+static void setup_sysctl()
+{
+	static struct {
+		const char* name;
+		const char* data;
+	} files[] = {
+	    {"/sys/kernel/debug/x86/nmi_longest_ns", "10000000000"},
+	    {"/proc/sys/kernel/watchdog_thresh", "55"},
+	    {"/proc/sys/kernel/hung_task_check_interval_secs", "20"},
+	    {"/proc/sys/net/core/bpf_jit_enable", "1"},
+	    {"/proc/sys/net/core/bpf_jit_kallsyms", "1"},
+	    {"/proc/sys/net/core/bpf_jit_harden", "0"},
+	    {"/proc/sys/kernel/kptr_restrict", "0"},
+	    {"/proc/sys/kernel/softlockup_all_cpu_backtrace", "1"},
+	    {"/proc/sys/fs/mount-max", "100"},
+	    {"/proc/sys/vm/oom_dump_tasks", "0"},
+	};
+	for (size_t i = 0; i < sizeof(files) / sizeof(files[0]); i++) {
+		if (!write_file(files[i].name, files[i].data))
+			printf("write to %s failed: %s\n", files[i].name, strerror(errno));
+	}
+}
+#endif
+
 #if GOARCH_s390x
 #include <sys/mman.h>
 #define CAST(f) ({void* p = (void*)f; p; })
@@ -10106,6 +10131,9 @@ int main(void)
 	/*{{{MMAP_DATA}}}*/
 #endif
 
+#if SYZ_SYSCTL
+	setup_sysctl();
+#endif
 #if SYZ_BINFMT_MISC
 	setup_binfmt_misc();
 #endif
