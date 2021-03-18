2d7868612d9f860559d881b25c787d689d1de4c4
Dmitry Vyukov <dvyukov@google.com>
2021-03-18 11:09:37 +0100

    pkg/host: don't include empty machine info sections

--- pkg/host/machine_info.go
+++ pkg/host/machine_info.go
@@ -13,15 +13,20 @@ import (
 func CollectMachineInfo() ([]byte, error) {
 	buf := new(bytes.Buffer)
 	for _, pair := range machineInfoFuncs {
+		pos0 := buf.Len()
 		fmt.Fprintf(buf, "[%s]\n", pair.name)
+		pos1 := buf.Len()
 		err := pair.fn(buf)
 		if err != nil {
 			if !os.IsNotExist(err) {
 				return nil, err
 			}
-			fmt.Fprintf(buf, "%v\n", err)
 		}
-		fmt.Fprintf(buf, "%v\n\n", strings.Repeat("-", 80))
+		if buf.Len() == pos1 {
+			buf.Truncate(pos0)
+			continue
+		}
+		fmt.Fprintf(buf, "\n%v\n\n", strings.Repeat("-", 80))
 	}
 	return buf.Bytes(), nil
 }
