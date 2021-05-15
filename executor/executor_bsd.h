918efccedbfe2e5979d4f71ae77ce4dc660b4b5f
Patrick Meyer <meyerpatrick@google.com>
2021-05-15 12:27:06 +0200

    executor: remove unreachable nocover.h include

--- executor/executor_bsd.h
+++ executor/executor_bsd.h
@@ -4,6 +4,7 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <sys/ioctl.h>
+#include <sys/kcov.h>
 #include <sys/mman.h>
 #include <sys/resource.h>
 #include <sys/stat.h>
@@ -47,13 +48,6 @@ static intptr_t execute_syscall(const call_t* c, intptr_t a[kMaxArgs])
 		return c->call(a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8]);
 	return __syscall(c->sys_nr, a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8]);
 }
-
-#if GOOS_freebsd || GOOS_openbsd || GOOS_netbsd
-
-// KCOV support was added to FreeBSD in https://svnweb.freebsd.org/changeset/base/342962
-// KCOV support added to NetBSD in https://github.com/NetBSD/src/commit/080a26f01d79ded8a2154c128950d1d663c6ca88
-
-#include <sys/kcov.h>
 
 static void cover_open(cover_t* cov, bool extra)
 {
@@ -172,9 +166,6 @@ static bool use_cover_edges(uint64 pc)
 {
 	return true;
 }
-#else
-#include "nocover.h"
-#endif
 
 #if GOOS_netbsd
 #define SYZ_HAVE_FEATURES 1
