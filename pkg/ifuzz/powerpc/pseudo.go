2f239304ab105b0c2e38bbc355c284e4af43cf4e
Alexey Kardashevskiy <aik@linux.ibm.com>
2021-07-12 14:48:15 +1000

    pkg/ifuzz/powerpc: preload registers with interesting numbers
    GenerateInt() generates sort of random numbers for instruction fuzzer
    with focus on corner cases, let's use it for POWERPC too.
    
    Since we want memory access instruction to try these addresses, we preload
    generated values in GPRs used by just generated instruction.
    This in turn requires Insn::Encode() access for the instruction map to
    encode load instructions so this moves ld64() from the generator to
    insnSetMap and adds Insn::insnMap. This adds enc() to encode just
    the instruction without any randomization.
    
    This does not add additional instructions if cfg.MemRegions is empty so
    the ifuzz_test.go test still passes.
    
    Since EncodeParam() is not used by anything but Encode(), this open codes
    it.
    
    Signed-off-by: Alexey Kardashevskiy <aik@linux.ibm.com>

--- pkg/ifuzz/powerpc/pseudo.go
+++ pkg/ifuzz/powerpc/pseudo.go
@@ -62,48 +62,12 @@ func (gen *generator) byte(v []byte) {
 	gen.text = append(gen.text, v...)
 }
 
-func (gen *generator) ld64(reg uint, v uint64) {
-	// This is a widely used macro to load immediate on ppc64
-	// #define LOAD64(rn,name)
-	//	addis   rn,0,name##@highest \ lis     rn,name##@highest
-	//	ori     rn,rn,name##@higher
-	//	rldicr  rn,rn,32,31
-	//	oris    rn,rn,name##@h
-	//	ori     rn,rn,name##@l
-	gen.byte(gen.imap["addis"].EncodeParam(map[string]uint{
-		"RT": reg,
-		"RA": 0, // In "addis", '0' means 0, not GPR0 .
-		"SI": uint((v >> 48) & 0xffff)},
-		nil))
-	gen.byte(gen.imap["ori"].EncodeParam(map[string]uint{
-		"RA": reg,
-		"RS": reg,
-		"UI": uint((v >> 32) & 0xffff)},
-		nil))
-	gen.byte(gen.imap["rldicr"].EncodeParam(map[string]uint{
-		"RA": reg,
-		"RS": reg,
-		"SH": 32,
-		"ME": 31},
-		nil))
-	gen.byte(gen.imap["oris"].EncodeParam(map[string]uint{
-		"RA": reg,
-		"RS": reg,
-		"UI": uint((v >> 16) & 0xffff)},
-		nil))
-	gen.byte(gen.imap["ori"].EncodeParam(map[string]uint{
-		"RA": reg,
-		"RS": reg,
-		"UI": uint(v & 0xffff)},
-		nil))
-}
-
 func (gen *generator) sc(lev uint) {
 	n := gen.r.Intn(9)
 	// Valid hcall humbers at the momemt are: 4..0x450
-	gen.ld64(3, uint64(gen.r.Intn(4+(0x450-4)/4)))
+	gen.byte(gen.imap.ld64(3, uint64(gen.r.Intn(4+(0x450-4)/4))))
 	for i := 4; i < n+4; i++ {
-		gen.ld64(uint(i), gen.r.Uint64())
+		gen.byte(gen.imap.ld64(uint(i), gen.r.Uint64()))
 	}
-	gen.byte(gen.imap["sc"].EncodeParam(map[string]uint{"LEV": lev}, nil))
+	gen.byte(gen.imap["sc"].enc(map[string]uint{"LEV": lev}))
 }
