0b99b55b11663ec9652e003bdf8e0eb6ad799ba0
Joey Jiao <joeyjiaojg@gmail.com>
2021-07-14 15:37:58 +0800

    pkg/cover: fix low covered pc for module

--- pkg/cover/html.go
+++ pkg/cover/html.go
@@ -24,9 +24,11 @@ import (
 	"github.com/google/syzkaller/sys/targets"
 )
 
-func fixUpPCs(target string, progs []Prog, coverFilter map[uint32]uint32) []Prog {
+func (rg *ReportGenerator) fixUpPCs(target string, progs []Prog, coverFilter map[uint32]uint32) []Prog {
+	nProgs := make([]Prog, len(progs))
+	copy(nProgs, progs)
 	if coverFilter != nil {
-		for _, prog := range progs {
+		for i, prog := range nProgs {
 			var nPCs []uint64
 			for _, pc := range prog.PCs {
 				if coverFilter[uint32(pc)] != 0 {
@@ -34,6 +36,7 @@ func fixUpPCs(target string, progs []Prog, coverFilter map[uint32]uint32) []Prog
 				}
 			}
 			prog.PCs = nPCs
+			nProgs[i] = prog
 		}
 	}
 
@@ -41,23 +44,23 @@ func fixUpPCs(target string, progs []Prog, coverFilter map[uint32]uint32) []Prog
 	// so there is 0x18 bytes offset from module load address for .text section
 	// we need to remove the 0x18 bytes offset in order to correct module symbol address
 	if target == targets.ARM64 {
-		for _, prog := range progs {
+		for i, prog := range nProgs {
 			var nPCs []uint64
 			for _, pc := range prog.PCs {
-				// TODO: avoid to hardcode the address
-				if pc < 0xffffffd010000000 {
+				if pc < rg.BaseAddr && pc > ((rg.BaseAddr>>32)<<32) {
 					pc -= 0x18
 				}
 				nPCs = append(nPCs, pc)
 			}
 			prog.PCs = nPCs
+			nProgs[i] = prog
 		}
 	}
-	return progs
+	return nProgs
 }
 
 func (rg *ReportGenerator) DoHTML(w io.Writer, progs []Prog, coverFilter map[uint32]uint32) error {
-	progs = fixUpPCs(rg.target.Arch, progs, coverFilter)
+	progs = rg.fixUpPCs(rg.target.Arch, progs, coverFilter)
 	files, err := rg.prepareFileMap(progs)
 	if err != nil {
 		return err
@@ -141,7 +144,7 @@ func (rg *ReportGenerator) DoHTML(w io.Writer, progs []Prog, coverFilter map[uin
 }
 
 func (rg *ReportGenerator) DoRawCoverFiles(w http.ResponseWriter, progs []Prog, coverFilter map[uint32]uint32) error {
-	progs = fixUpPCs(rg.target.Arch, progs, coverFilter)
+	progs = rg.fixUpPCs(rg.target.Arch, progs, coverFilter)
 	if err := rg.lazySymbolize(progs); err != nil {
 		return err
 	}
@@ -151,7 +154,7 @@ func (rg *ReportGenerator) DoRawCoverFiles(w http.ResponseWriter, progs []Prog, 
 
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	buf := bufio.NewWriter(w)
-	fmt.Fprintf(buf, "PC,Module,Offset,Filename,StartLine,EndLine\n")
+	fmt.Fprintf(buf, "PC,Module,Offset,Filename,StartLine\n")
 	for _, frame := range rg.Frames {
 		offset := frame.PC - frame.Module.Addr
 		fmt.Fprintf(buf, "0x%x,%v,0x%x,%v,%v\n", frame.PC, frame.Module.Name, offset, frame.Name, frame.StartLine)
@@ -161,7 +164,7 @@ func (rg *ReportGenerator) DoRawCoverFiles(w http.ResponseWriter, progs []Prog, 
 }
 
 func (rg *ReportGenerator) DoRawCover(w http.ResponseWriter, progs []Prog, coverFilter map[uint32]uint32) {
-	progs = fixUpPCs(rg.target.Arch, progs, coverFilter)
+	progs = rg.fixUpPCs(rg.target.Arch, progs, coverFilter)
 	var pcs []uint64
 	uniquePCs := make(map[uint64]bool)
 	for _, prog := range progs {
@@ -186,7 +189,7 @@ func (rg *ReportGenerator) DoRawCover(w http.ResponseWriter, progs []Prog, cover
 }
 
 func (rg *ReportGenerator) DoFilterPCs(w http.ResponseWriter, progs []Prog, coverFilter map[uint32]uint32) {
-	progs = fixUpPCs(rg.target.Arch, progs, coverFilter)
+	progs = rg.fixUpPCs(rg.target.Arch, progs, coverFilter)
 	var pcs []uint64
 	uniquePCs := make(map[uint64]bool)
 	for _, prog := range progs {
@@ -290,7 +293,7 @@ func (rg *ReportGenerator) convertToStats(progs []Prog) ([]fileStats, error) {
 }
 
 func (rg *ReportGenerator) DoCSVFiles(w io.Writer, progs []Prog, coverFilter map[uint32]uint32) error {
-	progs = fixUpPCs(rg.target.Arch, progs, coverFilter)
+	progs = rg.fixUpPCs(rg.target.Arch, progs, coverFilter)
 	data, err := rg.convertToStats(progs)
 	if err != nil {
 		return err
@@ -389,7 +392,7 @@ func groupCoverByFilePrefixes(datas []fileStats, subsystems []mgrconfig.Subsyste
 }
 
 func (rg *ReportGenerator) DoHTMLTable(w io.Writer, progs []Prog, coverFilter map[uint32]uint32) error {
-	progs = fixUpPCs(rg.target.Arch, progs, coverFilter)
+	progs = rg.fixUpPCs(rg.target.Arch, progs, coverFilter)
 	data, err := rg.convertToStats(progs)
 	if err != nil {
 		return err
@@ -465,7 +468,7 @@ func groupCoverByModule(datas []fileStats) map[string]map[string]string {
 }
 
 func (rg *ReportGenerator) DoModuleCover(w io.Writer, progs []Prog, coverFilter map[uint32]uint32) error {
-	progs = fixUpPCs(rg.target.Arch, progs, coverFilter)
+	progs = rg.fixUpPCs(rg.target.Arch, progs, coverFilter)
 	data, err := rg.convertToStats(progs)
 	if err != nil {
 		return err
@@ -484,7 +487,7 @@ var csvHeader = []string{
 }
 
 func (rg *ReportGenerator) DoCSV(w io.Writer, progs []Prog, coverFilter map[uint32]uint32) error {
-	progs = fixUpPCs(rg.target.Arch, progs, coverFilter)
+	progs = rg.fixUpPCs(rg.target.Arch, progs, coverFilter)
 	files, err := rg.prepareFileMap(progs)
 	if err != nil {
 		return err
