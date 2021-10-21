8b121c0666450f5ce790b56bf9bb672c79d8b925
Dmitry Vyukov <dvyukov@google.com>
2021-10-21 19:47:17 +0200

    pkg/tools: add empty.go
    Add empty.go to unbreak bazel+glaze build.
    Otherwise the package does not have any files not excluded by build constraints.

---
+++ pkg/tools/empty.go
@@ -0,0 +1,6 @@
+// Copyright 2021 syzkaller project authors. All rights reserved.
+// Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
+
+package tools
+
+// Empty file to unbreak bazel+glaze build (tools.go is excluded by build constraints).
--- pkg/tools/tools.go
+++ pkg/tools/tools.go
@@ -1,6 +1,7 @@
 // Copyright 2020 syzkaller project authors. All rights reserved.
 // Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
 
+//go:build tools
 // +build tools
 
 // tools is not a normal package, it's only purpose is tools dependency management.
