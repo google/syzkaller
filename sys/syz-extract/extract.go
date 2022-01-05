d3000b647fea6064feb770f6ee7f2df1ef218a8b
Dmitry Vyukov <dvyukov@google.com>
2022-01-05 16:01:21 +0100

    sys/linux: remove /dev/ion descriptions
    The comment says:
    
    // ION support was removed from kernel.
    // We plan to leave the descriptions for some time as is and later remove them.

--- sys/syz-extract/extract.go
+++ sys/syz-extract/extract.go
@@ -272,9 +272,6 @@ func archFileList(os, arch string, files []string) (string, []string, []string, 
 			// Not upstream, generated on:
 			// https://chromium.googlesource.com/chromiumos/third_party/kernel d2a8a1eb8b86
 			"dev_bifrost.txt": true,
-			// ION support was removed from kernel.
-			// We plan to leave the descriptions for some time as is and later remove them.
-			"dev_ion.txt": true,
 			// Not upstream, generated on unknown tree.
 			"dev_img_rogue.txt": true,
 		}
