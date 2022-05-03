8260364a5719396093f8c2ac46a432036fedf4ab
Dmitry Vyukov <dvyukov@google.com>
2022-05-03 14:13:39 +0200

    dashboard/app: don't show Uptime is manager is inactive
    Currnetly for inactive managers we show "Active", say, 10 days ago in red,
    but still show Uptime. It's wrong and confusing. Don't show Uptime for
    inactive managers.

--- dashboard/app/main.go
+++ dashboard/app/main.go
@@ -90,7 +90,6 @@ type uiManager struct {
 	FailedBuildBugLink    string
 	FailedSyzBuildBugLink string
 	LastActive            time.Time
-	LastActiveBad         bool // highlight LastActive in red
 	CurrentUpTime         time.Duration
 	MaxCorpus             int64
 	MaxCover              int64
@@ -1075,6 +1074,10 @@ func loadManagers(c context.Context, accessLevel AccessLevel, ns, manager string
 		if accessLevel < AccessUser {
 			link = ""
 		}
+		uptime := mgr.CurrentUpTime
+		if now.Sub(mgr.LastAlive) > 6*time.Hour {
+			uptime = 0
+		}
 		ui := &uiManager{
 			Now:                   timeNow(c),
 			Namespace:             mgr.Namespace,
@@ -1085,8 +1088,7 @@ func loadManagers(c context.Context, accessLevel AccessLevel, ns, manager string
 			FailedBuildBugLink:    bugLink(mgr.FailedBuildBug),
 			FailedSyzBuildBugLink: bugLink(mgr.FailedSyzBuildBug),
 			LastActive:            mgr.LastAlive,
-			LastActiveBad:         now.Sub(mgr.LastAlive) > 6*time.Hour,
-			CurrentUpTime:         mgr.CurrentUpTime,
+			CurrentUpTime:         uptime,
 			MaxCorpus:             stats.MaxCorpus,
 			MaxCover:              stats.MaxCover,
 			TotalFuzzingTime:      stats.TotalFuzzingTime,
@@ -1100,7 +1102,6 @@ func loadManagers(c context.Context, accessLevel AccessLevel, ns, manager string
 			ui.FailedBuildBugLink = ""
 			ui.FailedSyzBuildBugLink = ""
 			ui.CurrentUpTime = 0
-			ui.LastActiveBad = false
 			ui.TotalExecsBad = false
 		}
 		results = append(results, ui)
