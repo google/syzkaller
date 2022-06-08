4ad614a9027b9c0552ffb6639894e6520e5ac2b5
FahimaGold <df_mokhtari@esi.dz>
2022-06-08 16:38:51 +0300

    pkg/build: add kos builder

--- pkg/build/kos.go
+++ pkg/build/kos.go
@@ -4,12 +4,12 @@
 package build
 
 import (
-"fmt"
-"path/filepath"
-"time"
+	"fmt"
+	"path/filepath"
+	"time"
 
-"github.com/google/syzkaller/pkg/osutil"
-"github.com/google/syzkaller/sys/targets"
+	"github.com/google/syzkaller/pkg/osutil"
+	"github.com/google/syzkaller/sys/targets"
 )
 
 type kos struct{}
@@ -27,49 +27,49 @@ const LINKER_SCRIPT = "/opt/KasperskyOS-Community-Edition-1.0.0.69/libexec/arm-k
 const IMG_SRC = "/opt/KasperskyOS-Community-Edition-1.0.0.69/libexec/arm-kos/"
 
 func (ka kos) build(params Params) (ImageDetails, error) {
-sysTarget := targets.Get(targets.KOS, params.TargetArch)
-if sysTarget == nil {
-return ImageDetails{}, fmt.Errorf("unsupported Kaspersky arch %v", params.TargetArch)
-}
-arch := sysTarget.KernelHeaderArch
-kernelKOS := filepath.Join(params.KernelDir, "out", arch, "kos-image")
-if _, err := runSandboxed(time.Hour, params.KernelDir,
-"makeimg --target="+arch, "--sys-root="+SYSROOT,
-"--with-toolchain="+TOOLCHAIN,
-"--ldscript="+LINKER_SCRIPT,
-"--img-src="+IMG_SRC,
-"--img-dst="+kernelKOS,
-); err != nil {
-return ImageDetails{}, err
-}
-if _, err := runSandboxed(time.Hour*2, params.KernelDir, "scripts/fx", "clean-build"); err != nil {
-return ImageDetails{}, err
-}
+	sysTarget := targets.Get(targets.KOS, params.TargetArch)
+	if sysTarget == nil {
+		return ImageDetails{}, fmt.Errorf("unsupported Kaspersky arch %v", params.TargetArch)
+	}
+	arch := sysTarget.KernelHeaderArch
+	kernelKOS := filepath.Join(params.KernelDir, "out", arch, "kos-image")
+	if _, err := runSandboxed(time.Hour, params.KernelDir,
+		"makeimg --target="+arch, "--sys-root="+SYSROOT,
+		"--with-toolchain="+TOOLCHAIN,
+		"--ldscript="+LINKER_SCRIPT,
+		"--img-src="+IMG_SRC,
+		"--img-dst="+kernelKOS,
+	); err != nil {
+		return ImageDetails{}, err
+	}
+	if _, err := runSandboxed(time.Hour*2, params.KernelDir, "scripts/fx", "clean-build"); err != nil {
+		return ImageDetails{}, err
+	}
 
-// Add ssh keys to the zbi image so syzkaller can access the KOS vm.
-_, sshKeyPub, err := genSSHKeys(params.OutputDir)
-if err != nil {
-return ImageDetails{}, err
-}
+	// Add ssh keys to the zbi image so syzkaller can access the KOS vm.
+	_, sshKeyPub, err := genSSHKeys(params.OutputDir)
+	if err != nil {
+		return ImageDetails{}, err
+	}
 
-sshKOS := filepath.Join(params.OutputDir, "initrd")
+	sshKOS := filepath.Join(params.OutputDir, "initrd")
 
-authorizedKeys := fmt.Sprintf("data/ssh/authorized_keys=%s", sshKeyPub)
+	authorizedKeys := fmt.Sprintf("data/ssh/authorized_keys=%s", sshKeyPub)
 
-if _, err := osutil.RunCmd(time.Minute, params.KernelDir, "out/"+arch+"/host_x64/kos",
-"-o", sshKOS, kernelKOS, "--entry", authorizedKeys); err != nil {
-return ImageDetails{}, err
-}
-return ImageDetails{}, nil
+	if _, err := osutil.RunCmd(time.Minute, params.KernelDir, "out/"+arch+"/host_x64/kos",
+		"-o", sshKOS, kernelKOS, "--entry", authorizedKeys); err != nil {
+		return ImageDetails{}, err
+	}
+	return ImageDetails{}, nil
 }
 
 func runKOSSandboxed(timeout time.Duration, dir, command string, arg ...string) ([]byte, error) {
-cmd := osutil.Command(command, arg...)
-cmd.Dir = dir
-if err := osutil.Sandbox(cmd, true, false); err != nil {
-return nil, err
-}
-return osutil.Run(timeout, cmd)
+	cmd := osutil.Command(command, arg...)
+	cmd.Dir = dir
+	if err := osutil.Sandbox(cmd, true, false); err != nil {
+		return nil, err
+	}
+	return osutil.Run(timeout, cmd)
 }
 
 func (ka kos) clean(kernelDir, targetArch string) error {
