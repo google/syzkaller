google/syzkaller

commit 44a5ca633e186c5836010366c515a4017836121b

pkg/ast, pkg/compiler: support per-file metadata
We have a bunch of hacks in syz-extract, syz-sysgen and syz-check
with respect to description files unsupported on some arches,
or that must not be part of make extract.

Add 2 meta attribtues to files:

meta noextract
Tells `make extract` to not extract constants for this file.
Though, `syz-extract` can still be invoked manually on this file.

meta arches["arch1", "arch2"]
Restricts this file only to the given set of architectures.
`make extract` and ``make generate` will not use it on other architectures.

Later we can potentially use meta attributes to specify git tree/commit
that must be used for extraction. Maybe something else.

Fixes #2754

sys/syz-extract/extract.go
@@ -75,17 +75,12 @@ func main() {
 	if *flagBuild && *flagBuildDir != "" {
 		tool.Failf("-build and -builddir is an invalid combination")
 	}
-
-	OS, archArray, files, err := archFileList(*flagOS, *flagArch, flag.Args())
-	if err != nil {
-		tool.Fail(err)
-	}
-
+	OS := *flagOS
 	extractor := extractors[OS]
 	if extractor == nil {
 		tool.Failf("unknown os: %v", OS)
 	}
-	arches, err := createArches(OS, archArray, files)
+	arches, err := createArches(OS, archList(OS, *flagArch), flag.Args())
 	if err != nil {
 		tool.Fail(err)
 	}
@@ -97,7 +92,7 @@ func main() {
 		tool.Fail(err)
 	}
 
-	jobC := make(chan interface{}, len(archArray)*len(files))
+	jobC := make(chan interface{}, len(arches))
 	for _, arch := range arches {
 		jobC <- arch
 	}
@@ -108,11 +103,8 @@ func main() {
 
 	failed := false
 	constFiles := make(map[string]*compiler.ConstFile)
-	for _, file := range files {
-		constFiles[file] = compiler.NewConstFile()
-	}
 	for _, arch := range arches {
-		fmt.Printf("generating %v/%v...\n", arch.target.OS, arch.target.Arch)
+		fmt.Printf("generating %v/%v...\n", OS, arch.target.Arch)
 		<-arch.done
 		if arch.err != nil {
 			failed = true
@@ -125,6 +117,9 @@ func main() {
 				failed = true
 				fmt.Printf("%v: %v\n", f.name, f.err)
 				continue
+			}
+			if constFiles[f.name] == nil {
+				constFiles[f.name] = compiler.NewConstFile()
 			}
 			constFiles[f.name].AddArch(f.arch.target.Arch, f.consts, f.undeclared)
 		}
@@ -175,6 +170,18 @@ func worker(extractor Extractor, jobC chan interface{}) {
 }
 
 func createArches(OS string, archArray, files []string) ([]*Arch, error) {
+	errBuf := new(bytes.Buffer)
+	eh := func(pos ast.Pos, msg string) {
+		fmt.Fprintf(errBuf, "%v: %v\n", pos, msg)
+	}
+	top := ast.ParseGlob(filepath.Join("sys", OS, "*.txt"), eh)
+	if top == nil {
+		return nil, fmt.Errorf("%v", errBuf.String())
+	}
+	allFiles := compiler.FileList(top, OS, eh)
+	if allFiles == nil {
+		return nil, fmt.Errorf("%v", errBuf.String())
+	}
 	var arches []*Arch
 	for _, archStr := range archArray {
 		buildDir := ""
@@ -203,7 +210,17 @@ func createArches(OS string, archArray, files []string) ([]*Arch, error) {
 			build:       *flagBuild,
 			done:        make(chan bool),
 		}
-		for _, f := range files {
+		archFiles := files
+		if len(archFiles) == 0 {
+			for file, meta := range allFiles {
+				if meta.NoExtract || !meta.SupportsArch(archStr) {
+					continue
+				}
+				archFiles = append(archFiles, file)
+			}
+		}
+		sort.Strings(archFiles)
+		for _, f := range archFiles {
 			arch.files = append(arch.files, &File{
 				arch: arch,
 				name: f,
@@ -213,6 +230,18 @@ func createArches(OS string, archArray, files []string) ([]*Arch, error) {
 		arches = append(arches, arch)
 	}
 	return arches, nil
+}
+
+func archList(OS, arches string) []string {
+	if arches != "" {
+		return strings.Split(arches, ",")
+	}
+	var archArray []string
+	for arch := range targets.List[OS] {
+		archArray = append(archArray, arch)
+	}
+	sort.Strings(archArray)
+	return archArray
 }
 
 func checkUnsupportedCalls(arches []*Arch) bool {
@@ -238,60 +267,6 @@ func checkUnsupportedCalls(arches []*Arch) bool {
 			file, name)
 	}
 	return failed
-}
-
-func archFileList(os, arch string, files []string) (string, []string, []string, error) {
-	// Note: this is linux-specific and should be part of Extractor and moved to linux.go.
-	android := false
-	if os == "android" {
-		android = true
-		os = targets.Linux
-	}
-	var arches []string
-	if arch != "" {
-		arches = strings.Split(arch, ",")
-	} else {
-		for arch := range targets.List[os] {
-			arches = append(arches, arch)
-		}
-		if android {
-			arches = []string{targets.I386, targets.AMD64, targets.ARM, targets.ARM64}
-		}
-		sort.Strings(arches)
-	}
-	if len(files) == 0 {
-		matches, err := filepath.Glob(filepath.Join("sys", os, "*.txt"))
-		if err != nil || len(matches) == 0 {
-			return "", nil, nil, fmt.Errorf("failed to find sys files: %v", err)
-		}
-		manualFiles := map[string]bool{
-			// Not upstream, generated on https://github.com/multipath-tcp/mptcp_net-next
-			"vnet_mptcp.txt": true,
-			// Not upstream, generated on:
-			// https://chromium.googlesource.com/chromiumos/third_party/kernel d2a8a1eb8b86
-			"dev_bifrost.txt": true,
-			// Not upstream, generated on unknown tree.
-			"dev_img_rogue.txt": true,
-		}
-		androidFiles := map[string]bool{
-			"dev_tlk_device.txt": true,
-			// This was generated on:
-			// https://source.codeaurora.org/quic/la/kernel/msm-4.9 msm-4.9
-			"dev_video4linux.txt": true,
-			// This was generated on:
-			// https://chromium.googlesource.com/chromiumos/third_party/kernel 3a36438201f3
-			"fs_incfs.txt": true,
-		}
-		for _, f := range matches {
-			f = filepath.Base(f)
-			if manualFiles[f] || os == targets.Linux && android != androidFiles[f] {
-				continue
-			}
-			files = append(files, f)
-		}
-		sort.Strings(files)
-	}
-	return os, arches, files, nil
 }
 
 func processArch(extractor Extractor, arch *Arch) (map[string]*compiler.ConstInfo, error) {

pkg/ast/ast.go
@@ -46,6 +46,15 @@ func (n *Comment) Info() (Pos, string, string) {
 	return n.Pos, tok2str[tokComment], ""
 }
 
+type Meta struct {
+	Pos   Pos
+	Value *Type
+}
+
+func (n *Meta) Info() (Pos, string, string) {
+	return n.Pos, "meta", n.Value.Ident
+}
+
 type Include struct {
 	Pos  Pos
 	File *String

pkg/ast/clone.go
@@ -24,6 +24,13 @@ func (n *Comment) Clone() Node {
 	}
 }
 
+func (n *Meta) Clone() Node {
+	return &Meta{
+		Pos:   n.Pos,
+		Value: n.Value.Clone().(*Type),
+	}
+}
+
 func (n *Include) Clone() Node {
 	return &Include{
 		Pos:  n.Pos,

pkg/ast/format.go
@@ -74,6 +74,12 @@ func (n *Comment) serialize(w io.Writer) {
 	fmt.Fprintf(w, "#%v\n", strings.TrimRight(n.Text, " \t"))
 }
 
+func (n *Meta) serialize(w io.Writer) {
+	fmt.Fprintf(w, "meta ")
+	n.Value.serialize(w)
+	fmt.Fprintf(w, "\n")
+}
+
 func (n *Include) serialize(w io.Writer) {
 	fmt.Fprintf(w, "include <%v>\n", n.File.Value)
 }

pkg/ast/parser.go
@@ -128,7 +128,10 @@ func (p *parser) parseTop() Node {
 		return p.parseResource()
 	case tokIdent:
 		name := p.parseIdent()
-		if name.Name == "type" {
+		switch name.Name {
+		case "meta":
+			return p.parseMeta()
+		case "type":
 			return p.parseTypeDef()
 		}
 		switch p.tok {
@@ -188,6 +191,13 @@ func (p *parser) parseComment() *Comment {
 	}
 	p.consume(tokComment)
 	return c
+}
+
+func (p *parser) parseMeta() *Meta {
+	return &Meta{
+		Pos:   p.pos,
+		Value: p.parseType(),
+	}
 }
 
 func (p *parser) parseDefine() *Define {

pkg/ast/walk.go
@@ -35,6 +35,10 @@ func (n *Ident) walk(cb func(Node))   {}
 func (n *String) walk(cb func(Node))  {}
 func (n *Int) walk(cb func(Node))     {}
 
+func (n *Meta) walk(cb func(Node)) {
+	cb(n.Value)
+}
+
 func (n *Include) walk(cb func(Node)) {
 	cb(n.File)
 }

pkg/compiler/check.go
@@ -836,11 +836,14 @@ type checkCtx struct {
 }
 
 func (comp *compiler) checkType(ctx checkCtx, t *ast.Type, flags checkFlags) {
+	comp.checkTypeImpl(ctx, t, comp.getTypeDesc(t), flags)
+}
+
+func (comp *compiler) checkTypeImpl(ctx checkCtx, t *ast.Type, desc *typeDesc, flags checkFlags) {
 	if unexpected, _, ok := checkTypeKind(t, kindIdent); !ok {
 		comp.error(t.Pos, "unexpected %v, expect type", unexpected)
 		return
 	}
-	desc := comp.getTypeDesc(t)
 	if desc == nil {
 		comp.error(t.Pos, "unknown type %v", t.Ident)
 		return

pkg/compiler/compiler.go
@@ -7,6 +7,7 @@ package compiler
 
 import (
 	"fmt"
+	"path/filepath"
 	"sort"
 	"strconv"
 	"strings"
@@ -74,6 +75,7 @@ func createCompiler(desc *ast.Description, target *targets.Target, eh ast.ErrorH
 // Compile compiles sys description.
 func Compile(desc *ast.Description, consts map[string]uint64, target *targets.Target, eh ast.ErrorHandler) *Prog {
 	comp := createCompiler(desc.Clone(), target, eh)
+	comp.filterArch()
 	comp.typecheck()
 	// The subsequent, more complex, checks expect basic validity of the tree,
 	// in particular corrent number of type arguments. If there were errors,
@@ -149,6 +151,24 @@ func (comp *compiler) error(pos ast.Pos, msg string, args ...interface{}) {
 
 func (comp *compiler) warning(pos ast.Pos, msg string, args ...interface{}) {
 	comp.warnings = append(comp.warnings, warn{pos, fmt.Sprintf(msg, args...)})
+}
+
+func (comp *compiler) filterArch() {
+	files := comp.fileList()
+	comp.desc = comp.desc.Filter(func(n ast.Node) bool {
+		pos, typ, name := n.Info()
+		meta := files[filepath.Base(pos.File)]
+		if meta.SupportsArch(comp.target.Arch) {
+			return true
+		}
+		switch n.(type) {
+		case *ast.Resource, *ast.Struct, *ast.Call, *ast.TypeDef:
+			// This is required to keep the unsupported diagnostic working,
+			// otherwise sysgen will think that these things are still supported on some arches.
+			comp.unsupported[typ+" "+name] = true
+		}
+		return false
+	})
 }
 
 func (comp *compiler) structIsVarlen(name string) bool {

commit 542a49c596df352c4163f5b6b5ee6e52998e1368

sys/syz-extract: fix a deadlock
The jobs channel needs to have capacity to hold all files from all arches
since they may all be queued to the channel at the same time.
The bug was introduced in 44a5ca633e
("pkg/ast, pkg/compiler: support per-file metadata").
Restore full channel capacity.

Fixes #3197

sys/syz-extract/extract.go
@@ -80,7 +80,7 @@ func main() {
 	if extractor == nil {
 		tool.Failf("unknown os: %v", OS)
 	}
-	arches, err := createArches(OS, archList(OS, *flagArch), flag.Args())
+	arches, nfiles, err := createArches(OS, archList(OS, *flagArch), flag.Args())
 	if err != nil {
 		tool.Fail(err)
 	}
@@ -92,7 +92,7 @@ func main() {
 		tool.Fail(err)
 	}
 
-	jobC := make(chan interface{}, len(arches))
+	jobC := make(chan interface{}, len(arches)+nfiles)
 	for _, arch := range arches {
 		jobC <- arch
 	}
@@ -169,26 +169,27 @@ func worker(extractor Extractor, jobC chan interface{}) {
 	}
 }
 
-func createArches(OS string, archArray, files []string) ([]*Arch, error) {
+func createArches(OS string, archArray, files []string) ([]*Arch, int, error) {
 	errBuf := new(bytes.Buffer)
 	eh := func(pos ast.Pos, msg string) {
 		fmt.Fprintf(errBuf, "%v: %v\n", pos, msg)
 	}
 	top := ast.ParseGlob(filepath.Join("sys", OS, "*.txt"), eh)
 	if top == nil {
-		return nil, fmt.Errorf("%v", errBuf.String())
+		return nil, 0, fmt.Errorf("%v", errBuf.String())
 	}
 	allFiles := compiler.FileList(top, OS, eh)
 	if allFiles == nil {
-		return nil, fmt.Errorf("%v", errBuf.String())
+		return nil, 0, fmt.Errorf("%v", errBuf.String())
 	}
+	nfiles := 0
 	var arches []*Arch
 	for _, archStr := range archArray {
 		buildDir := ""
 		if *flagBuild {
 			dir, err := ioutil.TempDir("", "syzkaller-kernel-build")
 			if err != nil {
-				return nil, fmt.Errorf("failed to create temp dir: %v", err)
+				return nil, 0, fmt.Errorf("failed to create temp dir: %v", err)
 			}
 			buildDir = dir
 		} else if *flagBuildDir != "" {
@@ -199,7 +200,7 @@ func createArches(OS string, archArray, files []string) ([]*Arch, error) {
 
 		target := targets.Get(OS, archStr)
 		if target == nil {
-			return nil, fmt.Errorf("unknown arch: %v", archStr)
+			return nil, 0, fmt.Errorf("unknown arch: %v", archStr)
 		}
 
 		arch := &Arch{
@@ -228,8 +229,9 @@ func createArches(OS string, archArray, files []string) ([]*Arch, error) {
 			})
 		}
 		arches = append(arches, arch)
+		nfiles += len(arch.files)
 	}
-	return arches, nil
+	return arches, nfiles, nil
 }
 
 func archList(OS, arches string) []string {