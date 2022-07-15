9fc517666a5286882c88772354ec80198cd73ac8
Aleksandr Nogikh <nogikh@google.com>
2022-07-15 09:35:48 +0000

    syz-ci: upload build assets
    This commit introduces the syz-ci side of the asset storage
    functionality.
    * Intercept assets at various stages of syz-ci operation.
    * Compress and upload assets to GCS.
    * Report assets to the dashboard.
    * Remove no longer needed assets.

--- pkg/asset/type.go
+++ pkg/asset/type.go
@@ -3,35 +3,61 @@
 
 package asset
 
-import "github.com/google/syzkaller/sys/targets"
-
-type Type string
-
-// Asset types used throughout the system.
-const (
-	BootableDisk       Type = "bootable_disk"
-	NonBootableDisk    Type = "non_bootable_disk"
-	KernelObject       Type = "kernel_object"
-	KernelImage        Type = "kernel_image"
-	HTMLCoverageReport Type = "html_coverage_report"
+import (
+	"github.com/google/syzkaller/dashboard/dashapi"
+	"github.com/google/syzkaller/sys/targets"
 )
 
-func GetHumanReadableName(assetType Type, target *targets.Target) string {
-	switch assetType {
-	case BootableDisk:
-		return "disk image"
-	case NonBootableDisk:
-		return "disk image (non-bootable)"
-	case KernelImage:
-		return "kernel image"
-	case KernelObject:
-		if target != nil && target.KernelObject != "" {
-			return target.KernelObject
-		}
-		return "kernel object"
-	case HTMLCoverageReport:
-		return "coverage report (html)"
-	default:
-		panic("invalid asset type: " + assetType)
+type TypeDescription struct {
+	AllowMultiple     bool
+	GetTitle          QueryTypeTitle
+	ContentType       string
+	ReportingPrio     int // the smaller, the higher the asset is on the list during reporting
+	NoReporting       bool
+	customCompressor  Compressor
+	preserveExtension bool
+}
+
+var assetTypes = map[dashapi.AssetType]*TypeDescription{
+	dashapi.BootableDisk: {
+		GetTitle:      constTitle("disk image"),
+		ReportingPrio: 1,
+	},
+	dashapi.NonBootableDisk: {
+		GetTitle:      constTitle("disk image (non-bootable)"),
+		ReportingPrio: 2,
+	},
+	dashapi.KernelObject: {
+		GetTitle: func(target *targets.Target) string {
+			if target != nil && target.KernelObject != "" {
+				return target.KernelObject
+			}
+			return "kernel object"
+		},
+		ReportingPrio: 3,
+	},
+	dashapi.KernelImage: {
+		GetTitle:      constTitle("kernel image"),
+		ReportingPrio: 4,
+	},
+	dashapi.HTMLCoverageReport: {
+		GetTitle:          constTitle("coverage report(html)"),
+		AllowMultiple:     true,
+		ContentType:       "text/html",
+		NoReporting:       true,
+		customCompressor:  gzipCompressor,
+		preserveExtension: true,
+	},
+}
+
+type QueryTypeTitle func(*targets.Target) string
+
+func constTitle(title string) QueryTypeTitle {
+	return func(*targets.Target) string {
+		return title
 	}
+}
+
+func GetTypeDescription(assetType dashapi.AssetType) *TypeDescription {
+	return assetTypes[assetType]
 }
