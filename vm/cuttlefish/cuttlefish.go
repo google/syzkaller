8978bb194d39b650ae4d5011df83da0aaa968bfc
Liz Prucka <lizprucka@google.com>
2022-08-31 20:44:27 +0000

    vm/cuttlefish: run commands from deviceRoot
    Run commands from deviceRoot (data/fuzz).
    Otherwise was receiving errors about a
    'read-only file system'

--- vm/cuttlefish/cuttlefish.go
+++ vm/cuttlefish/cuttlefish.go
@@ -149,7 +149,7 @@ func (inst *instance) Close() {
 
 func (inst *instance) Run(timeout time.Duration, stop <-chan bool, command string) (
 	<-chan []byte, <-chan error, error) {
-	return inst.gceInst.Run(timeout, stop, fmt.Sprintf("adb shell %s", command))
+	return inst.gceInst.Run(timeout, stop, fmt.Sprintf("adb shell cd %s; %s", deviceRoot, command))
 }
 
 func (inst *instance) Diagnose(rep *report.Report) ([]byte, bool) {
