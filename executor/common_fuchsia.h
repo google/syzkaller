c10a654ab900331bdea520a4a338efb677169e3c
Dmitry Vyukov <dvyukov@google.com>
2022-09-06 10:03:40 +0200

    Revert "pkg/csource: inline void* cast into generated code"
    This reverts commit 922294abb4c0bc72b24d8526d625110d73fa1b5a.
    
    The commit reported to cause old warnings on s390x:
    https://github.com/google/syzkaller/commit/922294abb4c0bc72b24d8526d625110d73fa1b5a#commitcomment-83096994

--- executor/common_fuchsia.h
+++ executor/common_fuchsia.h
@@ -272,3 +272,7 @@ static int do_sandbox_none(void)
 	return 0;
 }
 #endif
+
+// Ugly way to work around gcc's "error: function called through a non-compatible type".
+// The macro is used in generated C code.
+#define CAST(f) ({void* p = (void*)f; p; })
