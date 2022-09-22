2b3c17304cd6d27bcb92176268e2f3ce0f90b388
Taras Madan <tarasmadan@google.com>
2022-09-22 17:00:53 +0000

    vm/proxyapp: add proxyapp tests

--- vm/proxyapp/proxyappclient_mocks_test.go
+++ vm/proxyapp/proxyappclient_mocks_test.go
@@ -7,6 +7,10 @@ package proxyapp
 //go:generate ../../tools/mockery.sh --name ProxyAppInterface -r
 
 import (
+	"context"
+	"testing"
+
+	"github.com/google/syzkaller/vm/proxyapp/mocks"
 	"github.com/google/syzkaller/vm/proxyapp/proxyrpc"
 )
 
@@ -14,3 +18,28 @@ var (
 	_ subProcessCmd              = &mocks.SubProcessCmd{}
 	_ proxyrpc.ProxyAppInterface = &mocks.ProxyAppInterface{}
 )
+
+type mockCommandRunner struct {
+	*mocks.SubProcessCmd
+	ctx          context.Context
+	onWaitCalled chan bool
+}
+
+func makeMockCommandRunner(t *testing.T) (*mockCommandRunner, *proxyAppParams) {
+	cmdRunner := &mockCommandRunner{
+		SubProcessCmd: mocks.NewSubProcessCmd(t),
+		onWaitCalled:  make(chan bool, 1),
+	}
+
+	params := makeTestParams()
+	params.CommandRunner = func(ctx context.Context, cmd string, params ...string) subProcessCmd {
+		cmdRunner.ctx = ctx
+		return cmdRunner
+	}
+	return cmdRunner, params
+}
+
+func (cmd *mockCommandRunner) Wait() error {
+	cmd.onWaitCalled <- true
+	return cmd.SubProcessCmd.Wait()
+}
