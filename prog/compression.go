a9826d9931ef4ad9714e1f7f427b91fb4583f3c6
Hrutvik Kanabar <hrutvik@google.com>
2022-10-21 15:34:55 +0000

    prog: add (de)compression wrappers
    Add the file `compression.go`, with entry points for
    compressing/decompressing using `zlib`, and encoding/decoding Base64.
    
    Also add roundtrip compress/decompress and encode/decode tests.

---
+++ prog/compression.go
@@ -0,0 +1,67 @@
+// Copyright 2022 syzkaller project authors. All rights reserved.
+// Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
+
+package prog
+
+import (
+	"bytes"
+	"compress/zlib"
+	"encoding/base64"
+	"fmt"
+	"io"
+)
+
+func Compress(rawData []byte) []byte {
+	var buffer bytes.Buffer
+	zlibWriter := zlib.NewWriter(&buffer)
+
+	_, err := zlibWriter.Write(rawData)
+	if err != nil {
+		panic(fmt.Sprintf("could not compress with zlib: %v", err))
+	}
+
+	err = zlibWriter.Close()
+	if err != nil {
+		panic(fmt.Sprintf("could not finalize compression with zlib: %v", err))
+	}
+
+	return buffer.Bytes()
+}
+
+func Decompress(compressedData []byte) ([]byte, error) {
+	zlibReader, err := zlib.NewReader(bytes.NewReader(compressedData))
+	if err != nil {
+		return nil, fmt.Errorf("could not initialise zlib: %v", err)
+	}
+
+	data, err := io.ReadAll(zlibReader)
+	if err != nil {
+		return nil, fmt.Errorf("could not read data with zlib: %v", err)
+	}
+
+	err = zlibReader.Close()
+	return data, err
+}
+
+func DecodeB64(b64Data []byte) ([]byte, error) {
+	decoder := base64.NewDecoder(base64.StdEncoding, bytes.NewReader(b64Data))
+	rawData, err := io.ReadAll(decoder)
+	if err != nil {
+		return nil, fmt.Errorf("could not decode Base64: %v", err)
+	}
+	return rawData, nil
+}
+
+func EncodeB64(rawData []byte) []byte {
+	var buf bytes.Buffer
+	encoder := base64.NewEncoder(base64.StdEncoding, &buf)
+	_, err := encoder.Write(rawData)
+	if err != nil {
+		panic(fmt.Sprintf("could not encode Base64: %v", err))
+	}
+	err = encoder.Close()
+	if err != nil {
+		panic(fmt.Sprintf("could not finalize encoding to Base64: %v", err))
+	}
+	return buf.Bytes()
+}
