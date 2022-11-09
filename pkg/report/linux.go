2a006ae0041c9148e6b6d1b6df9704652a2fa303
kalder <61064868+kalder@users.noreply.github.com>
2022-11-09 10:16:44 -0800

    pkg/report: don't call get_maintainer.pl for cuttlefish
    * pkg/report: check for get_maintainer.pl before calling it
    
    For Android kernels, this tool will be in a different location
    (common/scripts/get_maintainer.pl). Currently this causes the
    Symbolize() call to fail, which stops reproduction.
    
    * pkg/report: check for get_maintainer.pl before calling it
    
    For Android kernels, this tool will be in a different location
    (common/scripts/get_maintainer.pl). Currently this causes the
    Symbolize() call to fail, which stops reproduction.

--- pkg/report/linux.go
+++ pkg/report/linux.go
@@ -370,6 +370,12 @@ func (ctx *linux) Symbolize(rep *Report) error {
 
 	rep.Report = ctx.decompileOpcodes(rep.Report, rep)
 
+	// Skip getting maintainers for Android fuzzing since the kernel source
+	// directory structure is different.
+	if ctx.config.vmType == "cuttlefish" || ctx.config.vmType == "proxyapp" {
+		return nil
+	}
+
 	// We still do this even if we did not symbolize,
 	// because tests pass in already symbolized input.
 	rep.guiltyFile = ctx.extractGuiltyFile(rep)
