2916877bea154f8e1da30dfc4f0f7c9630ed8efd
Aleksandr Nogikh <nogikh@google.com>
2022-11-29 10:40:54 +0100

    pkg/email: tolerate newline characters after #syz
    It look like email clients can insert newlines there if the line is too long.

--- pkg/email/parser.go
+++ pkg/email/parser.go
@@ -15,6 +15,7 @@ import (
 	"regexp"
 	"sort"
 	"strings"
+	"unicode"
 )
 
 type Email struct {
@@ -251,7 +252,7 @@ func extractCommand(body string) (cmd Command, str, args string) {
 		return
 	}
 	cmdPos += len(commandPrefix) + 1
-	for cmdPos < len(body) && (body[cmdPos] == ' ' || body[cmdPos] == '\t') {
+	for cmdPos < len(body) && unicode.IsSpace(rune(body[cmdPos])) {
 		cmdPos++
 	}
 	cmdEnd := strings.IndexByte(body[cmdPos:], '\n')
