ce102536fc45f5a2d332f2587c2669141319b370
Aleksandr Nogikh <nogikh@google.com>
2023-03-07 16:07:35 +0100

    dashboard: support obsolete KernelRepo entries
    There are cases when a previously fuzzed tree was obsoleted and can no
    longer be polled for commits. At the same time, we still want to display
    short repo aliases in bug reportings and on the dashboard.

--- dashboard/app/config.go
+++ dashboard/app/config.go
@@ -194,6 +194,9 @@ type KernelRepo struct {
 	ReportingPriority int
 	// CC for all bugs reported on this repo.
 	CC CCConfig
+	// This repository is no longer active and should not be polled for commits.
+	// It will only be used to display kernel aliases for older crashes.
+	Obsolete bool
 }
 
 type CCConfig struct {
