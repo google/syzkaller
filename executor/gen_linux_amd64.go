ab9f712fa1164d0c35839d8a84ee3e53512c9ebb
Space Meyer <spm@google.com>
2023-04-12 12:03:17 +0200

    executor: mark amd64 kvm_gen.cc stack nonexecutable
    Without this change `go generate ./executor` on gcc 12.2.0 gives:
    /usr/bin/ld: warning: /tmp/ccmfWMO4.o: missing .note.GNU-stack section implies executable stack
    /usr/bin/ld: NOTE: This behaviour is deprecated and will be removed in a future version of the linker
    
    This does not change the generated code in kvm_amd64.S.h

--- executor/gen_linux_amd64.go
+++ executor/gen_linux_amd64.go
@@ -2,6 +2,6 @@
 // Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
 
 // nolint: lll
-//go:generate bash -c "gcc -DGOARCH_$GOARCH=1 kvm_gen.cc kvm_amd64.S -o kvm_gen && ./kvm_gen > kvm_amd64.S.h && rm ./kvm_gen"
+//go:generate bash -c "gcc -Wa,--noexecstack -DGOARCH_$GOARCH=1 kvm_gen.cc kvm_amd64.S -o kvm_gen && ./kvm_gen > kvm_amd64.S.h && rm ./kvm_gen"
 
 package executor
