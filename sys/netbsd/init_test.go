65320f8e65b2fd5215822c6fc85034cec63dd887
Aleksandr Nogikh <nogikh@google.com>
2023-04-21 14:45:31 +0200

    sys/netbsd: neutralize compat_50_mknod
    Otherwise we may end up corrupting device memory.
    
    See https://groups.google.com/g/syzkaller-netbsd-bugs/c/Iy8-NZ_M9Ug/m/5jKKfncsAQAJ

---
+++ sys/netbsd/init_test.go
@@ -0,0 +1,21 @@
+// Copyright 2023 syzkaller project authors. All rights reserved.
+// Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
+
+package netbsd_test
+
+import (
+	"testing"
+
+	"github.com/google/syzkaller/prog"
+	_ "github.com/google/syzkaller/sys/netbsd/gen"
+	"github.com/google/syzkaller/sys/targets"
+)
+
+func TestNetBSDNeutralize(t *testing.T) {
+	prog.TestDeserializeHelper(t, targets.NetBSD, targets.AMD64, nil, []prog.DeserializeTest{
+		{
+			In:  `compat_50_mknod(&(0x7f0000000000)='./file0\x00', 0x2001, 0x400)`,
+			Out: `compat_50_mknod(&(0x7f0000000000)='./file0\x00', 0x8001, 0x400)`,
+		},
+	})
+}
