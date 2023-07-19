73abc7a7b25db139fe42cf9d017a60b72331d8dc
Andrew Donnellan <ajd@linux.ibm.com>
2023-07-19 17:42:59 +1000

    tools/create-gce-image: add rootwait command line option for ppc64le
    Sometimes it can take a while for a root block device to appear when
    backed by hardware that is slow to initialise, e.g. the ibmvfc Virtual
    Fibre Channel interface.
    
    Use the "rootwait" option to have the kernel wait for a disk to appear
    rather than panicking immediately.
    
    Signed-off-by: Andrew Donnellan <ajd@linux.ibm.com>

--- pkg/build/linux_generated.go
+++ pkg/build/linux_generated.go
@@ -146,7 +146,7 @@ menuentry 'linux' --class gnu-linux --class gnu --class os {
 	insmod part_msdos
 	insmod ext2
 	search -f --set /vmlinuz
-	linux /vmlinuz root=/dev/sda2 console=ttyS0 earlyprintk=serial oops=panic panic_on_warn=1 nmi_watchdog=panic panic=86400 net.ifnames=0 $CMDLINE
+	linux /vmlinuz root=/dev/sda2 rootwait console=ttyS0 earlyprintk=serial oops=panic panic_on_warn=1 nmi_watchdog=panic panic=60 net.ifnames=0 $CMDLINE
 }
 EOF
 	sudo grub-install --target=powerpc-ieee1275 --boot-directory=disk.mnt/boot $DISKDEV"p1"
