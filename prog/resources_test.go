77f31e1f821f48a72259fdbd9d285d1ce16956f5
Paul Chaignon <paul.chaignon@gmail.com>
2023-10-06 17:43:01 +0200

    prog: test optional input resources are skipped
    This commit adds a unit test for getInputResources, to verify in
    particular that it doesn't return input resources that are optional.
    
    Note we can't test the built-in "optional[]" because that relies on
    unions and those aren't supported yet.
    
    Signed-off-by: Paul Chaignon <paul.chaignon@gmail.com>

--- prog/resources_test.go
+++ prog/resources_test.go
@@ -98,6 +98,32 @@ func TestTransitivelyEnabledCallsLinux(t *testing.T) {
 	}
 }
 
+func TestGetInputResources(t *testing.T) {
+	expectedRequiredResources := map[string]bool{
+		"required_res1": false,
+	}
+
+	t.Parallel()
+	target, err := GetTarget("test", "64")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	resources := target.getInputResources(target.SyscallMap["test$optional_res"])
+	for _, resource := range resources {
+		if _, ok := expectedRequiredResources[resource.Name]; ok {
+			expectedRequiredResources[resource.Name] = true
+		} else {
+			t.Fatalf(" unexpected %v", resource.Name)
+		}
+	}
+	for expectedRes, found := range expectedRequiredResources {
+		if !found {
+			t.Fatalf(" missing %v", expectedRes)
+		}
+	}
+}
+
 func TestClockGettime(t *testing.T) {
 	t.Parallel()
 	target, err := GetTarget("linux", "amd64")
