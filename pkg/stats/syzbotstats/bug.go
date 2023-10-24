7256c5bd35ec860ad8a46282316ea57ccb707a95
Aleksandr Nogikh <nogikh@google.com>
2023-10-24 11:52:01 +0200

    dashboard: export more stat data
    Export first crash time and fix commit hashes.
    Export list of managers on which the bug was triggered.
    Calculate reporting time more precisely.

--- pkg/stats/syzbotstats/bug.go
+++ pkg/stats/syzbotstats/bug.go
@@ -8,14 +8,17 @@ import "time"
 type BugStatSummary struct {
 	Title           string
 	IDs             []string  // IDs used by syzbot for this bug.
+	FirstTime       time.Time // When the bug was first hit.
 	ReleasedTime    time.Time // When the bug was published.
 	ReproTime       time.Time // When we found the reproducer.
 	CauseBisectTime time.Time // When we found cause bisection.
 	ResolvedTime    time.Time // When the bug was resolved.
 	Status          BugStatus
 	Subsystems      []string
-	Strace          bool    // Whether we managed to reproduce under strace.
-	HitsPerDay      float64 // Average number of bug hits per day.
+	Strace          bool     // Whether we managed to reproduce under strace.
+	HitsPerDay      float64  // Average number of bug hits per day.
+	FixHashes       []string // Hashes of fix commits.
+	HappenedOn      []string // Managers on which the crash happened.
 }
 
 type BugStatus string
