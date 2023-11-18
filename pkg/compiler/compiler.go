b8984a38024cfb6d5475bc7d063fd5b430c8cba4
Paul Chaignon <paul.chaignon@gmail.com>
2023-11-18 14:09:06 +0100

    compiler: support nested int flags definitions
    This commit adds support for flags definitions such as:
    
        flags1 = VAL1, VAL2
        flags2 = flags1, VAL3
    
    This is achieved by flattening nested flag definitions as part of the
    compilation. That is, nested flags are compiled into their fully
    unreferenced/unnested form.
    
    This flattening cannot be achieved in a single pass over the flags
    because we don't have a guarantee that we will encounter the subflags
    (ex. flags1 above) before their superflags (ex. flags2 above). Instead,
    in a first pass, we need to build an indexing of flags that we can use
    to flatten superflags in a second pass.
    
    Thankfully, this indexing is already computed in the form of
    comp.intFlags. Thus, we only need to implement the second pass, done
    with function compiler.flattenFlags().
    
    This second pass walks the flag definitions recursively in an attemp to
    fully flatten them. It errors out if a flag definition has more than 5
    levels of nested definitions.
    
    Being able to error in that way requires a bit of care in how we flatten
    the flags. Consider the following example where flags1 to flags5 have
    less than 5 leves of nesting, whereas flags6 should cause an error.
    
        flags6 = VAL6, flags5
        flags5 = VAL5, flags6
        ...
        flags1 = VAL1
    
    If we were to flatten the flag definitions in place, then we might walk
    into flags5 first and fully flatten it. As a result, when we would walk
    into flags6, we would detect a single level of nesting and wouldn't
    error out. To avoid that, we work on the original set of nested flags
    and copy the flattened flags over only once we're done with all flags.
    
    Signed-off-by: Paul Chaignon <paul.chaignon@gmail.com>

--- pkg/compiler/compiler.go
+++ pkg/compiler/compiler.go
@@ -76,6 +76,7 @@ func Compile(desc *ast.Description, consts map[string]uint64, target *targets.Ta
 	comp := createCompiler(desc.Clone(), target, eh)
 	comp.filterArch()
 	comp.typecheck()
+	comp.flattenFlags()
 	// The subsequent, more complex, checks expect basic validity of the tree,
 	// in particular corrent number of type arguments. If there were errors,
 	// don't proceed to avoid out-of-bounds references to type arguments.
@@ -356,4 +357,41 @@ func arrayContains(a []string, v string) bool {
 		}
 	}
 	return false
+}
+
+func (comp *compiler) flattenFlags() {
+	for name, flags := range comp.intFlags {
+		if err := comp.recurFlattenFlags(name, flags); err != nil {
+			comp.error(flags.Pos, "%v", err)
+		}
+	}
+
+	for _, n := range comp.desc.Nodes {
+		switch flags := n.(type) {
+		case *ast.IntFlags:
+			// It's possible that we don't find the flag in intFlags if it was
+			// skipped due to errors (or special name "_") when populating
+			// intFlags (see checkNames).
+			if f, ok := comp.intFlags[flags.Name.Name]; ok {
+				flags.Values = f.Values
+			}
+		}
+	}
+}
+
+func (comp *compiler) recurFlattenFlags(name string, flags *ast.IntFlags) error {
+	var values []*ast.Int
+	for _, flag := range flags.Values {
+		if f, isFlags := comp.intFlags[flag.Ident]; isFlags {
+			comp.recurFlattenFlags(flag.Ident, f)
+			values = append(values, comp.intFlags[flag.Ident].Values...)
+		} else {
+			values = append(values, flag)
+		}
+	}
+	if len(values) > 2000 {
+		return fmt.Errorf("%v has more than 2000 values", name)
+	}
+	flags.Values = values
+	return nil
 }
