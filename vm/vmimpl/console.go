a2c55c08c52f516b0313c7cb9e46c68640a5f3ec
Aleksandr Nogikh <nogikh@google.com>
2024-01-09 12:51:12 +0100

    vmimpl: don't build console.go under Windows

--- vm/vmimpl/console.go
+++ vm/vmimpl/console.go
@@ -1,5 +1,6 @@
 // Copyright 2017 syzkaller project authors. All rights reserved.
 // Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
+//go:build !windows
 
 package vmimpl
 
