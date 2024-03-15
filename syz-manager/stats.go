3968a6757670c79d1db16456538b74e49bbe31c5
Aleksandr Nogikh <nogikh@google.com>
2024-03-15 17:44:36 +0100

    syz-manager: remove input rotation
    Once we move pkg/fuzzer to the host, we won't be able to reuse this
    implementation.

--- syz-manager/stats.go
+++ syz-manager/stats.go
@@ -19,7 +19,6 @@ type Stats struct {
 	crashSuppressed     Stat
 	vmRestarts          Stat
 	newInputs           Stat
-	rotatedInputs       Stat
 	execTotal           Stat
 	rpcTraffic          Stat
 	hubSendProgAdd      Stat
@@ -68,7 +67,6 @@ func (stats *Stats) all() map[string]uint64 {
 		"suppressed":        stats.crashSuppressed.get(),
 		"vm restarts":       stats.vmRestarts.get(),
 		"new inputs":        stats.newInputs.get(),
-		"rotated inputs":    stats.rotatedInputs.get(),
 		"exec total":        stats.execTotal.get(),
 		"coverage":          stats.corpusCover.get(),
 		"filtered coverage": stats.corpusCoverFiltered.get(),
