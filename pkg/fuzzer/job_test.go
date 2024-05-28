701ad974ccc0ba0a61d2a2bd58db569f794bd037
Dmitry Vyukov <dvyukov@google.com>
2024-05-28 16:17:13 +0200

    pkg/fuzzer: refactor progTypes
    The next commit will add another Candidate flag.
    Candidate flags duplicate progTypes enum, so to avoid conversions
    of one to another use progTypes in Candidate struct directly.
    
    Rename progTypes to progFlags since multiple can be set,
    so this is effectively flags rather than a single type.

--- pkg/fuzzer/job_test.go
+++ pkg/fuzzer/job_test.go
@@ -29,7 +29,7 @@ func TestDeflakeFail(t *testing.T) {
 	}
 
 	run := 0
-	ret, stop := testJob.deflake(func(_ *queue.Request, _ ProgTypes) *queue.Result {
+	ret, stop := testJob.deflake(func(_ *queue.Request, _ ProgFlags) *queue.Result {
 		run++
 		// For first, we return 0 and 1. For second, 1 and 2. And so on.
 		return fakeResult(0, []uint64{uint64(run), uint64(run + 1)}, []uint64{10, 20})
@@ -54,7 +54,7 @@ func TestDeflakeSuccess(t *testing.T) {
 		newSignal: signal.FromRaw([]uint64{0, 1, 2}, 0),
 	}
 	run := 0
-	ret, stop := testJob.deflake(func(_ *queue.Request, _ ProgTypes) *queue.Result {
+	ret, stop := testJob.deflake(func(_ *queue.Request, _ ProgFlags) *queue.Result {
 		run++
 		switch run {
 		case 1:
