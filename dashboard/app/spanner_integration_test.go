9f1f6d8cc5b7bc997862e9f8471e77b2b4562eff
Taras Madan <tarasmadan@google.com>
2024-07-11 19:09:48 +0200

    dashboard/app: testcontainer to check it works

---
+++ dashboard/app/spanner_integration_test.go
@@ -0,0 +1,36 @@
+// Copyright 2024 syzkaller project authors. All rights reserved.
+// Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
+
+//go:build gcp_integration
+
+package main
+
+import (
+	"context"
+	"log"
+	"testing"
+
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
+)
+
+func TestWithRedis(t *testing.T) {
+	ctx := context.Background()
+	req := testcontainers.ContainerRequest{
+		Image:        "redis:latest",
+		ExposedPorts: []string{"6379/tcp"},
+		WaitingFor:   wait.ForLog("Ready to accept connections"),
+	}
+	redisC, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+	if err != nil {
+		log.Fatalf("Could not start redis: %s", err)
+	}
+	defer func() {
+		if err := redisC.Terminate(ctx); err != nil {
+			log.Fatalf("Could not stop redis: %s", err)
+		}
+	}()
+}
