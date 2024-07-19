cb93a42b625ea72d926b3ea5499546ef923d853a
Taras Madan <tarasmadan@google.com>
2024-07-19 16:25:12 +0200

    syz-covermerger: factor out db init code

--- tools/syz-bq.sh
+++ tools/syz-bq.sh
@@ -49,39 +49,6 @@ then
   exit
 fi
 
-db="coverage"
-
-# it also allows to early check gcloud credentials
-echo "making sure spanner table 'files' exists"
-create_table=$( echo -n '
-CREATE TABLE IF NOT EXISTS
-  files (
-    "session" text,
-    "filepath" text,
-    "instrumented" bigint,
-    "covered" bigint,
-  PRIMARY KEY
-    (session, filepath) );')
-gcloud spanner databases ddl update $db --instance=syzbot --project=syzkaller \
- --ddl="$create_table"
-
-echo "making sure spanner table 'merge_history' exists"
-create_table=$( echo -n '
-CREATE TABLE IF NOT EXISTS
-  merge_history (
-    "namespace" text,
-    "repo" text,
-    "duration" bigint,
-    "dateto" date,
-    "session" text,
-    "time" timestamptz,
-    "commit" text,
-    "totalrows" bigint,
-  PRIMARY KEY
-    (namespace, repo, duration, dateto) );')
-gcloud spanner databases ddl update $db --instance=syzbot --project=syzkaller \
- --ddl="$create_table"
-
 echo "Workdir: $workdir"
 base_dir="${workdir}repos/linux_kernels"
 if [ ! -d $base_dir ]; then
---
+++ tools/syz-covermerger/init_db.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+# Copyright 2024 syzkaller project authors. All rights reserved.
+# Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
+
+set -e # exit on any problem
+set -o pipefail
+
+db="coverage"
+echo "making sure spanner table 'files' exists"
+create_table=$( echo -n '
+CREATE TABLE IF NOT EXISTS
+  files (
+    "session" text,
+    "filepath" text,
+    "instrumented" bigint,
+    "covered" bigint,
+  PRIMARY KEY
+    (session, filepath) );')
+gcloud spanner databases ddl update $db --instance=syzbot --project=syzkaller \
+ --ddl="$create_table"
+
+echo "making sure spanner table 'merge_history' exists"
+create_table=$( echo -n '
+CREATE TABLE IF NOT EXISTS
+  merge_history (
+    "namespace" text,
+    "repo" text,
+    "duration" bigint,
+    "dateto" date,
+    "session" text,
+    "time" timestamptz,
+    "commit" text,
+    "totalrows" bigint,
+  PRIMARY KEY
+    (namespace, repo, duration, dateto) );')
+gcloud spanner databases ddl update $db --instance=syzbot --project=syzkaller \
+ --ddl="$create_table"
