2ebbc7259f2bda405779335b46a2f519f14ec473
Alexander Potapenko <glider@google.com>
2024-11-21 16:30:50 +0100

    executor: arm64: sys/linux: allocate 1024 pages for guest address space
    Pass 1024 pages of memory to both syz_kvm_setup_syzos_vm() and
    syz_kvm_setup_cpu$arm64() to make sure that:
     - there is enough memory for guest allocations (e.g. ITS pages)
     - host can tamper with that memory, provoking more bugs

--- executor/common_linux.h
+++ executor/common_linux.h
@@ -3222,7 +3222,7 @@ static long syz_kvm_add_vcpu(volatile long a0, volatile long a1, volatile long a
 }
 #endif
 #if !GOARCH_arm64 && (SYZ_EXECUTOR || __NR_syz_kvm_setup_syzos_vm)
-static long syz_kvm_setup_syzos_vm(volatile long a0)
+static long syz_kvm_setup_syzos_vm(volatile long a0, volatile long a1)
 {
 	return 0;
 }
