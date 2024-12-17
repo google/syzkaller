73b1d1880f6bbca9e2f9be7c9e88c2a40dbefa24
Aleksandr Nogikh <nogikh@google.com>
2024-12-17 15:38:19 +0100

    pkg/email/lore: extract patch series
    Refactor the code to make it more reusable.
    Add a method to extract specifically the list of new patch series.

--- pkg/email/lore/read.go
+++ pkg/email/lore/read.go
@@ -4,29 +4,49 @@
 package lore
 
 import (
+	"bytes"
 	"fmt"
+	"time"
 
+	"github.com/google/syzkaller/pkg/email"
 	"github.com/google/syzkaller/pkg/vcs"
 )
 
 type EmailReader struct {
-	Extract func() ([]byte, error)
+	Read func() ([]byte, error)
 }
 
 // ReadArchive queries the parsed messages from a single LKML message archive.
-func ReadArchive(dir string, messages chan<- *EmailReader) error {
-	repo := vcs.NewLKMLRepo(dir)
-	commits, err := repo.ListCommitHashes("HEAD")
+func ReadArchive(repo vcs.Repo, fromTime time.Time) ([]EmailReader, error) {
+	commits, err := repo.ListCommitHashes("HEAD", fromTime)
 	if err != nil {
-		return fmt.Errorf("failed to get recent commits: %w", err)
+		return nil, fmt.Errorf("failed to get recent commits: %w", err)
 	}
+	var ret []EmailReader
 	for _, iterCommit := range commits {
 		commit := iterCommit
-		messages <- &EmailReader{
-			Extract: func() ([]byte, error) {
+		ret = append(ret, EmailReader{
+			Read: func() ([]byte, error) {
 				return repo.Object("m", commit)
 			},
-		}
+		})
 	}
-	return nil
+	return ret, nil
+}
+
+func (er *EmailReader) Parse(emails, domains []string) (*email.Email, error) {
+	body, err := er.Read()
+	if err != nil {
+		return nil, err
+	}
+	msg, err := email.Parse(bytes.NewReader(body), emails, nil, domains)
+	if err != nil {
+		return nil, err
+	}
+	// Keep memory consumption low.
+	msg.Body = ""
+	msg.Patch = ""
+	// TODO: We definitely don't care about the patch here. Add an option to avoid extracting it?
+	// TODO: If emails/domains are nil, we also don't need to parse the body at all.
+	return msg, nil
 }
