7d4c3984359e4edd043f5e75be65674d58cd5950
Taras Madan <tarasmadan@google.com>
2025-04-08 12:06:03 +0200

    dashboard/app: enable coverage pages formatting
    cover.Format controls the resulting view.
    It allows to:
    1. Remove records with 0 covered blocks.
    2. Remove lines with low (user defined) coverage.
    3. Order records by the covered lines drop value.
    
    The corresponding GET parameters are:
    1. Implicitly enabled for onlyUnique records.
    2. min-cover-lines-drop=%d
    3. order-by-cover-lines-drop=1

--- pkg/cover/heatmap_test.go
+++ pkg/cover/heatmap_test.go
@@ -14,10 +14,9 @@ import (
 
 func TestFilesCoverageToTemplateData(t *testing.T) {
 	tests := []struct {
-		name      string
-		input     []*coveragedb.FileCoverageWithDetails
-		hideEmpty bool
-		want      *templateHeatmap
+		name  string
+		input []*coveragedb.FileCoverageWithDetails
+		want  *templateHeatmap
 	}{
 		{
 			name:  "empty input",
@@ -43,11 +42,12 @@ func TestFilesCoverageToTemplateData(t *testing.T) {
 				Root: &templateHeatmapRow{
 					Items: []*templateHeatmapRow{
 						{
-							Name:                "file1",
-							Coverage:            []int64{100},
-							IsDir:               false,
-							Depth:               0,
-							LastDayInstrumented: 1,
+							Name:     "file1",
+							Coverage: []int64{100},
+							Covered:  []int64{1},
+							IsDir:    false,
+							Depth:    0,
+							Summary:  1,
 							Tooltips: []string{
 								"Instrumented:\t1 blocks\nCovered:\t1 blocks",
 							},
@@ -55,11 +55,12 @@ func TestFilesCoverageToTemplateData(t *testing.T) {
 								"/coverage/file?dateto=2024-07-01&period=day&commit=commit1&filepath=file1"},
 						},
 					},
-					Name:                "",
-					Coverage:            []int64{100},
-					IsDir:               true,
-					Depth:               0,
-					LastDayInstrumented: 1,
+					Name:     "",
+					Coverage: []int64{100},
+					Covered:  []int64{1},
+					IsDir:    true,
+					Depth:    0,
+					Summary:  1,
 					Tooltips: []string{
 						"Instrumented:\t1 blocks\nCovered:\t1 blocks",
 					},
@@ -91,11 +92,12 @@ func TestFilesCoverageToTemplateData(t *testing.T) {
 						{
 							Items: []*templateHeatmapRow{
 								{
-									Name:                "file1",
-									Coverage:            []int64{100, 0},
-									IsDir:               false,
-									Depth:               1,
-									LastDayInstrumented: 0,
+									Name:     "file1",
+									Coverage: []int64{100, 0},
+									Covered:  []int64{1, 0},
+									IsDir:    false,
+									Depth:    1,
+									Summary:  0,
 									Tooltips: []string{
 										"Instrumented:\t1 blocks\nCovered:\t1 blocks",
 										"Instrumented:\t0 blocks\nCovered:\t0 blocks",
@@ -105,11 +107,12 @@ func TestFilesCoverageToTemplateData(t *testing.T) {
 										"/coverage/file?dateto=2024-07-02&period=day&commit=commit2&filepath=dir/file1"},
 								},
 								{
-									Name:                "file2",
-									Coverage:            []int64{0, 0},
-									IsDir:               false,
-									Depth:               1,
-									LastDayInstrumented: 1,
+									Name:     "file2",
+									Coverage: []int64{0, 0},
+									Covered:  []int64{0, 0},
+									IsDir:    false,
+									Depth:    1,
+									Summary:  1,
 									Tooltips: []string{
 										"Instrumented:\t0 blocks\nCovered:\t0 blocks",
 										"Instrumented:\t1 blocks\nCovered:\t0 blocks",
@@ -119,20 +122,22 @@ func TestFilesCoverageToTemplateData(t *testing.T) {
 										"/coverage/file?dateto=2024-07-02&period=day&commit=commit2&filepath=dir/file2"},
 								},
 							},
-							Name:                "dir",
-							Coverage:            []int64{100, 0},
-							IsDir:               true,
-							Depth:               0,
-							LastDayInstrumented: 1,
+							Name:     "dir",
+							Coverage: []int64{100, 0},
+							Covered:  []int64{1, 0},
+							IsDir:    true,
+							Depth:    0,
+							Summary:  1,
 							Tooltips: []string{
 								"Instrumented:\t1 blocks\nCovered:\t1 blocks",
 								"Instrumented:\t1 blocks\nCovered:\t0 blocks",
 							},
 						},
 					},
-					Name:                "",
-					Coverage:            []int64{100, 0},
-					LastDayInstrumented: 1,
+					Name:     "",
+					Coverage: []int64{100, 0},
+					Covered:  []int64{1, 0},
+					Summary:  1,
 					Tooltips: []string{
 						"Instrumented:\t1 blocks\nCovered:\t1 blocks",
 						"Instrumented:\t1 blocks\nCovered:\t0 blocks",
@@ -142,33 +147,176 @@ func TestFilesCoverageToTemplateData(t *testing.T) {
 				Periods: []string{"2024-07-01(1)", "2024-07-02(1)"},
 			},
 		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got := filesCoverageToTemplateData(test.input)
+			assert.EqualExportedValues(t, test.want, got)
+		})
+	}
+}
+
+// nolint: dupl
+func TestFormatResult(t *testing.T) {
+	tests := []struct {
+		name   string
+		sample *templateHeatmap
+		input  Format
+		want   *templateHeatmap
+	}{
 		{
-			name:      "hide empty",
-			hideEmpty: true,
-			input: []*coveragedb.FileCoverageWithDetails{
-				{
-					Filepath:     "file1",
-					Instrumented: 1,
-					Covered:      0,
-					TimePeriod:   makeTimePeriod(t, civil.Date{Year: 2024, Month: time.July, Day: 1}, coveragedb.DayPeriod),
-					Commit:       "commit1",
+			name: "remove empty",
+			input: Format{
+				DropCoveredLines0: true,
+			},
+			sample: &templateHeatmap{
+				Root: &templateHeatmapRow{
+					Items: []*templateHeatmapRow{
+						{
+							Items: []*templateHeatmapRow{
+								{
+									Name:    "file1",
+									Covered: []int64{0, 0},
+									Depth:   1,
+								},
+							},
+							Name:    "dir",
+							Covered: []int64{1, 0},
+							IsDir:   true,
+						},
+					},
+					Covered: []int64{1, 0},
+					IsDir:   true,
 				},
 			},
 			want: &templateHeatmap{
 				Root: &templateHeatmapRow{
-					Coverage:            []int64{0},
-					IsDir:               true,
-					LastDayInstrumented: 1,
-					Tooltips:            []string{"Instrumented:\t1 blocks\nCovered:\t0 blocks"},
+					Items: []*templateHeatmapRow{
+						{
+							Name:    "dir",
+							Covered: []int64{1, 0},
+							IsDir:   true,
+						},
+					},
+					Covered: []int64{1, 0},
+					IsDir:   true,
 				},
-				Periods: []string{"2024-07-01(1)"},
+			},
+		},
+		{
+			name: "remove small lines drop",
+			input: Format{
+				FilterMinCoveredLinesDrop: 10,
+			},
+			sample: &templateHeatmap{
+				Root: &templateHeatmapRow{
+					Items: []*templateHeatmapRow{
+						{
+							Items: []*templateHeatmapRow{
+								{
+									Name:    "file1",
+									Covered: []int64{5, 0},
+									Depth:   1,
+								},
+								{
+									Name:    "file2",
+									Covered: []int64{10, 0},
+									Depth:   1,
+								},
+							},
+							Name:    "dir",
+							Covered: []int64{1, 1},
+							IsDir:   true,
+						},
+					},
+					Covered: []int64{1, 1},
+					IsDir:   true,
+				},
+			},
+			want: &templateHeatmap{
+				Root: &templateHeatmapRow{
+					Items: []*templateHeatmapRow{
+						{
+							Items: []*templateHeatmapRow{
+								{
+									Name:    "file2",
+									Covered: []int64{10, 0},
+									Depth:   1,
+								},
+							},
+							Name:    "dir",
+							Covered: []int64{1, 1},
+							IsDir:   true,
+						},
+					},
+					Covered: []int64{1, 1},
+					IsDir:   true,
+				},
+			},
+		},
+		{
+			name: "order by the lines drop",
+			input: Format{
+				OrderByCoveredLinesDrop: true,
+			},
+			sample: &templateHeatmap{
+				Root: &templateHeatmapRow{
+					Items: []*templateHeatmapRow{
+						{
+							Items: []*templateHeatmapRow{
+								{
+									Name:    "file1",
+									Covered: []int64{5, 0},
+									Depth:   1,
+								},
+								{
+									Name:    "file2",
+									Covered: []int64{10, 0},
+									Depth:   1,
+								},
+							},
+							Name:    "dir",
+							Covered: []int64{1, 1},
+							IsDir:   true,
+						},
+					},
+					Covered: []int64{1, 1},
+					IsDir:   true,
+				},
+			},
+			want: &templateHeatmap{
+				Root: &templateHeatmapRow{
+					Items: []*templateHeatmapRow{
+						{
+							Items: []*templateHeatmapRow{
+								{
+									Name:    "file2",
+									Covered: []int64{10, 0},
+									Depth:   1,
+									Summary: -10,
+								},
+								{
+									Name:    "file1",
+									Covered: []int64{5, 0},
+									Depth:   1,
+									Summary: -5,
+								},
+							},
+							Name:    "dir",
+							Covered: []int64{1, 1},
+							IsDir:   true,
+						},
+					},
+					Covered: []int64{1, 1},
+					IsDir:   true,
+				},
 			},
 		},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			got := filesCoverageToTemplateData(test.input, test.hideEmpty)
-			assert.EqualExportedValues(t, test.want, got)
+			FormatResult(test.sample, test.input)
+			assert.Equal(t, test.want, test.sample)
 		})
 	}
 }
