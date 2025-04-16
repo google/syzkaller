552876f8f8f64b90e59c3ecc76806b04f4a30b1c
Aleksandr Nogikh <nogikh@google.com>
2025-04-16 13:12:17 +0200

    syz-manager: ignore context.Canceled error on shutdown
    These are perfectly normal since we are stopping all the functionality.

--- syz-manager/manager.go
+++ syz-manager/manager.go
@@ -495,7 +495,7 @@ func reportReproError(err error) {
 		// The kernel could have crashed before we executed any programs.
 		log.Logf(0, "repro failed: %v", err)
 		return
-	} else if errors.Is(err, repro.ErrNoVMs) {
+	} else if errors.Is(err, repro.ErrNoVMs) || errors.Is(err, context.Canceled) {
 		// This error is to be expected if we're shutting down.
 		if shutdown {
 			return
