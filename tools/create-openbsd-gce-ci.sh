6a893178a1efc0914c4dcedf379aaeeb5fd2b42e
Greg Steuck <greg@nest.cx>
2025-08-07 06:49:36 -0700

    tools: openbsd workarounds to use clang-19
    That's the current version in the tree.

--- tools/create-openbsd-gce-ci.sh
+++ tools/create-openbsd-gce-ci.sh
@@ -42,9 +42,9 @@ echo 'permit keepenv nopass syzkaller as root' > /etc/doas.conf
 mkdir /syzkaller
 echo '/dev/sd1a /syzkaller ffs rw,noauto 1 0' >> /etc/fstab
 
-mkdir -p /usr/lib/clang/16/lib/openbsd
-ln -s /usr/lib/clang/16/lib/libclang_rt.ubsan_minimal.a /usr/lib/clang/16/lib/openbsd/libclang_rt.ubsan_standalone-x86_64.a
-touch /usr/lib/clang/16/lib/openbsd/libclang_rt.ubsan_standalone_cxx-x86_64.a
+mkdir -p /usr/lib/clang/19/lib/openbsd
+ln -s /usr/lib/clang/19/lib/libclang_rt.ubsan_minimal.a /usr/lib/clang/19/lib/openbsd/libclang_rt.ubsan_standalone-x86_64.a
+touch /usr/lib/clang/19/lib/openbsd/libclang_rt.ubsan_standalone_cxx-x86_64.a
 EOF
 
 cat >etc/installurl <<EOF
