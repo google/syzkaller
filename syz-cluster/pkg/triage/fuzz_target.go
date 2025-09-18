efea798d3388a8d93de90657096140d37bf1d49c
Aleksandr Nogikh <nogikh@google.com>
2025-09-18 17:36:53 +0200

    syz-cluster: rewrite fuzz config generation
    Instead of a predefined set of manually written syz-manager configs,
    construct it dynamically from different bits.
    
    During triage, select not just one, but all matching fuzzer
    configurations and then merge them together.

--- syz-cluster/pkg/triage/fuzz_target.go
+++ syz-cluster/pkg/triage/fuzz_target.go
@@ -4,25 +4,97 @@
 package triage
 
 import (
+	"sort"
 	"strings"
 
 	"github.com/google/syzkaller/syz-cluster/pkg/api"
 )
 
-func SelectFuzzConfig(series *api.Series, fuzzConfigs []*api.FuzzTriageTarget) *api.FuzzTriageTarget {
+func SelectFuzzConfigs(series *api.Series, fuzzConfigs []*api.FuzzTriageTarget) []*api.KernelFuzzConfig {
 	seriesCc := map[string]bool{}
 	for _, cc := range series.Cc {
 		seriesCc[strings.ToLower(cc)] = true
 	}
+	var ret, defaultRet []*api.KernelFuzzConfig
 	for _, config := range fuzzConfigs {
 		intersects := false
 		for _, cc := range config.EmailLists {
 			intersects = intersects || seriesCc[cc]
 		}
-		if len(config.EmailLists) != 0 && !intersects {
-			continue
+		if intersects {
+			ret = append(ret, config.Campaigns...)
+		} else if len(config.EmailLists) == 0 {
+			defaultRet = append(defaultRet, config.Campaigns...)
 		}
-		return config
 	}
-	return nil
+	// We want to return the fallback option only if no element matched exactly.
+	if len(ret) > 0 {
+		return ret
+	}
+	return defaultRet
+}
+
+type MergedFuzzConfig struct {
+	KernelConfig string
+	FuzzConfig   *api.FuzzConfig
+}
+
+func MergeKernelFuzzConfigs(configs []*api.KernelFuzzConfig) []*MergedFuzzConfig {
+	type groupKey struct {
+		config     string
+		track      string
+		bugTitleRe string
+	}
+	groups := map[groupKey][]*api.KernelFuzzConfig{}
+	var orderedKeys []groupKey
+	for _, config := range configs {
+		// Some of the different fuzz configs may still be merged together,
+		// e.g. if they only differ in the syscall lists and corpuses.
+		key := groupKey{config.KernelConfig, config.Track, config.BugTitleRe}
+		if _, ok := groups[key]; !ok {
+			orderedKeys = append(orderedKeys, key)
+		}
+		groups[key] = append(groups[key], config)
+	}
+	var ret []*MergedFuzzConfig
+	for _, key := range orderedKeys {
+		// TODO: is there way to auto-generate a prefix?
+		ret = append(ret, &MergedFuzzConfig{
+			KernelConfig: key.config,
+			FuzzConfig:   mergeFuzzConfigs(groups[key]),
+		})
+	}
+	return ret
+}
+
+func mergeFuzzConfigs(configs []*api.KernelFuzzConfig) *api.FuzzConfig {
+	var ret api.FuzzConfig
+	for _, config := range configs {
+		if config.Focus != "" {
+			ret.Focus = append(ret.Focus, config.Focus)
+		}
+		if config.CorpusURL != "" {
+			ret.CorpusURLs = append(ret.CorpusURLs, config.CorpusURL)
+		}
+		ret.SkipCoverCheck = ret.SkipCoverCheck || config.SkipCoverCheck
+		// Must be the same.
+		ret.BugTitleRe = config.BugTitleRe
+		ret.Track = config.Track
+	}
+	ret.Focus = unique(ret.Focus)
+	ret.CorpusURLs = unique(ret.CorpusURLs)
+	return &ret
+}
+
+func unique(list []string) []string {
+	seen := make(map[string]struct{}, len(list))
+	for _, s := range list {
+		seen[s] = struct{}{}
+	}
+	var unique []string
+	for s := range seen {
+		unique = append(unique, s)
+	}
+	sort.Strings(unique)
+	return unique
 }
