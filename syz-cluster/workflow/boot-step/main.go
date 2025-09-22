d4d5f8849c45f29e23bb4b56d1f02e89730b816b
Aleksandr Nogikh <nogikh@google.com>
2025-09-22 11:07:47 +0200

    syz-cluster: log boot step findings
    If a boot test step failed and we don't report the finding to the
    dashboard, print the report/output to the console to facilitate
    debugging.

--- syz-cluster/workflow/boot-step/main.go
+++ syz-cluster/workflow/boot-step/main.go
@@ -113,6 +113,9 @@ func runTest(ctx context.Context, client *api.Client) (bool, error) {
 		if findingErr != nil {
 			return false, fmt.Errorf("failed to report the finding: %w", findingErr)
 		}
+	} else {
+		log.Printf("report:\n%s", rep.Report)
+		log.Printf("output:\n%s", rep.Output)
 	}
 	return false, nil
 }
