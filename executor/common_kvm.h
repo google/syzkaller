32020baac439e072d25871dcef06a13c5a51e270
Alexander Potapenko <glider@google.com>
2025-10-17 15:08:11 +0200

    executor: common_kvm.h: fix compilation
    Add #if checks to define executor_fn_guest_addr() for
    __NR_syz_kvm_setup_cpu and __NR_syz_kvm_setup_syzos_vm.
    This fixes a compilation error spotted by csource_test.go

--- executor/common_kvm.h
+++ executor/common_kvm.h
@@ -8,7 +8,7 @@
 #include "common_kvm_syzos.h"
 #include "kvm.h"
 
-#if SYZ_EXECUTOR || __NR_syz_kvm_add_vcpu
+#if SYZ_EXECUTOR || __NR_syz_kvm_add_vcpu || __NR_syz_kvm_setup_cpu || __NR_syz_kvm_setup_syzos_vm
 extern char* __start_guest;
 
 // executor_fn_guest_addr() is compiled into both the host and the guest code.
