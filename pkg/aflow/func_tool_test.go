a9988a8e345c8786716ca1aa506338b596eaaa24
Dmitry Vyukov <dvyukov@google.com>
2026-01-21 19:42:47 +0100

    pkg/aflow: refactor tests
    Add helper function that executes test workflows,
    compares results (trajectory, LLM requests) against golden files,
    and if requested updates these golden files.

--- pkg/aflow/func_tool_test.go
+++ pkg/aflow/func_tool_test.go
@@ -4,15 +4,9 @@
 package aflow
 
 import (
-	"context"
 	"errors"
-	"path/filepath"
 	"testing"
-	"time"
 
-	"github.com/google/syzkaller/pkg/aflow/trajectory"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 	"google.golang.org/genai"
 )
 
@@ -23,90 +17,43 @@ func TestToolErrors(t *testing.T) {
 	type toolArgs struct {
 		CallError bool `jsonschema:"call error"`
 	}
-	flows := make(map[string]*Flow)
-	err := register[struct{}, flowOutputs]("test", "description", flows, []*Flow{
-		{
-			Root: &LLMAgent{
-				Name:        "smarty",
-				Model:       "model",
-				Reply:       "Reply",
-				Temperature: 0,
-				Instruction: "Do something!",
-				Prompt:      "Prompt",
-				Tools: []Tool{
-					NewFuncTool("faulty", func(ctx *Context, state struct{}, args toolArgs) (struct{}, error) {
-						if args.CallError {
-							return struct{}{}, BadCallError("you are wrong")
-						}
-						return struct{}{}, errors.New("hard error")
-					}, "tool 1 description"),
+	testFlow[struct{}, flowOutputs](t, nil,
+		"tool faulty failed: error: hard error\nargs: map[CallError:false]",
+		&LLMAgent{
+			Name:        "smarty",
+			Model:       "model",
+			Reply:       "Reply",
+			Temperature: 0,
+			Instruction: "Do something!",
+			Prompt:      "Prompt",
+			Tools: []Tool{
+				NewFuncTool("faulty", func(ctx *Context, state struct{}, args toolArgs) (struct{}, error) {
+					if args.CallError {
+						return struct{}{}, BadCallError("you are wrong")
+					}
+					return struct{}{}, errors.New("hard error")
+				}, "tool 1 description"),
+			},
+		},
+		[]any{
+			&genai.Part{
+				FunctionCall: &genai.FunctionCall{
+					ID:   "id0",
+					Name: "faulty",
+					Args: map[string]any{
+						"CallError": true,
+					},
+				},
+			},
+			&genai.Part{
+				FunctionCall: &genai.FunctionCall{
+					ID:   "id0",
+					Name: "faulty",
+					Args: map[string]any{
+						"CallError": false,
+					},
 				},
 			},
 		},
-	})
-	require.NoError(t, err)
-	replySeq := 0
-	stub := &stubContext{
-		// nolint:dupl
-		generateContent: func(model string, cfg *genai.GenerateContentConfig, req []*genai.Content) (
-			*genai.GenerateContentResponse, error) {
-			replySeq++
-			switch replySeq {
-			case 1:
-				return &genai.GenerateContentResponse{
-					Candidates: []*genai.Candidate{{
-						Content: &genai.Content{
-							Role: string(genai.RoleModel),
-							Parts: []*genai.Part{
-								{
-									FunctionCall: &genai.FunctionCall{
-										ID:   "id0",
-										Name: "faulty",
-										Args: map[string]any{
-											"CallError": true,
-										},
-									},
-								},
-							}}}}}, nil
-			case 2:
-				assert.Equal(t, req[2], &genai.Content{
-					Role: string(genai.RoleUser),
-					Parts: []*genai.Part{
-						{
-							FunctionResponse: &genai.FunctionResponse{
-								ID:   "id0",
-								Name: "faulty",
-								Response: map[string]any{
-									"error": "you are wrong",
-								},
-							},
-						}}})
-				return &genai.GenerateContentResponse{
-					Candidates: []*genai.Candidate{{
-						Content: &genai.Content{
-							Role: string(genai.RoleModel),
-							Parts: []*genai.Part{
-								{
-									FunctionCall: &genai.FunctionCall{
-										ID:   "id0",
-										Name: "faulty",
-										Args: map[string]any{
-											"CallError": false,
-										},
-									},
-								},
-							}}}}}, nil
-			default:
-				t.Fatal("unexpected LLM calls")
-				return nil, nil
-			}
-		},
-	}
-	ctx := context.WithValue(context.Background(), stubContextKey, stub)
-	workdir := t.TempDir()
-	cache, err := newTestCache(t, filepath.Join(workdir, "cache"), 0, time.Now)
-	require.NoError(t, err)
-	onEvent := func(span *trajectory.Span) error { return nil }
-	_, err = flows["test"].Execute(ctx, "", workdir, nil, cache, onEvent)
-	require.Equal(t, err.Error(), "tool faulty failed: error: hard error\nargs: map[CallError:false]")
+	)
 }
