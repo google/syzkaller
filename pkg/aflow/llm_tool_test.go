778c365cd9979cc5ab5eb7df8e7ec5d1ab834495
Dmitry Vyukov <dvyukov@google.com>
2026-01-22 11:48:05 +0100

    pkg/aflow: add LLMTool
    LLMTool acts like a tool for the parent LLM, but is itself implemented as an LLM agent.
    It can have own tools, different from the parent LLM agent.
    It can do complex multi-step research, and provide a concise answer to the parent LLM
    without polluting its context window.

---
+++ pkg/aflow/llm_tool_test.go
@@ -0,0 +1,99 @@
+// Copyright 2026 syzkaller project authors. All rights reserved.
+// Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
+
+package aflow
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/genai"
+)
+
+func TestLLMTool(t *testing.T) {
+	type inputs struct {
+		Input int
+	}
+	type outputs struct {
+		Reply string
+	}
+	type toolArgs struct {
+		Something string `jsonschema:"something"`
+	}
+	testFlow[inputs, outputs](t, map[string]any{"Input": 42}, map[string]any{"Reply": "YES"},
+		NewPipeline(
+			&LLMAgent{
+				Name:        "smarty",
+				Model:       "model",
+				Temperature: 1,
+				Reply:       "Reply",
+				Instruction: "Do something!",
+				Prompt:      "Prompt",
+				Tools: []Tool{
+					&LLMTool{
+						Name:        "researcher",
+						Model:       "sub-agent-model",
+						Temperature: 2,
+						Description: "researcher description",
+						Instruction: "researcher instruction",
+						Tools: []Tool{
+							NewFuncTool("researcher-tool", func(ctx *Context, state inputs, args toolArgs) (struct{}, error) {
+								// State passed all the way from the workflow inputs.
+								assert.Equal(t, state.Input, 42)
+								assert.True(t, args.Something == "subtool input 1" || args.Something == "subtool input 2",
+									"args.Something=%q", args.Something)
+								return struct{}{}, nil
+							}, "researcher-tool description"),
+						},
+					},
+				},
+			},
+		),
+		[]any{
+			// Main agent calls the tool sub-agent.
+			&genai.Part{
+				FunctionCall: &genai.FunctionCall{
+					ID:   "id0",
+					Name: "researcher",
+					Args: map[string]any{
+						"Question": "What do you think?",
+					},
+				},
+			},
+			// Sub-agent calls own tool.
+			&genai.Part{
+				FunctionCall: &genai.FunctionCall{
+					ID:   "id1",
+					Name: "researcher-tool",
+					Args: map[string]any{
+						"Something": "subtool input 1",
+					},
+				},
+			},
+			// Sub-agent returns result.
+			genai.NewPartFromText("Nothing."),
+			// Repeat the same one more time.
+			&genai.Part{
+				FunctionCall: &genai.FunctionCall{
+					ID:   "id2",
+					Name: "researcher",
+					Args: map[string]any{
+						"Question": "But really?",
+					},
+				},
+			},
+			&genai.Part{
+				FunctionCall: &genai.FunctionCall{
+					ID:   "id2",
+					Name: "researcher-tool",
+					Args: map[string]any{
+						"Something": "subtool input 2",
+					},
+				},
+			},
+			genai.NewPartFromText("Still nothing."),
+			// Main returns result.
+			genai.NewPartFromText("YES"),
+		},
+	)
+}
