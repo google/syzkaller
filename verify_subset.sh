c64b1f14572108e5fbce40c4718ec86abdaf820f
Taras Madan <tarasmadan@google.com>
2026-01-27 21:39:28 +0100

    syzkaller: fix verification scripts lint errors

--- verify_subset.sh
+++ verify_subset.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# Copyright 2025 syzkaller project authors. All rights reserved.
+# Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
+
 # fast verification on subset
 set -e
 
