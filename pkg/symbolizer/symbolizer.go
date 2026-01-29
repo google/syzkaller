7b77f05dc37a865188a0fb25882d03d1250af851
Taras Madan <tarasmadan@google.com>
2026-01-29 16:10:09 +0100

    pkg/symbolizer: fix performance issues
    25s -> 12s cold
    2s cold

--- pkg/symbolizer/symbolizer.go
+++ pkg/symbolizer/symbolizer.go
@@ -3,7 +3,12 @@
 
 package symbolizer
 
-import "github.com/google/syzkaller/sys/targets"
+import (
+	"os"
+	"sync"
+
+	"github.com/google/syzkaller/sys/targets"
+)
 
 type Frame struct {
 	PC     uint64
@@ -22,9 +27,42 @@ type Symbolizer interface {
 
 func Make(target *targets.Target, bin string) (Symbolizer, error) {
 	if target != nil && target.Arch == targets.AMD64 && bin != "" {
+		cacheMu.Lock()
+		defer cacheMu.Unlock()
+		if entry, ok := cache[bin]; ok {
+			if info, err := os.Stat(bin); err == nil && os.SameFile(info, entry.info) && info.ModTime().Equal(entry.info.ModTime()) {
+				return &sharedSymbolizer{entry.sym}, nil
+			}
+			delete(cache, bin)
+			// We can't close the old symbolizer easily as it might be in use.
+			// Rely on GC or leak (it's small number of kernels usually).
+			// Ideally we should Close() it when refcount drops, but that's complex.
+			// For now, let's assume valid kernels are few.
+		}
+
 		if s, err := newELFSymbolizer(bin); err == nil {
+			if info, err := os.Stat(bin); err == nil {
+				cache[bin] = &cachedSym{s, info}
+				return &sharedSymbolizer{s}, nil
+			}
 			return s, nil
 		}
 	}
 	return &addr2Line{target: target}, nil
 }
+
+var (
+	cacheMu sync.Mutex
+	cache   = make(map[string]*cachedSym)
+)
+
+type cachedSym struct {
+	sym  Symbolizer
+	info os.FileInfo
+}
+
+type sharedSymbolizer struct {
+	Symbolizer
+}
+
+func (s *sharedSymbolizer) Close() {}
