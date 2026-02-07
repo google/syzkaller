9ca304f096ce425e3556bbd05745f896f5e0b268
Aleksandr Nogikh <nogikh@google.com>
2026-02-07 19:07:44 +0100

    vm/vmimpl: refactor Merger error processing
    Introduce an Error() method to avoid capturing the errors of already
    overridden decoders.

--- vm/vmimpl/merger.go
+++ vm/vmimpl/merger.go
@@ -5,9 +5,12 @@ package vmimpl
 
 import (
 	"bytes"
+	"context"
 	"fmt"
 	"io"
 	"sync"
+
+	"golang.org/x/sync/errgroup"
 )
 
 type OutputType int
@@ -23,12 +26,17 @@ type Chunk struct {
 	Type OutputType
 }
 
+type decoderState struct {
+	done chan struct{} // Closed when the decoder exits.
+	err  error
+}
+
 type OutputMerger struct {
-	Output chan Chunk
-	Err    chan error
-	teeMu  sync.Mutex
-	tee    io.Writer
-	wg     sync.WaitGroup
+	Output     chan Chunk
+	decoderErr map[string]*decoderState
+	teeMu      sync.Mutex
+	tee        io.Writer
+	wg         sync.WaitGroup
 }
 
 type MergerError struct {
@@ -43,9 +51,9 @@ func (err MergerError) Error() string {
 
 func NewOutputMerger(tee io.Writer) *OutputMerger {
 	return &OutputMerger{
-		Output: make(chan Chunk, 1000),
-		Err:    make(chan error, 1),
-		tee:    tee,
+		Output:     make(chan Chunk, 1000),
+		decoderErr: map[string]*decoderState{},
+		tee:        tee,
 	}
 }
 
@@ -54,70 +62,100 @@ func (merger *OutputMerger) Wait() {
 	close(merger.Output)
 }
 
+// Errors returns a channel that will receive errors from the curretly active decoderErr.
+func (merger *OutputMerger) Errors(ctx context.Context) <-chan error {
+	eg, egCtx := errgroup.WithContext(ctx)
+	for _, decoder := range merger.decoderErr {
+		eg.Go(func() error {
+			select {
+			case <-egCtx.Done():
+				return nil
+			case <-decoder.done:
+				return decoder.err
+			}
+		})
+	}
+	ret := make(chan error, 1)
+	go func() {
+		err := eg.Wait()
+		if err != nil {
+			ret <- err
+		}
+		close(ret)
+	}()
+	return ret
+}
+
 func (merger *OutputMerger) Add(name string, typ OutputType, r io.ReadCloser) {
 	merger.AddDecoder(name, typ, r, nil)
 }
 
 func (merger *OutputMerger) AddDecoder(name string, typ OutputType, r io.ReadCloser,
 	decoder func(data []byte) (start, size int, decoded []byte)) {
+	state := &decoderState{
+		done: make(chan struct{}),
+	}
+	merger.decoderErr[name] = state
 	merger.wg.Add(1)
 	go func() {
-		var pending []byte
-		var proto []byte
-		var buf [4 << 10]byte
-		for {
-			n, err := r.Read(buf[:])
-			if n != 0 {
-				if decoder != nil {
-					proto = append(proto, buf[:n]...)
-					start, size, decoded := decoder(proto)
-					proto = proto[start+size:]
-					if len(decoded) != 0 {
-						merger.Output <- Chunk{decoded, typ} // note: this can block
-					}
-				}
-				// Remove all carriage returns.
-				buf := buf[:n]
-				if bytes.IndexByte(buf, '\r') != -1 {
-					buf = bytes.ReplaceAll(buf, []byte("\r"), nil)
-				}
-				pending = append(pending, buf...)
-				if pos := bytes.LastIndexByte(pending, '\n'); pos != -1 {
-					out := pending[:pos+1]
-					if merger.tee != nil {
-						merger.teeMu.Lock()
-						merger.tee.Write(out)
-						merger.teeMu.Unlock()
-					}
-					select {
-					case merger.Output <- Chunk{append([]byte{}, out...), typ}:
-						r := copy(pending, pending[pos+1:])
-						pending = pending[:r]
-					default:
-					}
+		defer merger.wg.Done()
+		defer close(state.done)
+		err := merger.runDecoder(typ, r, decoder)
+		state.err = MergerError{name, r, err}
+	}()
+}
+func (merger *OutputMerger) runDecoder(typ OutputType, r io.ReadCloser,
+	decoder func(data []byte) (start, size int, decoded []byte)) error {
+	var pending []byte
+	var proto []byte
+	var buf [4 << 10]byte
+	for {
+		n, err := r.Read(buf[:])
+		if n != 0 {
+			if decoder != nil {
+				proto = append(proto, buf[:n]...)
+				start, size, decoded := decoder(proto)
+				proto = proto[start+size:]
+				if len(decoded) != 0 {
+					merger.Output <- Chunk{decoded, typ} // note: this can block
 				}
 			}
-			if err != nil {
-				if len(pending) != 0 {
-					pending = append(pending, '\n')
-					if merger.tee != nil {
-						merger.teeMu.Lock()
-						merger.tee.Write(pending)
-						merger.teeMu.Unlock()
-					}
-					select {
-					case merger.Output <- Chunk{pending, typ}:
-					default:
-					}
+			// Remove all carriage returns.
+			buf := buf[:n]
+			if bytes.IndexByte(buf, '\r') != -1 {
+				buf = bytes.ReplaceAll(buf, []byte("\r"), nil)
+			}
+			pending = append(pending, buf...)
+			if pos := bytes.LastIndexByte(pending, '\n'); pos != -1 {
+				out := pending[:pos+1]
+				if merger.tee != nil {
+					merger.teeMu.Lock()
+					merger.tee.Write(out)
+					merger.teeMu.Unlock()
 				}
-				r.Close()
 				select {
-				case merger.Err <- MergerError{name, r, err}:
+				case merger.Output <- Chunk{append([]byte{}, out...), typ}:
+					r := copy(pending, pending[pos+1:])
+					pending = pending[:r]
 				default:
 				}
-				merger.wg.Done()
-				return
 			}
 		}
-	}()
+		if err != nil {
+			if len(pending) != 0 {
+				pending = append(pending, '\n')
+				if merger.tee != nil {
+					merger.teeMu.Lock()
+					merger.tee.Write(pending)
+					merger.teeMu.Unlock()
+				}
+				select {
+				case merger.Output <- Chunk{pending, typ}:
+				default:
+				}
+			}
+			r.Close()
+			return err
+		}
+	}
 }
