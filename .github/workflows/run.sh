4807eb5b2790fdbbb991e847e81000fa8be0f0cc
vineet  patil <vineet.v88888@gmail.com>
2026-02-09 16:47:44 +0530

    Update run.sh

--- .github/workflows/run.sh
+++ .github/workflows/run.sh
@@ -1,11 +1,1 @@
-#!/usr/bin/env bash
-# Copyright 2020 syzkaller project authors. All rights reserved.
-# Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
 
-export HOME="$PWD"
-mkdir -p .cache
-set -o pipefail
-# Run the specified command and convert error messages to github format:
-# https://help.github.com/en/actions/reference/workflow-commands-for-github-actions#setting-an-error-message
-$1 "${@:2}" | \
-	sed -E 's#\s*([a-zA-Z0-9._/-]+):([0-9]+):(([0-9]+):)? (.+)#\0\n::error file=\1,line=\2,col=0\4::\5#'
