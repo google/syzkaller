d1c5fd541897c61e87070943546cf0fbb2b37382
Aleksandr Nogikh <nogikh@google.com>
2026-02-11 11:40:10 +0000

    prog: generate no_generate syscalls for tests
    Only consider disable and no_generate attributes during fuzzing, but
    permit the generation of such syscalls for tests (specifically, this
    is needed for compilation tests in pkg/csource).

--- prog/rand.go
+++ prog/rand.go
@@ -623,6 +623,10 @@ func (r *randGen) generateParticularCall(s *state, meta *Syscall) (calls []*Call
 	if meta.Attrs.NoGenerate {
 		panic(fmt.Sprintf("generating no_generate call: %v", meta.Name))
 	}
+	return r.generateParticularCallUnsafe(s, meta)
+}
+
+func (r *randGen) generateParticularCallUnsafe(s *state, meta *Syscall) (calls []*Call) {
 	c := MakeCall(meta, nil)
 	// KFuzzTest calls restrict mutation and generation. Since calls to
 	// generateParticularCall can be recursive, we save the previous value, and
@@ -648,7 +652,7 @@ func (target *Target) GenerateAllSyzProg(rs rand.Source) *Prog {
 	r := newRand(target, rs)
 	s := newState(target, target.DefaultChoiceTable(), nil)
 	for _, meta := range target.PseudoSyscalls() {
-		calls := r.generateParticularCall(s, meta)
+		calls := r.generateParticularCallUnsafe(s, meta)
 		for _, c := range calls {
 			s.analyze(c)
 			p.Calls = append(p.Calls, c)
@@ -667,8 +671,13 @@ func (target *Target) PseudoSyscalls() []*Syscall {
 	for _, meta := range target.Syscalls {
 		if !strings.HasPrefix(meta.CallName, "syz_") ||
 			handled[meta.CallName] ||
-			meta.Attrs.Disabled ||
-			meta.Attrs.NoGenerate {
+			meta.Attrs.Disabled {
+			continue
+		}
+		// TODO: enable syz_kfuzztest_run.
+		// Use of global cache in KFuzzTestRunID causes problems when running multiple
+		// targets in the same process (e.g. in TestGenerate).
+		if meta.Attrs.KFuzzTest {
 			continue
 		}
 		ret = append(ret, meta)
@@ -684,7 +693,7 @@ func (target *Target) GenSampleProg(meta *Syscall, rs rand.Source, ct *ChoiceTab
 	p := &Prog{
 		Target: target,
 	}
-	for _, c := range r.generateParticularCall(s, meta) {
+	for _, c := range r.generateParticularCallUnsafe(s, meta) {
 		s.analyze(c)
 		p.Calls = append(p.Calls, c)
 	}
