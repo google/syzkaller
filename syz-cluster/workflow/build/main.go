cbefe8bbbaa4b5e1a0819a78f831686fd319b9d8
Pimyn Girgis <pimyn@google.com>
2026-03-06 19:41:14 +0100

    syz-cluster: support GCE VMs in workflows
    Propagate VM type configuration throughout the syz-cluster pipeline to allow fuzzing on GCE in addition to QEMU.

--- syz-cluster/workflow/build/main.go
+++ syz-cluster/workflow/build/main.go
@@ -247,7 +247,7 @@ func buildKernel(tracer debugtracer.DebugTracer, req *api.BuildRequest) (*BuildR
 	params := build.Params{
 		TargetOS:     targets.Linux,
 		TargetArch:   req.Arch,
-		VMType:       "qemu", // TODO: support others.
+		VMType:       req.VMType,
 		KernelDir:    *flagRepository,
 		OutputDir:    *flagOutput,
 		Compiler:     "clang",
