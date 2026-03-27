6dbdcb9faf679b24ac3c710de1e25e58fd12c498
Florent Revest <revest@chromium.org>
2026-03-27 14:10:01 +0100

    pkg/aflow: fix package typo assessmenet -> assessment

--- pkg/aflow/flow/assessment/actions.go
+++ pkg/aflow/flow/assessment/actions.go
@@ -1,7 +1,7 @@
 // Copyright 2026 syzkaller project authors. All rights reserved.
 // Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
 
-package assessmenet
+package assessment
 
 import (
 	"github.com/google/syzkaller/pkg/aflow"
