085759698c17f193668e8551a98e8ef748c3abbc
Marco Elver <elver@google.com>
2026-03-28 00:31:27 +0100

    pkg/aflow: fix assessment package spelling

--- pkg/aflow/flow/assessment/kcsan.go
+++ pkg/aflow/flow/assessment/kcsan.go
@@ -1,7 +1,7 @@
 // Copyright 2025 syzkaller project authors. All rights reserved.
 // Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
 
-package assessmenet
+package assessment
 
 import (
 	"github.com/google/syzkaller/pkg/aflow"
