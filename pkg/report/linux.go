5171e1e4ae459298df3cd243ed4a7a6a129e42ce
Pimyn Girgis <pimyn@google.com>
2026-05-26 17:28:17 +0200

    pkg/report: allow newlines in task name
    When a syz program runs `prctl$PR_SET_NAME`, we sometimes have task names with a newline, which we didn't account for.
    This caused the report titles to be truncated.
    Allow the task name to contain newlines, and continue parsing normally.
    See https://syzkaller.appspot.com/bug?id=9d0591d87139a0786b377c6ca58b513257ed39a2.

--- pkg/report/linux.go
+++ pkg/report/linux.go
@@ -2250,7 +2250,7 @@ var linuxOopses = append([]*oops{
 				},
 			},
 			{
-				title: compile(`INFO: task .* blocked (?:in I/O wait )?for more than [0-9]+ seconds`),
+				title: compile(`INFO: task {{TASK}} blocked (?:in I/O wait )?for more than [0-9]+ seconds`),
 				fmt:   "INFO: task hung in %[1]v",
 				alt:   []string{"hang in %[1]v"},
 				stack: &stackFmt{
@@ -2262,7 +2262,7 @@ var linuxOopses = append([]*oops{
 				},
 			},
 			{
-				title: compile("INFO: task .* can't die for more than .* seconds"),
+				title: compile("INFO: task {{TASK}} can't die for more than .* seconds"),
 				fmt:   "INFO: task can't die in %[1]v",
 				alt:   []string{"hang in %[1]v"},
 				stack: &stackFmt{
