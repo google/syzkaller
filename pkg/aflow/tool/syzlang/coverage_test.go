a96d05977ad2656c510d035dd401e9036fb3aaea
Chuyang Wang <chuyangw@google.com>
2026-06-10 10:24:25 +0000

    pkg/aflow/tool/syzlang: extend coverage retrieval tools
    Currently, getFileCoverage returns the coverage of the entire file,
    which could blow up LLM context for certain Linux kernel files with
    several thousands lines. Add an array Functions as argument to allow the
    LLM to grep only coverage for certain functions it's interested in. We
    also add a hard line number limit to getFileCoverage.

--- pkg/aflow/tool/syzlang/coverage_test.go
+++ pkg/aflow/tool/syzlang/coverage_test.go
@@ -6,6 +6,7 @@ package syzlang
 import (
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/google/syzkaller/pkg/aflow"
@@ -91,4 +92,81 @@ void foo(void) {
      9: 
 `
 	require.Equal(t, expectedSnippet, res.Snippets[0])
+
+	// Test truncation limit using the formatter directly.
+	lines := []int{5, 6, 7}
+	srcLines := strings.Split(srcContent, "\n")
+	formatter := newCoverageFormatter(srcLines, 5)
+	truncated := formatter.addFunction("foo", lines)
+
+	require.True(t, truncated)
+	require.Len(t, formatter.snippets, 1)
+	require.Equal(t, 0, formatter.remainingLines)
+
+	expectedSnippetTrunc := `Function: foo
+     1: 1
+     2: 2
+     3: 3
+     4: 4
+*    5: void foo(void) {
+[Truncated due to reaching maximum line limit. Use 'Functions' to narrow your query]
+`
+	require.Equal(t, expectedSnippetTrunc, formatter.snippets[0])
+}
+
+func TestFileCoverageNoCoverage(t *testing.T) {
+	ctx := aflow.NewTestContext(t)
+
+	// Provide empty coverage for this test.
+	_, reproExecCachedID, err := aflow.CacheObject(ctx, "repro", "dummy-desc-3", func() (map[string]any, error) {
+		return map[string]any{"Coverage": [][]symbolizer.Frame{}}, nil
+	})
+	require.NoError(t, err)
+
+	_, err = getFileCoverage(ctx, reproduceState{}, FileCoverageArgs{
+		ExecutionCachedID: reproExecCachedID,
+		Filename:          "nonexistent.c",
+	})
+	require.IsType(t, aflow.BadCallError(""), err)
+}
+
+func TestFileCoverageMissingFunction(t *testing.T) {
+	ctx := aflow.NewTestContext(t)
+
+	dummyCov := [][]symbolizer.Frame{
+		{
+			{File: "foo.c", Func: "foo", Line: 5},
+		},
+	}
+
+	_, reproExecCachedID, err := aflow.CacheObject(ctx, "repro", "dummy-desc-4", func() (map[string]any, error) {
+		return map[string]any{"Coverage": dummyCov}, nil
+	})
+	require.NoError(t, err)
+
+	kernelSrc := t.TempDir()
+	err = osutil.MkdirAll(kernelSrc)
+	require.NoError(t, err)
+
+	srcContent := `1
+2
+3
+4
+void foo(void) {
+    int a = 1;
+    a++;
+}
+`
+	err = os.WriteFile(filepath.Join(kernelSrc, "foo.c"), []byte(srcContent), 0644)
+	require.NoError(t, err)
+
+	res, err := getFileCoverage(ctx, reproduceState{KernelSrc: kernelSrc}, FileCoverageArgs{
+		ExecutionCachedID: reproExecCachedID,
+		Filename:          "foo.c",
+		Functions:         []string{"foo", "nonexistent_function"},
+	})
+	require.NoError(t, err)
+
+	require.Len(t, res.Snippets, 2)
+	require.Contains(t, res.Snippets[1], "nonexistent_function: [No coverage found or function does not exist]")
 }
