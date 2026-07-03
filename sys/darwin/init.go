3772dab8df9ec4d3ecb2ef80278dabeeda0ae4c7
Alexander Potapenko <glider@google.com>
2026-07-03 09:27:41 +0000

    sys: add package comments
    Update #6915

--- sys/darwin/init.go
+++ sys/darwin/init.go
@@ -1,6 +1,7 @@
 // Copyright 2021 syzkaller project authors. All rights reserved.
 // Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
 
+// Package darwin contains target initialization and syscall neutralization logic.
 package darwin
 
 import (
--- sys/trusty/init.go
+++ sys/trusty/init.go
@@ -1,6 +1,7 @@
 // Copyright 2018 syzkaller project authors. All rights reserved.
 // Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
 
+// Package trusty contains target initialization logic.
 package trusty
 
 import (
