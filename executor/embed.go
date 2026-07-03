f9192193433fcd44356a10af42b3244dce5aa106
Alexander Potapenko <glider@google.com>
2026-07-03 09:27:41 +0000

    executor: add package comments
    Update #6915

--- executor/embed.go
+++ executor/embed.go
@@ -1,6 +1,7 @@
 // Copyright 2024 syzkaller project authors. All rights reserved.
 // Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
 
+// Package executor embeds C/C++ executor sources and prebuilt binaries.
 package executor
 
 import (
